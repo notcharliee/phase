c8bb2ad5545adb2c6deeb47f75a88142ab4503da
Xictul <saunderscharlie@hotmail.com>
2023-10-27 20:13:47 +0100

    edit: tailwind config values

--- apps/site/tailwind.config.js
+++ apps/site/tailwind.config.js
@@ -1,16 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: [
-    './pages/**/*.{js,ts,jsx,tsx,mdx}',
-    './utils/components/**/*.{js,ts,jsx,tsx,mdx}',
+    './components/**/*.{js,ts,jsx,tsx,mdx}',
     './app/**/*.{js,ts,jsx,tsx,mdx}',
   ],
   theme: {
     extend: {
       fontFamily: {
-        'lilita': ['var(--font-lilita)'],
+        'cubano': ['var(--font-cubano)'],
         'poppins': ['var(--font-poppins)'],
-      }
+      },
+      backgroundImage: {
+        'phase': 'linear-gradient(45deg, #7000FF, #DB00FF)',
+        'main-gif': 'url(/bg.gif)',
+        'main-static': 'url(/bg.png)',
+      },
     },
   },
   plugins: [],
