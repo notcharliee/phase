c239f377a210df30cd7bb9f6b1a7090d43256614
Xictul <saunderscharlie@hotmail.com>
2023-10-30 16:28:59 +0000

    modify(site): edit bot invite redirect

--- apps/site/app/redirect/invite/route.ts
+++ apps/site/app/redirect/invite/route.ts
@@ -1,9 +1,8 @@
-import { NextResponse, NextRequest } from 'next/server'
+import * as Server from 'next/server'
 
-export function GET (request: NextRequest) {
-    if(process.env.NODE_ENV == 'development') {
-        return NextResponse.redirect('https://discord.com/oauth2/authorize?client_id=1078305837245796483&response_type=code&scope=guilds%20identify%20bot%20applications.commands&redirect_uri=http://localhost:3000/api/login&permissions=486911216')
-    } else {
-        return NextResponse.redirect('https://discord.com/oauth2/authorize?client_id=1078305837245796483&response_type=code&scope=guilds%20identify%20bot%20applications.commands&redirect_uri=https://phasebot.xyz/api/login&permissions=486911216')
-    }
+
+export function GET () {
+
+    return Server.NextResponse.redirect(`https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_ID}&response_type=code&scope=guilds%20identify%20bot%20applications.commands&redirect_uri=${process.env.BASE_URL}/api/login&permissions=486911216`)
+
 }
