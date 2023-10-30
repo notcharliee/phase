a108a6cd89e02fb3735d575c1d4cc4a1f15deca1
Xictul <saunderscharlie@hotmail.com>
2023-10-30 04:39:16 +0000

    create(site): sidebar link component

---
+++ apps/site/components/SidebarLink.tsx
@@ -0,0 +1,18 @@
+'use client'
+
+import Link from 'next/link'
+
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import * as FaSolid from '@fortawesome/free-solid-svg-icons'
+
+
+export default ({ icon, title, href }: { icon: FaSolid.IconDefinition, title?: string, href: string }) => {
+
+  return (
+    <Link href={href} className='w-full bg-dark-700 hover:bg-dark-600 hover:no-underline font-medium p-3 pl-4 pr-4 rounded shadow border border-dark-600'>
+      <FontAwesomeIcon icon={icon} className='w-4 h-4 inline-block' style={{ marginRight: title ? '10px' : undefined }}></FontAwesomeIcon>
+      {title ?? <></>}
+    </Link>
+  )
+
+}
