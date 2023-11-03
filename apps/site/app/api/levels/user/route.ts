b274ffe140f0d881714d2449e0233030becc7192
Xictul <saunderscharlie@hotmail.com>
2023-11-03 22:33:57 +0000

    create(site): `/api/levels/user` endpoint

---
+++ apps/site/app/api/levels/user/route.ts
@@ -0,0 +1,84 @@
+import { NextResponse, NextRequest } from 'next/server'
+import { cookies } from 'next/headers'
+
+import { API } from '@discordjs/core/http-only'
+import { REST } from '@discordjs/rest'
+
+import mongoose from 'mongoose'
+import * as Schemas from 'utils/schemas'
+
+
+export const GET = async (request: NextRequest) => {
+
+  const discordREST = new REST().setToken(process.env.DISCORD_TOKEN!)
+  const discordAPI = new API(discordREST)
+
+  // Check if user is authorised to use endpoint...
+
+  const authorisationCode = request.headers.get('Authorization')
+  ? request.headers.get('Authorization')?.split(' ')[1] 
+  : cookies().get('authorised_user')?.value
+
+  const userId = request.nextUrl.searchParams.get('user')
+  const guildId = request.nextUrl.searchParams.get('guild')
+
+  if (!authorisationCode) return NextResponse.json({
+    error: 'Unauthorised',
+    documentation: `${process.env.BASE_URL}/docs/api/authorisation`,
+  }, { status: 401 })
+
+  await mongoose.connect(process.env.DATABASE_URI!)
+
+  const authorisedUserSchema = await Schemas.AuthorisedUsers.findOne({ session: authorisationCode })
+
+  const isPhaseBot = authorisationCode == process.env.DISCORD_SECRET
+  const isAuthorised = isPhaseBot ? true : (!!authorisedUserSchema && !!(authorisedUserSchema.guilds.find((guild) => guild.id == guildId)))
+
+  if (!isAuthorised) return NextResponse.json({
+    error: 'Unauthorised',
+    documentation: `${process.env.BASE_URL}/docs/api/authorisation`,
+  }, { status: 401 })
+
+  // Get data...
+
+  if (!userId || !guildId) return NextResponse.json({
+    error: 'Bad Request',
+    documentation: `${process.env.BASE_URL}/docs/api/levels`,
+  }, { status: 400 })
+
+  try {
+
+    const guildLevelData = await Schemas.Levels.findOne({ guild: guildId, levels: { $elemMatch: { id: userId } } })
+
+    if (!guildLevelData) return NextResponse.json({
+      error: 'Not Found',
+      message: `No matching data found.`,
+    }, { status: 404 })
+
+    const userLevelsArray = guildLevelData.levels
+    const userLevelsArraySorted = userLevelsArray.sort((a, b) => a == b ? b.xp - a.xp : b.level - a.level)
+
+    const userLevelDataIndex = userLevelsArraySorted.findIndex((user) => user.id == userId)
+    const userLevelData = userLevelsArraySorted[userLevelDataIndex]
+
+    const userData = await discordAPI.users.get(userLevelData.id)
+
+    return NextResponse.json({
+      ...userData,
+      ...((({ id, ...rest }) => rest)(userLevelData)),
+      rank: userLevelDataIndex + 1,
+    })
+
+  } catch (error) {
+
+    console.log(error)
+
+    return NextResponse.json(error, { status: 500 })
+    
+  } finally {
+
+    await mongoose.connection.close()
+
+  }
+
+}
