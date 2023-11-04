405cc1d1d212af75bce01c6804b80bc0dfbb93cd
Xictul <saunderscharlie@hotmail.com>
2023-11-04 22:55:17 +0000

    fix(site): `SidebarDropdown` component styling

--- apps/site/components/SidebarDropdown.tsx
+++ apps/site/components/SidebarDropdown.tsx
@@ -16,14 +16,14 @@ export default ({ icon, title, items, disabled }: { icon: FaSolid.IconDefinition
   return (
     <div>
       <div
-        className={'w-full bg-dark-700 font-medium p-3 pl-4 pr-4 rounded select-none shadow border border-dark-600' + `${disabled
+        className={'w-full bg-dark-700 font-medium p-3 pl-4 pr-4 rounded select-none shadow border border-dark-600 flex items-center' + `${disabled
           ? ' cursor-not-allowed brightness-90'
           : ' cursor-pointer hover:bg-dark-600'
         }`} onClick={toggleLinksVisibility}
       >
         <FontAwesomeIcon icon={icon} className='w-4 h-4 mr-2.5 inline-block'></FontAwesomeIcon>
         {title}
-        <FontAwesomeIcon icon={FaSolid.faAngleDown} className='w-4 h-4 float-right duration-150' style={{ transform: linksVisibility ? 'rotate(180deg)' : 'rotate(0deg)' }}></FontAwesomeIcon>
+        <FontAwesomeIcon icon={FaSolid.faAngleDown} className='ml-auto w-4 h-4 float-right duration-150' style={{ transform: linksVisibility ? 'rotate(180deg)' : 'rotate(0deg)' }}></FontAwesomeIcon>
       </div>
       <div className='flex-col border-l ml-5 border-dark-600' style={{ display: linksVisibility ? 'flex' : 'none' }}>
         {items?.map((item, key) => {
