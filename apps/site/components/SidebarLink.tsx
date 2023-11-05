164282c6692a5ee153a8b51f5d34e38001e54bd2
Xictul <saunderscharlie@hotmail.com>
2023-11-05 15:00:38 +0000

    modify(site): `SidebarLink` component styles

--- apps/site/components/SidebarLink.tsx
+++ apps/site/components/SidebarLink.tsx
@@ -9,7 +9,7 @@ import * as FaSolid from '@fortawesome/free-solid-svg-icons'
 export default ({ icon, title, href }: { icon: FaSolid.IconDefinition, title?: string, href: string }) => {
 
   return (
-    <Link href={href} className='w-full bg-dark-700 hover:bg-dark-600 hover:no-underline font-medium p-3 pl-4 pr-4 rounded shadow border border-dark-600'>
+    <Link href={href} className='w-full bg-dark-700 hover:bg-dark-600 font-medium p-3 pl-4 pr-4 rounded shadow border border-dark-600'>
       <FontAwesomeIcon icon={icon} className='w-4 h-4 inline-block' style={{ marginRight: title ? '10px' : undefined }}></FontAwesomeIcon>
       {title ?? <></>}
     </Link>
