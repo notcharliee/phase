f9899c359ea1e629aa79b34e5435964ebc93c2b6
Xictul <saunderscharlie@hotmail.com>
2023-11-07 09:57:29 +0000

    modify(site): reorder `QuickNavigate` links again

--- apps/site/components/QuickNavigate.tsx
+++ apps/site/components/QuickNavigate.tsx
@@ -22,20 +22,21 @@ export default ({ text }: { text?: 'none' | 'min' | 'max' } = { text: 'max' }) =
           <FontAwesomeIcon icon={FaRegular.faCompass} className='w-5 h-5'></FontAwesomeIcon>
         </span>
         <SidebarDropdown
-          icon={FaRegular.faSquare}
+          icon={FaRegular.faWindowMaximize}
           title='pages'
           items={[
-            <Link href='/dashboard'>Dashboard</Link>,
-            <Link href='/redirect/donate'>Donate</Link>,
+            <Link href='/'>Home</Link>,
             <Link href='/blog'>Blog</Link>,
+            <Link href='/dashboard'>Dashboard</Link>,
+            <Link href='/docs'>Docs</Link>,
           ]}
           open
         ></SidebarDropdown>
         <SidebarDropdown
-          icon={FaRegular.faMap}
-          title='resources'
+          icon={FaRegular.faWindowRestore}
+          title='links'
           items={[
-            <Link href='/docs'>Documentation</Link>,
+            <Link href='/redirect/donate'>Donate</Link>,
             <Link href='/redirect/discord'>Discord Server</Link>,
             <Link href='/redirect/github'>GitHub Repository</Link>,
           ]}
