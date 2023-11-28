3942d144c1c4d06ad2ef9880ec6bdd80736efb69
Xictul <saunderscharlie@hotmail.com>
2023-11-28 08:07:54 +0000

    modify(site): redirect route runtimes

--- apps/site/src/app/redirect/github/route.ts
+++ apps/site/src/app/redirect/github/route.ts
@@ -1,5 +1,8 @@
 import { NextResponse } from "next/server"
 
+export const runtime = "edge"
+export const dynamic = "force-static"
+
 export function GET() {
   return NextResponse.redirect("https://github.com/notcharliee/phase")
 }
--- apps/site/src/app/redirect/invite/route.ts
+++ apps/site/src/app/redirect/invite/route.ts
@@ -1,7 +1,10 @@
 import { NextResponse } from "next/server"
 
+export const runtime = "edge"
+export const dynamic = "force-static"
+
 export function GET() {
   return NextResponse.redirect(
-    `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_ID}&response_type=code&scope=guilds%20identify%20bot%20applications.commands&redirect_uri=${process.env.BASE_URL}/api/login&permissions=486911216`,
+    `https://discord.com/oauth2/authorize?client_id=${process.env.DISCORD_ID}&response_type=code&scope=guilds%20identify%20bot%20applications.commands&redirect_uri=${process.env.NEXT_PUBLIC_BASE_URL}/api/login&permissions=486911216`,
   )
 }
