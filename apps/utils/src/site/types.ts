ea46c76da40aa912dcf97b3f11f7b2fe27711cf0
Xictul <saunderscharlie@hotmail.com>
2023-12-03 05:28:13 +0000

    remove(utils)

--- apps/utils/src/site/types.ts
+++
@@ -1,9 +0,0 @@
-import * as discord_api_types_v10 from 'discord-api-types/v10'
-
-export type AuthorisedUser = {
-  identity: discord_api_types_v10.APIUser,
-  guilds: discord_api_types_v10.RESTGetAPICurrentUserGuildsResult,
-  session: `${string}-${string}-${string}-${string}-${string}`,
-  token: discord_api_types_v10.RESTPostOAuth2AccessTokenResult,
-  timestamp: string,
-}
