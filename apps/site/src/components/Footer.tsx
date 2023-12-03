8e4392c74826ad546e420c2ed38cc275d7f90d7c
Xictul <saunderscharlie@hotmail.com>
2023-12-03 16:03:31 +0000

    modify(site): `Footer` component

--- apps/site/src/components/Footer.tsx
+++ apps/site/src/components/Footer.tsx
@@ -6,28 +6,27 @@ export default () => {
       <div className="mx-auto max-w-7xl px-4 py-6 lg:px-6">
         <div className="flex flex-col md:flex-row-reverse md:justify-between">
           <div className="flex gap-4">
-            <Link href="/docs" className="hover:text-light-500">
+            <Link href="/docs" className="hover:text-light-800 duration-150">
               docs
             </Link>
-            <Link href="/blog" className="hover:text-light-500">
+            <Link href="/blog" className="hover:text-light-800 duration-150">
               blog
             </Link>
-            <Link href="/redirect/github" className="hover:text-light-500">
+            <Link href="/redirect/github" className="hover:text-light-800 duration-150">
               github
             </Link>
-            <Link href="/redirect/donate" className="hover:text-light-500">
+            <Link href="/redirect/donate" className="hover:text-light-800 duration-150">
               donate
             </Link>
           </div>
           <span className="mt-4 md:mt-0">
             built by{" "}
             <Link
-              href="https://github.com/notcharliee"
-              className="hover:text-light-500"
+              href="https://charliee.dev"
+              className="text-light-500 hover:text-light-800 font-semibold duration-150"
             >
-              notcharliee
+              notcharliee.
             </Link>
-            .
           </span>
         </div>
       </div>
