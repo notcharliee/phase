7675f1799c4b7b83db9567fabcd5be887999d691
notcharliee <saunderscharlie@hotmail.com>
2023-12-29 13:36:39 +0000

    modify(site): global layout metadata & viewport values

--- apps/site/src/app/layout.tsx
+++ apps/site/src/app/layout.tsx
@@ -1,61 +1,47 @@
 import "@/styles/globals.css"
+import "@/styles/keyframes.css"
 import { GeistSans } from "geist/font"
 import { Metadata, Viewport } from "next"
 import { env } from '@/env'
 
 
-export const metadata: Metadata = {
-  title: "Phase",
-  description:
-    "Phase is a free and open source all-in-one Discord bot. Our aim is to provide a comprehensive set of tools that enable effortless management, moderation, and optimization of your servers.",
+export const metadata = {
+  title: "Phase Bot",
+  description: "Phase is a free to use, open source Discord bot that aims to be the all-in-one solution for as many servers as possible.",
+  metadataBase: new URL(env.NEXT_PUBLIC_BASE_URL),
   icons: env.NEXT_PUBLIC_BASE_URL + "/og_image.png",
-  authors: { name: "@notcharliee", url: "https://github.com/notcharliee" },
+  authors: {
+    name: "@notcharliee",
+    url: "https://github.com/notcharliee",
+  },
   keywords: [
     "discord",
     "bot",
     "phase",
     "phasebot",
-    "levels",
     "free",
     "open source",
     "notcharliee",
+    "charliee",
   ],
-  metadataBase: new URL(env.NEXT_PUBLIC_BASE_URL),
-  openGraph: {
-    type: "website",
-    url: env.NEXT_PUBLIC_BASE_URL,
-    title: "Phase",
-    description:
-      "Phase is a free and open source all-in-one Discord bot. Our aim is to provide a comprehensive set of tools that enable effortless management, moderation, and optimization of your servers.",
-    images: env.NEXT_PUBLIC_BASE_URL + "/og_image.png",
-  },
-  twitter: {
-    card: "summary",
-    creator: "@notcharliee",
-    description:
-      "Phase is a free and open source all-in-one Discord bot. Our aim is to provide a comprehensive set of tools that enable effortless management, moderation, and optimization of your servers.",
-    site: env.NEXT_PUBLIC_BASE_URL,
-    images: env.NEXT_PUBLIC_BASE_URL + "/og_image.png",
-  },
-}
+} satisfies Metadata
 
 
-export const viewort: Viewport = {
+export const viewort = {
   colorScheme: "dark",
-  themeColor: "#A400FF",
-}
+  themeColor: "#F5F5F5",
+} satisfies Viewport
 
 
-export default ({ children }: { children: React.ReactNode }) => {
-  return (
-    <html
-      lang="en"
-      className={GeistSans.variable + " font-geist-sans tracking-tight"}
-    >
-      <body>
-        <div id="modals"></div>
-        {children}
-      </body>
-    </html>
-  )
-}
+export default ({
+  children,
+}: {
+  children: React.ReactNode,
+}) => (
+  <html lang="en" className={GeistSans.variable + " font-geist-sans tracking-tight"}>
+    <body>
+      <div id="modals"></div>
+      {children}
+    </body>
+  </html>
+)
