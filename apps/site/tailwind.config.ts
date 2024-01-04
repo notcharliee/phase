8b316625d8e0c45bf3db4c0ebae6a53dfd2048fb
notcharliee <saunderscharlie@hotmail.com>
2024-01-04 15:26:27 +0000

    modify(site): tailwind config

--- apps/site/tailwind.config.ts
+++ apps/site/tailwind.config.ts
@@ -7,15 +7,27 @@ const config: Config = {
   ],
   theme: {
     extend: {
-      fontFamily: {
-        "geist-sans": "var(--font-geist-sans)",
+      animation: {
+        "accordion-down": "accordion-down 0.2s ease-out",
+        "accordion-up": "accordion-up 0.2s ease-out",
       },
       backgroundImage: {
-        main: "url(/bg.png)",
-        grid: "url(/docs/bg-grid.png)",
-        noise: "url(/noise.png)",
+        "main": "url(/bg.png)",
+        "grid": "url(/docs/bg-grid.png)",
+        "noise": "url(/noise.png)",
+      },
+      boxShadow: {
+        "glow": `
+          0 0 4px 0 var(--tw-shadow-color),
+          0 0 11px 0 var(--tw-shadow-color),
+          0 0 19px 0 var(--tw-shadow-color),
+          0 0 40px 0 var(--tw-shadow-color),
+          0 0 80px 0 var(--tw-shadow-color),
+          0 0 90px 0 var(--tw-shadow-color)
+        `
       },
       colors: {
+        // main colours
         "dark-900": "rgb(0 0 0)",
         "dark-800": "rgb(8 8 8)",
         "dark-700": "rgb(16 16 16)",
@@ -34,31 +46,65 @@ const config: Config = {
         "light-300": "rgb(208 208 208)",
         "light-200": "rgb(200 200 200)",
         "light-100": "rgb(192 192 192)",
-        "phase": "rgb(164 0 255)",
+        // shadcn colours
+        "background": "rgb(8 8 8)",
+        "foreground": "rgb(248 248 248)",
+        "card": "rgb(8 8 8)",
+        "card-foreground": "rgb(248 248 248)",
+        "popover": "rgb(8 8 8)",
+        "popover-foreground": "rgb(248 248 248)",
+        "primary": "rgb(248 248 248)",
+        "primary-foreground": "rgb(24 24 24)",
+        "secondary": "rgb(40 40 40)",
+        "secondary-foreground": "rgb(248 248 248)",
+        "muted": "rgb(40 40 40)",
+        "muted-foreground": "rgb(192 192 192)",
+        "accent": "rgb(40 40 40)",
+        "accent-foreground": "rgb(248 248 248)",
+        "border": "rgb(40 40 40)",
+        "input": "rgb(40 40 40)",
+        "ring": "rgb(216 216 216)",
       },
-      height: { // @ts-ignore
-        "dvh": [
-          "100dvh",
-          "100vh",
-        ],
+      fontFamily: {
+        "geist-sans": "var(--font-geist-sans)",
       },
-      minHeight: { // @ts-ignore
-        "dvh": [
-          "100dvh",
-          "100vh",
-        ],
+      height: {
+        "dvh": "100dvh",
       },
-      maxHeight: { // @ts-ignore
-        "dvh": [
-          "100dvh",
-          "100vh",
-        ],
+      maxHeight: {
+        "dvh": "100dvh",
+      },
+      minHeight: {
+        "dvh": "100dvh",
+      },
+      textShadow: {
+        "sm": "0 1px 2px var(--tw-shadow-color)",
+        "DEFAULT": "0 2px 4px var(--tw-shadow-color)",
+        "lg": "0 8px 16px var(--tw-shadow-color)",
+        "glow": `
+          0 0 4px var(--tw-shadow-color),
+          0 0 11px var(--tw-shadow-color),
+          0 0 19px var(--tw-shadow-color),
+          0 0 40px var(--tw-shadow-color),
+          0 0 80px var(--tw-shadow-color),
+          0 0 90px var(--tw-shadow-color)
+        `
       },
     },
   },
   plugins: [
     plugin(({ addVariant }) => { // children:text-white
       addVariant("children", "&>*")
+    }),
+    plugin(({ matchUtilities, theme }) => { // text-shadow
+      matchUtilities(
+        {
+          "text-shadow": (value) => ({
+            textShadow: value,
+          }),
+        },
+        { values: theme("textShadow") }
+      )
     }),
     plugin(({ matchUtilities, theme }) => { // animation-delay-200
       matchUtilities(
