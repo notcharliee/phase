1ae7fc7bd6e46bd7b98f40b32bc6824191a02a58
notcharliee <saunderscharlie@hotmail.com>
2024-01-06 00:21:43 +0000

    remove: utils package

--- packages/utils/dist/schemas/index.d.ts
+++
@@ -1,7 +0,0 @@
-export { AFK, AFKSchema } from './AFKs.js';
-export { Game, GameData, GameDataTictactoe, GameSchema } from './Games.js';
-export { Giveaway, GiveawaySchema } from './Giveaways.js';
-export { Guild, GuildCommand, GuildModule, GuildModuleAuditLogs, GuildModuleAutoPartners, GuildModuleAutoRoles, GuildModuleJoinToCreates, GuildModuleLevels, GuildModuleReactionRoles, GuildModuleTickets, GuildSchema } from './Guilds.js';
-export { Level, LevelSchema } from './Levels.js';
-export { Tag, TagSchema } from './Tags.js';
-import 'mongoose';
