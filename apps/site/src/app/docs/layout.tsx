be3bcdd2bc64e6592460252e8b12a0ddfac576e5
notcharliee <saunderscharlie@hotmail.com>
2024-01-06 01:26:13 +0000

    modify(docs): v3 redesign

--- apps/site/src/app/docs/layout.tsx
+++ apps/site/src/app/docs/layout.tsx
@@ -1,69 +1,16 @@
-import Link from "next/link"
-import QuickNavigate from "@/components/QuickNavigate"
-import axios from "axios"
-import discord_api_types_v10 from "discord-api-types/v10"
-import { env } from '@/env'
+import {
+  MobileNaviagtion,
+  SidebarLinks,
+} from "@/components/docs"
 
-
-export const dynamic = "force-dynamic"
-
-
-export default async ({ children }: { children: React.ReactNode }) => {
-  const commandArray = await axios.get<discord_api_types_v10.RESTGetAPIApplicationCommandsResult>(env.NEXT_PUBLIC_BASE_URL + '/api/bot/commands')
-
-  return (
-    <div className="font-poppins flex min-h-dvh flex-col gap-3 bg-dark-900 p-6 text-light-800 md:flex-row">
-      <div className="flex max-h-[calc(100vh-48px)] min-h-full flex-col gap-3">
-        <div className="flex items-center justify-between rounded border border-dark-600 p-3">
-          <Link href="/docs" className="text-xl font-bold">
-            <span>phase</span>
-            <span className="text-phase">docs</span>
-          </Link>
-          <QuickNavigate text="min"></QuickNavigate>
-        </div>
-        <nav className="scrollbar-float hidden h-full min-w-[280px] flex-col gap-6 overflow-y-auto rounded border border-dark-600 p-3 md:flex">
-          <div className="flex flex-col gap-3">
-            <div>
-              <Link href={"/docs"} className="text-lg font-semibold decoration-phase underline-offset-4 duration-150 hover:underline">
-                Introduction
-              </Link>
-              <div className="mt-1.5 flex flex-col pl-1 text-sm leading-6 text-light-100 children:duration-150 hover:children:text-phase">
-                <Link href="/docs#what-is-phase">What is Phase?</Link>
-                <Link href="/docs#why-should-i-use-phase">Why should I use Phase?</Link>
-              </div>
-            </div>
-            <div>
-              <Link href={"/docs/modules"} className="text-lg font-semibold decoration-phase underline-offset-4 duration-150 hover:underline">
-                Modules
-              </Link>
-              <div className="mt-1.5 flex flex-col pl-1 text-sm leading-6 text-light-100 children:duration-150 hover:children:text-phase">
-                <Link href="/docs/modules#afks">AFKs</Link>
-                <Link href="/docs/modules#auditlogs">Audit Logs</Link>
-                <Link href="/docs/modules#autopartners">Auto Partners</Link>
-                <Link href="/docs/modules#autoroles">Auto Roles</Link>
-                <Link href="/docs/modules#giveaways">Giveaways</Link>
-                <Link href="/docs/modules#jointocreate">Join to Create</Link>
-                <Link href="/docs/modules#levels">Levels & XP</Link>
-                <Link href="/docs/modules#reactionroles">Reaction Roles</Link>
-                <Link href="/docs/modules#tickets">Tickets</Link>
-              </div>
-            </div>
-            <div>
-            <Link href={"/docs/commands"} className="text-lg font-semibold decoration-phase underline-offset-4 duration-150 hover:underline">
-                Commands
-              </Link>
-              <div className="mt-1.5 flex flex-col pl-1 text-sm leading-6 text-light-100 children:duration-150 hover:children:text-phase">
-                {commandArray.data.map((command, key) => {
-                  return <Link key={key} href={`/docs/commands#${command.name}`}>/{command.name}</Link>
-                })}
-              </div>
-            </div>
-          </div>
-        </nav>
-      </div>
-      <main className="scrollbar-float w-full flex-grow scroll-pt-6 overflow-y-auto scroll-smooth break-words rounded border border-dark-600 bg-grid p-6 md:max-h-[calc(100vh-48px)]">
-        {children}
-      </main>
-    </div>
-  )
-}
+export default ({ children }: { children: React.ReactNode }) => (
+  <div className="w-full max-w-[1400px] min-h-screen mx-auto flex gap-12 px-8">
+    <aside className="w-64 h-screen sticky hidden md:block">
+      <SidebarLinks className="py-6 lg:py-8 pr-6 h-full flex flex-col gap-4 overflow-auto scrollbar-float" />
+    </aside>
+    <main className="py-8 w-full">
+      <MobileNaviagtion />
+      {children}
+    </main>
+  </div>
+)
