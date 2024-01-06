0105242fe9804cea27862eec1658d4c3cc3ea4ba
notcharliee <saunderscharlie@hotmail.com>
2024-01-06 00:37:22 +0000

    fix(site): home page svg props

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -26,15 +26,15 @@ export default () => (
 const Moon: React.FC<SVGProps<SVGSVGElement>> = (props) => (
   <svg {...props} width="676" height="676" viewBox="0 0 676 676" fill="none" xmlns="http://www.w3.org/2000/svg">
     <circle cx="338" cy="338" r="337.5" fill="#F8F8F8"/>
-    <path fill-rule="evenodd" clip-rule="evenodd" d="M645.348 477.639C589.864 523.494 519.292 550.946 442.464 550.946C263.835 550.946 119.027 402.541 119.027 219.473C119.027 156.577 136.12 97.7722 165.805 47.6718C66.8288 106.502 0.5 214.505 0.5 338C0.5 524.396 151.604 675.5 338 675.5C474.594 675.5 592.236 594.354 645.348 477.639Z" fill="#B2B2B2"/>
-    <circle cx="273.714" cy="490.679" r="45.2009" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="10.0446"/>
-    <circle cx="146.434" cy="338" r="23.856" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="10.0446"/>
-    <circle cx="357.085" cy="51.7277" r="19.0848" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
-    <circle cx="399.272" cy="216.46" r="37.1652" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
-    <circle cx="555.969" cy="332.978" r="75.3348" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
-    <circle cx="362.107" cy="352.063" r="42.1875" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
-    <circle cx="221.482" cy="139.116" r="30.1339" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
-    <circle cx="247.598" cy="261.661" r="16.0714" fill="#B2B2B2" stroke="#F8F8F8" stroke-width="8.03571"/>
+    <path fillRule="evenodd" clipRule="evenodd" d="M645.348 477.639C589.864 523.494 519.292 550.946 442.464 550.946C263.835 550.946 119.027 402.541 119.027 219.473C119.027 156.577 136.12 97.7722 165.805 47.6718C66.8288 106.502 0.5 214.505 0.5 338C0.5 524.396 151.604 675.5 338 675.5C474.594 675.5 592.236 594.354 645.348 477.639Z" fill="#B2B2B2"/>
+    <circle cx="273.714" cy="490.679" r="45.2009" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="10.0446"/>
+    <circle cx="146.434" cy="338" r="23.856" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="10.0446"/>
+    <circle cx="357.085" cy="51.7277" r="19.0848" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
+    <circle cx="399.272" cy="216.46" r="37.1652" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
+    <circle cx="555.969" cy="332.978" r="75.3348" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
+    <circle cx="362.107" cy="352.063" r="42.1875" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
+    <circle cx="221.482" cy="139.116" r="30.1339" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
+    <circle cx="247.598" cy="261.661" r="16.0714" fill="#B2B2B2" stroke="#F8F8F8" strokeWidth="8.03571"/>
   </svg>
 )
 
