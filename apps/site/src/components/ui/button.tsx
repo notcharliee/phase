101de9958388a2ea28be1261656d7a5d08fae551
notcharliee <saunderscharlie@hotmail.com>
2024-01-08 23:28:35 +0000

    fix(site): add back `xl` size variant for button

--- apps/site/src/components/ui/button.tsx
+++ apps/site/src/components/ui/button.tsx
@@ -22,6 +22,7 @@ const buttonVariants = cva(
         default: "h-9 px-4 py-2",
         sm: "h-8 rounded-md px-3 text-xs",
         lg: "h-10 rounded-md px-8",
+        xl: "h-12 rounded-lg px-12 text-md",
         icon: "h-9 w-9",
       },
     },
