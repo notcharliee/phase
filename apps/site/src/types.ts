bfd613b89b1ebf0f380423fa5650ce8108d82184
notcharliee <saunderscharlie@hotmail.com>
2024-01-08 20:02:44 +0000

    remove(site): outdated types file

--- apps/site/src/types.ts
+++
@@ -1,8 +0,0 @@
-import discord_api_types_v10 from 'discord-api-types/v10'
-
-export type AuthorisedUser = {
-  id: string,
-  session: `${string}-${string}-${string}-${string}-${string}`,
-  token: discord_api_types_v10.RESTPostOAuth2AccessTokenResult,
-  timestamp: string,
-}
