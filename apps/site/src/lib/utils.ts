5dd67502fff9bbb09604b707b675adbeb783181d
notcharliee <saunderscharlie@hotmail.com>
2024-01-11 18:53:27 +0000

    create(site): `getInitials()` function

--- apps/site/src/lib/utils.ts
+++ apps/site/src/lib/utils.ts
@@ -4,3 +4,12 @@ import { twMerge } from "tailwind-merge"
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const getInitials = (input: string) => {
+  const words = input.split(' ')
+
+  if (!words.length) return "N/A"
+  if (words.length == 1) return words[0]!.slice(0, 2).toUpperCase()
+
+  return words[0]!.charAt(0).toUpperCase() + words[words.length - 1]!.charAt(0).toUpperCase()
+}
