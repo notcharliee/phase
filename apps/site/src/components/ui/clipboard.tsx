9db17d31eef5e48075d5ede46ac238e342711b32
notcharliee <saunderscharlie@hotmail.com>
2024-01-13 03:20:25 +0000

    modify(site): make clipboard component cooler

--- apps/site/src/components/ui/clipboard.tsx
+++ apps/site/src/components/ui/clipboard.tsx
@@ -5,17 +5,30 @@ import { CopyIcon } from "@radix-ui/react-icons"
 import { toast } from "sonner"
 
 import { Button } from "@/components/ui/button"
+import { Input } from "@/components/ui/input"
 
-export const Clipboard = (props: { content: string }) => (
-  <Button variant={"outline"} className="w-full max-w-[256px] text-muted-foreground flex justify-between" onClick={() => {
-    toast("Copied to clipboard.", {
-      description: (
-        <span>Copied <code className="rounded-md border border-border px-1 py-0.5 text-foreground">{props.content}</code> to your clipboard.</span>
-      ),
-    })
-    
-    navigator.clipboard.writeText(props.content)
-  }}>
-    {props.content} <CopyIcon className="h-4 w-4" />
-  </Button>
+
+const copyToClipboard = (content: string, secret?: boolean) => {
+  toast("Copied to clipboard.", {
+    description: (
+      <span>Copied <code className="rounded-md border border-border px-1 py-0.5 text-foreground">{secret ? content.replace(/./g, '*') : content}</code> to your clipboard.</span>
+    ),
+  })
+  
+  navigator.clipboard.writeText(content)
+}
+
+
+export const Clipboard = (props: { children: string, secret?: boolean }) => (
+  <div className="flex items-center space-x-2">
+    <Input
+      className="text-muted-foreground pointer-events-none select-none"
+      defaultValue={props.secret ? props.children.replace(/./g, '*') : props.children}
+      readOnly
+    />
+    <Button type="submit" size="sm" className="px-3" onClick={() => copyToClipboard(props.children, props.secret)}>
+      <span className="sr-only">Copy</span>
+      <CopyIcon className="h-4 w-4" />
+    </Button>
+  </div>
 )
