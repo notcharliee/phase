c6075c0b7124b03f0e40642a0f82c5cedcb85b73
notcharliee <saunderscharlie@hotmail.com>
2024-01-13 03:19:08 +0000

    modify(site): add destructive button variant

--- apps/site/src/components/ui/button.tsx
+++ apps/site/src/components/ui/button.tsx
@@ -11,6 +11,8 @@ const buttonVariants = cva(
       variant: {
         default:
           "bg-primary text-primary-foreground shadow hover:bg-primary/90",
+        destructive:
+          "bg-destructive text-destructive-foreground shadow-sm hover:bg-destructive/90",
         outline:
           "border border-input bg-background shadow-sm hover:bg-accent hover:text-accent-foreground",
         secondary:
