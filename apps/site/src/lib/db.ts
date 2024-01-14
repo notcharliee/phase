53172b3b084961683fb1ef738df75d770f276c20
notcharliee <saunderscharlie@hotmail.com>
2024-01-14 18:42:10 +0000

    create(site): mongoose db function

---
+++ apps/site/src/lib/db.ts
@@ -0,0 +1,35 @@
+import mongoose from "mongoose"
+import { env } from "@/env"
+
+declare global {
+  var mongoose: {
+    conn: null | typeof import("mongoose"),
+    promise: null | Promise<typeof import("mongoose")>,
+  }
+}
+
+let cached = global.mongoose
+
+if (!cached) {
+  cached = global.mongoose = {
+    conn: null,
+    promise: null,
+  }
+}
+
+export async function dbConnect() {
+  if (cached.conn) return cached.conn
+  
+  if (!cached.promise) {
+    cached.promise = mongoose.connect(env.MONGODB_URI, { bufferCommands: false })
+  }
+
+  try {
+    cached.conn = await cached.promise
+  } catch (e) {
+    cached.promise = null
+    throw e
+  }
+
+  return cached.conn
+}
