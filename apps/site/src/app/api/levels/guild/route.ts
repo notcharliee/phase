9a8f65f9db8dc64ab0592997e6ddfcb07d55145f
notcharliee <saunderscharlie@hotmail.com>
2024-01-14 18:43:02 +0000

    modify(api): update to use new db function

--- apps/site/src/app/api/levels/guild/route.ts
+++ apps/site/src/app/api/levels/guild/route.ts
@@ -1,19 +1,24 @@
 import { NextResponse, NextRequest } from "next/server"
-import { LevelSchema } from "@repo/schemas"
+
 import { API } from "@discordjs/core/http-only"
 import { REST } from "@discordjs/rest"
+
+import { LevelSchema } from "@repo/schemas"
+import { dbConnect } from "@/lib/db"
+
 import { env } from "@/env"
-import mongoose from "mongoose"
+
 
 const discordREST = new REST().setToken(env.DISCORD_TOKEN)
 const discordAPI = new API(discordREST)
+
 
 export const GET = async (request: NextRequest) => {
   const rankStart = parseInt(request.nextUrl.searchParams.get("rankStart") || "1", 10)
   const rankEnd = parseInt(request.nextUrl.searchParams.get("rankEnd") || "1", 10)
   const guildId = request.nextUrl.searchParams.get("guild")
 
-  if (!guildId || rankStart > rankEnd) {
+  if (!guildId || rankStart < 0 || rankStart > rankEnd || rankEnd > 15) {
     return NextResponse.json({
       error: "Bad Request",
       documentation: `${env.NEXT_PUBLIC_BASE_URL}/docs/api/levels`,
@@ -21,16 +26,13 @@ export const GET = async (request: NextRequest) => {
   }
 
   try {
-    // Connect to database
-    await mongoose.connect(env.MONGODB_URI)
+    await dbConnect()
 
-    // Fetch users within the specified rank range
     const usersData = await LevelSchema.find({ guild: guildId })
       .sort({ level: -1, xp: -1 })
       .skip(rankStart - 1)
       .limit(rankEnd - rankStart + 1)
 
-    // Return 404 if no users found
     if (!usersData || !usersData.length) {
       return NextResponse.json({
         error: "Not Found",
@@ -38,8 +40,7 @@ export const GET = async (request: NextRequest) => {
       }, { status: 404 })
     }
 
-    // Respond with an array of user data, including level, XP, and rank
-    const responseData = await Promise.all(usersData.map(async (data, index) => {
+    return NextResponse.json(await Promise.all(usersData.map(async (data, index) => {
       const user = await discordAPI.users.get(data.user)
 
       return {
@@ -52,16 +53,8 @@ export const GET = async (request: NextRequest) => {
         rank: rankStart + index,
         target: 500 * (data.level + 1),
       }
-    }))
-
-    // Disconnect from database
-    await mongoose.disconnect()
-
-    return NextResponse.json(responseData)
+    })))
   } catch (error) {
-    // Disconnect from database
-    await mongoose.disconnect()
-
     console.log(error)
     return NextResponse.json(error, { status: 500 })
   }
--- apps/site/src/app/api/levels/user/route.ts
+++ apps/site/src/app/api/levels/user/route.ts
@@ -1,12 +1,17 @@
 import { NextResponse, NextRequest } from "next/server"
-import { LevelSchema } from "@repo/schemas"
+
 import { API } from "@discordjs/core/http-only"
 import { REST } from "@discordjs/rest"
+
+import { LevelSchema } from "@repo/schemas"
+import { dbConnect } from "@/lib/db"
+
 import { env } from "@/env"
-import mongoose from "mongoose"
+
 
 const discordREST = new REST().setToken(env.DISCORD_TOKEN)
 const discordAPI = new API(discordREST)
+
 
 export const GET = async (request: NextRequest) => {
   const userId = request.nextUrl.searchParams.get("user")
@@ -18,29 +23,21 @@ export const GET = async (request: NextRequest) => {
   }, { status: 400 })
 
   try {
-    // Connect to database
-    await mongoose.connect(env.MONGODB_URI)
+    await dbConnect()
 
-    // Get user and level data
     const user = await discordAPI.users.get(userId)
     const data = await LevelSchema.findOne({ user: userId, guild: guildId })
     
-    // Return 404 if data not found
     if (!data) return NextResponse.json({
       error: "Not Found",
       message: "Level data not found.",
     }, { status: 404 })
 
-    // Calculate user rank within the guild
     const rank = await LevelSchema.countDocuments({ $or: [
       { guild: guildId, level: { $gt: data.level } },
       { guild: guildId, level: data.level, xp: { $gt: data.xp } }
     ] }) + 1
 
-    // Disconnect from database
-    await mongoose.disconnect()
-
-    // Respond with user data, level, XP, and rank
     return NextResponse.json({
       id: user.id,
       username: user.username,
@@ -52,9 +49,6 @@ export const GET = async (request: NextRequest) => {
       target: 500 * (data.level + 1),
     })
   } catch (error) {
-    // Disconnect from database
-    await mongoose.disconnect()
-
     console.log(error)
     return NextResponse.json(error, { status: 500 })
   }
