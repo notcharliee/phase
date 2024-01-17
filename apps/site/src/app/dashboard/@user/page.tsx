65a485995d697da70f0e984c6f6c4f78c4febe8d
notcharliee <saunderscharlie@hotmail.com>
2024-01-17 02:54:13 +0000

    modify(dash): update to use `getUser()` function

--- apps/site/src/app/dashboard/@user/page.tsx
+++ apps/site/src/app/dashboard/@user/page.tsx
@@ -1,15 +1,12 @@
 import { headers } from "next/headers"
 import Link from "next/link"
 
-import { API } from "@discordjs/core/http-only"
 import { REST } from "@discordjs/rest"
 
+import { getUser } from "@/lib/auth"
 import { getInitials } from "@/lib/utils"
-import { env } from "@/lib/env"
 
-import {
-  ExitIcon,
-} from "@radix-ui/react-icons"
+import { ExitIcon } from "@radix-ui/react-icons"
 
 import {
   Avatar,
@@ -29,19 +26,16 @@ import {
 } from "@/components/ui/tooltip"
 
 
-const discordREST = new REST().setToken(env.DISCORD_TOKEN)
-const discordAPI = new API(discordREST)
-
-
 export default async () => {
   const userId = headers().get("x-user-id")!
-  const user = await discordAPI.users.get(userId)
+  const userToken = headers().get("x-user-token")!
+  const user = (await getUser(userId, userToken))!
 
   return (
     <Card>
       <CardContent className="flex items-center gap-4 pt-6">
         <Avatar>
-          <AvatarImage src={user.avatar ? discordREST.cdn.avatar(user.id, user.avatar) : undefined} />
+          <AvatarImage src={user.avatar ? new REST().cdn.avatar(user.id, user.avatar) : undefined} />
           <AvatarFallback>{getInitials(user.global_name ?? user.username)}</AvatarFallback>
         </Avatar>
         <div className="flex flex-col -space-y-2">
