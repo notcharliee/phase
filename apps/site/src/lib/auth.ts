0273dc6451bfc3fc34d6d1b9c6e50e73d3997926
notcharliee <saunderscharlie@hotmail.com>
2024-01-17 02:19:23 +0000

    create(site) `getUser()` function in auth lib

---
+++ apps/site/src/lib/auth.ts
@@ -0,0 +1,24 @@
+import { API } from "@discordjs/core/http-only"
+import { REST } from "@discordjs/rest"
+
+import { dbConnect } from "./db"
+
+
+/**
+ * Checks if the user id and user token are valid and returns a user object.
+ * 
+ * @param userId The user ID
+ * @param userToken The user access token
+ */
+export const getUser = async (userId: string, userToken: string) => {
+  await dbConnect()
+
+  const userREST = new REST({ authPrefix: "Bearer" }).setToken(userToken)
+  const userAPI = new API(userREST)
+
+  const user = await userAPI.users.getCurrent()
+
+  if (user.id !== userId) return null
+
+  return user
+}
