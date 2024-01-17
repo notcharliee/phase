86bb7144354e4c9a2817cbfbe30a413ad0e17877
notcharliee <saunderscharlie@hotmail.com>
2024-01-17 16:24:19 +0000

    modify(dash): wrap modules subroutes in `CardContent`

--- apps/site/src/app/dashboard/@modules/(group)/page.tsx
+++ apps/site/src/app/dashboard/@modules/(group)/page.tsx
@@ -16,17 +16,19 @@ import modules from "@/lib/modules"
 
 
 export default () => (
-  <>{modules.map((module, index) => (
-    <Card key={index} className={"flex flex-col justify-between " + (!cookies().get("guild")?.value ? "pointer-events-none opacity-50" : "")}>
-      <CardHeader>
-        <CardTitle>{module.name}</CardTitle>
-        <CardDescription>{module.description}</CardDescription>
-      </CardHeader>
-      <CardFooter>
-        <Link href={"/dashboard/modules/" + module.name.replaceAll(" ","-").toLowerCase()} className="w-full">
-          <Button variant={"secondary"} className="w-full">Configure</Button>
-        </Link>
-      </CardFooter>
-    </Card>
-  ))}</>
+  <CardContent className="pt-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+    {modules.map((module, index) => (
+      <Card key={index} className={"flex flex-col justify-between " + (!cookies().get("guild")?.value ? "pointer-events-none opacity-50" : "")}>
+        <CardHeader>
+          <CardTitle>{module.name}</CardTitle>
+          <CardDescription>{module.description}</CardDescription>
+        </CardHeader>
+        <CardFooter>
+          <Link href={"/dashboard/modules/" + module.name.replaceAll(" ","-").toLowerCase()} className="w-full">
+            <Button variant={"secondary"} className="w-full">Configure</Button>
+          </Link>
+        </CardFooter>
+      </Card>
+    ))}
+  </CardContent>
 )
