d5b2bd941c658fb02e160eee797c5d7496a5f647
notcharliee <saunderscharlie@hotmail.com>
2024-01-19 15:57:26 +0000

    create(site): actions.ts lib file

---
+++ apps/site/src/lib/actions.ts
@@ -0,0 +1,78 @@
+"use server"
+
+import { redirect } from "next/navigation"
+import { cookies, headers } from "next/headers"
+import { kv } from "@vercel/kv"
+
+import { API } from "@discordjs/core/http-only"
+import { REST } from "@discordjs/rest"
+
+import { StatusCodes } from "http-status-codes"
+
+import {
+  GuildSchema,
+  type GuildModuleAuditLogs,
+} from "@repo/schemas"
+
+import { dbConnect } from "@/lib/db"
+import { getUser } from "@/lib/auth"
+import { env } from "@/lib/env"
+
+
+const discordREST = new REST().setToken(env.DISCORD_TOKEN)
+const discordAPI = new API(discordREST)
+
+
+export const deleteAccount = async () => {
+  const sessionId = headers().get("x-user-session")
+  const key = "auth:" + sessionId
+
+  await kv.del(key)
+  
+  cookies().delete("session")
+
+  redirect("/")
+}
+
+
+export const updateAuditLogsModule = async (data: GuildModuleAuditLogs) => {
+  const promise = new Promise(resolve => {
+    setTimeout(() => {
+      console.log(data)
+      resolve(data)
+    }, 3000)
+  })
+
+  return await promise
+}
+
+
+export const updateBotNickname = async (nickname: string) => {
+  await dbConnect()
+
+  const guildId = cookies().get("guild")?.value
+  const userId = headers().get("x-user-id")
+  const userToken = headers().get("x-user-token")
+
+  if (!guildId || !userId || !userToken) return StatusCodes.BAD_REQUEST
+
+  // Checks if user id and token are valid
+  const validUser = await getUser(userId, userToken)
+  if (!validUser) return StatusCodes.UNAUTHORIZED
+
+  // Checks if user is a dashboard admin for the guild
+  const userIsAdmin = !!(await GuildSchema.findOne({ id: guildId, admins: { $in: userId } }))
+  if (!userIsAdmin) return StatusCodes.UNAUTHORIZED
+
+  await discordAPI.users.editCurrentGuildMember(guildId, {
+    nick: nickname
+  })
+
+  return StatusCodes.OK
+}
+
+
+export const setGuildCookie = async (guild: string) => {
+  "use server"
+  cookies().set("guild", guild)
+}
