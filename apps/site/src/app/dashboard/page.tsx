6903c54e691718bff3727a7c6fbd609dbd588f68
notcharliee <saunderscharlie@hotmail.com>
2024-01-19 18:30:37 +0000

    modify(dash): module card stuff

--- apps/site/src/app/dashboard/page.tsx
+++ apps/site/src/app/dashboard/page.tsx
@@ -1,7 +1,8 @@
-import { cookies } from "next/headers"
-import Link from "next/link"
+import { readdirSync } from "fs"
 
-import modules from "@/lib/modules"
+import { cookies } from "next/headers"
+import { type Metadata } from "next"
+import Link from "next/link"
 
 import { Button } from "@/components/ui/button"
 
@@ -14,20 +15,28 @@ import {
 } from "@/components/ui/card"
 
 
-export default () => (
-  <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
-    {modules.map((module, index) => (
-      <Card key={index} className={"flex flex-col justify-between " + (!cookies().get("guild")?.value ? "pointer-events-none opacity-50" : "")}>
-        <CardHeader>
-          <CardTitle>{module.name}</CardTitle>
-          <CardDescription>{module.description}</CardDescription>
-        </CardHeader>
-        <CardFooter>
-          <Link href={"/dashboard/modules/" + module.name.replaceAll(" ","-").toLowerCase()} className="w-full">
-            <Button variant={"secondary"} className="w-full">Configure</Button>
-          </Link>
-        </CardFooter>
-      </Card>
-    ))}
-  </div>
-)
+export default async () => {
+  const moduleDirs = readdirSync("./src/app/dashboard/modules")
+  const moduleData = await Promise.all(moduleDirs.map(async (moduleDir) => ({
+    ...(await import(`./modules/${moduleDir}/page`)).metadata as Metadata,
+    url: `/dashboard/modules/${moduleDir}`,
+  })))
+  
+  return (
+    <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3 gap-3">
+      {moduleData.map((module, index) => (
+        <Card key={index} className={"flex flex-col justify-between " + (!cookies().get("guild")?.value ? "pointer-events-none opacity-50" : "")}>
+          <CardHeader>
+            <CardTitle>{module.title?.toString().replace(" - Phase Bot", "")}</CardTitle>
+            <CardDescription>{module.description}</CardDescription>
+          </CardHeader>
+          <CardFooter>
+            <Link href={module.url} className="w-full">
+              <Button variant={"secondary"} className="w-full">Configure</Button>
+            </Link>
+          </CardFooter>
+        </Card>
+      ))}
+    </div>
+  )
+}
