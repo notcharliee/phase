7546493119b42cb6f38b79d387b447393e67e7bc
notcharliee <saunderscharlie@hotmail.com>
2024-01-21 15:38:35 +0000

    create(dash): jtc module metadata export

--- apps/site/src/app/dashboard/modules/index.ts
+++ apps/site/src/app/dashboard/modules/index.ts
@@ -1,3 +1,2 @@
-export {
-  metadata as audit_logs,
-} from "./audit-logs/page"
+export { metadata as audit_logs } from "./audit-logs/page"
+export { metadata as join_to_create } from "./join-to-create/page"
