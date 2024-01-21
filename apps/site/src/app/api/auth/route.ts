03760f35f1c72ed1bc0bff35fd6d90b9ccf7982c
notcharliee <saunderscharlie@hotmail.com>
2024-01-21 15:45:46 +0000

    fix(api): oauth2 prompt

--- apps/site/src/app/api/auth/route.ts
+++ apps/site/src/app/api/auth/route.ts
@@ -16,7 +16,7 @@ export const GET = (request: NextRequest) => {
     redirect_uri: env.NEXT_PUBLIC_BASE_URL + "/api/auth/callback",
     response_type: "code",
     scope: "identify guilds",
-    prompt: "none",
+    prompt: "consent",
   })
 
   return NextResponse.redirect(authorizationURL)
