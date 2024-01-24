cbfe4e7ac16ac18785407920a1d3ac9adcaf76a3
notcharliee <saunderscharlie@hotmail.com>
2024-01-24 14:45:57 +0000

    modify(site): remove cookie log from middleware

--- apps/site/src/middleware.ts
+++ apps/site/src/middleware.ts
@@ -6,9 +6,6 @@ import type { User } from "@/lib/types"
 
 export async function middleware(request: NextRequest) {
   const userCookie = request.cookies.get("user")
-
-  console.log(request.cookies.getAll())
-
   const user = userCookie ? await kv.get(`user:${userCookie.value}`) as User | null : null
 
   if (!user || (user.expires_timestamp < (Date.now() + (1000 * 60 * 15)) / 1000))
