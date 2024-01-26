4199001cdd3b3a27f86215696eee559e0eadffc2
notcharliee <saunderscharlie@hotmail.com>
2024-01-26 09:17:14 +0000

    modify(site): homepage styles

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -7,10 +7,10 @@ import { Stars } from "@/components/stars"
 
 export default () => (
   <main className="min-h-dvh grid text-light-900">
-    <Stars className="place-self-center fixed -z-10 overflow-hidden scale-50 rotate-90 sm:scale-[.6] sm:rotate-0 md:scale-75" />
-    <div className="place-self-center w-full flex flex-col items-center">
+    <Stars className="place-self-center fixed overflow-hidden scale-50 rotate-90 sm:scale-[.6] sm:rotate-0 md:scale-75" />
+    <div className="relative z-10 place-self-center w-full flex flex-col items-center">
       <div className="relative flex justify-center items-center w-full">
-        <h1 className="animate-[scale-up_1s_ease-out_forwards] shadow-foreground/30 text-shadow-glow text-6xl sm:text-7xl md:text-8xl max-w-[500px] sm:max-w-[600px] md:max-w-[700px] font-black tracking-tighter text-center absolute z-10">The all in one Discord Bot</h1>
+        <h1 className="animate-[scale-up_1s_ease-out_forwards] shadow-foreground/30 text-shadow-glow text-6xl sm:text-7xl md:text-8xl max-w-[500px] sm:max-w-[600px] md:max-w-[700px] font-black tracking-tighter text-center absolute z-20">The all in one Discord Bot</h1>
         <Moon className="w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 animate-[moon-rotate_1s_ease-out_forwards]" />
       </div>
       <div className="animate-[scale-up_1s_ease-out_forwards] opacity-0 animation-delay-500 flex flex-col sm:flex-row gap-6 shadow-foreground/30 mt-20">
@@ -20,7 +20,7 @@ export default () => (
           </Button>
         </Link>
         <Link href={"/docs"}>
-          <Button variant={"outline"} className="shadow-glow border-2 border-light-800" size={"xl"}>
+          <Button variant={"outline"} size={"xl"}>
             Learn More
           </Button>
         </Link>
