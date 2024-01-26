728a38049115e00b5101902decc1ce122a39626c
notcharliee <saunderscharlie@hotmail.com>
2024-01-26 09:14:33 +0000

    create(dash): `/modules` layout & route

---
+++ apps/site/src/app/dashboard/modules/layout.tsx
@@ -0,0 +1,26 @@
+import { Separator } from "@/components/ui/separator"
+import { SidebarNav } from "../components/sidebar-nav"
+
+import { dashboardConfig } from "@/config/dashboard"
+
+
+const sidebarNavItems = dashboardConfig.sidebarNav[0]!.items.map(item => ({ title: item.title, href: item.href! }))
+
+
+export default ({ children }: { children: React.ReactNode }) => (
+  <div>
+    <div className="space-y-0.5">
+      <h2 className="text-2xl font-bold tracking-tight">Modules</h2>
+      <p className="text-muted-foreground">
+        Manage your server's module settings.
+      </p>
+    </div>
+    <Separator className="my-6" />
+    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+      <aside className="lg:w-1/5">
+        <SidebarNav items={sidebarNavItems} />
+      </aside>
+      <div className="flex-1 lg:max-w-2xl xl:max-w-4xl">{children}</div>
+    </div>
+  </div>
+)
