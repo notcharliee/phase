172491c65e45ff6ba20d0964b45f5c0b46833bea
notcharliee <saunderscharlie@hotmail.com>
2024-01-27 17:13:36 +0000

    create(site): bot & commands response type exports

--- apps/site/src/types/api.ts
+++ apps/site/src/types/api.ts
@@ -4,3 +4,7 @@ import { NextResponse } from "next/server"
 export type ExtractResponseType<T> = T extends NextResponse<infer R> ? R : never
 
 export type ExtractAPIType<T extends (params: any) => Promise<NextResponse<any>>> = ExtractResponseType<Awaited<ReturnType<T>>>
+
+
+export type { GetBotResponse } from "@/app/api/bot/route"
+export type { GetBotCommandsResponse } from "@/app/api/bot/commands/route"
