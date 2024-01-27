ed6a44a54cb00d46d75ec2f6fe1052805bdcd399
notcharliee <saunderscharlie@hotmail.com>
2024-01-27 12:07:51 +0000

    modify(docs): code formatting

--- apps/site/src/app/docs/layout.tsx
+++ apps/site/src/app/docs/layout.tsx
@@ -12,27 +12,24 @@ import { PathParts } from "./components/path-parts"
 import { DocsSidebarNav } from "./components/sidebar-nav"
 
 
-export default ({ children }: { children: React.ReactNode }) => {
-
-  return (
-    <div className="w-full max-w-[1400px] min-h-screen mx-auto flex gap-12 px-8">
-      <aside className="w-64 h-screen py-6 lg:py-8 pr-6 sticky top-0 hidden md:block">
-        <DocsSidebarNav items={docsConfig.sidebarNav} />
-      </aside>
-      <main className="py-8 w-full">
-        <div className="mb-4 flex items-center space-x-1 text-muted-foreground text-sm">
-          <Sheet>
-            <SheetTrigger>
-              <HamburgerMenuIcon className="mr-2 h-4 w-4 md:hidden block" />
-            </SheetTrigger>
-            <SheetContent side={"left"}>
-              <DocsSidebarNav items={docsConfig.sidebarNav} />
-            </SheetContent>
-          </Sheet>
-          <PathParts />
-        </div>
-        {children}
-      </main>
-    </div>
-  )
-}
+export default ({ children }: { children: React.ReactNode }) => (
+  <div className="w-full max-w-[1400px] min-h-screen mx-auto flex gap-12 px-8">
+    <aside className="w-64 h-screen py-6 lg:py-8 pr-6 sticky top-0 hidden md:block">
+      <DocsSidebarNav items={docsConfig.sidebarNav} />
+    </aside>
+    <main className="py-8 w-full">
+      <div className="mb-4 flex items-center space-x-1 text-muted-foreground text-sm">
+        <Sheet>
+          <SheetTrigger>
+            <HamburgerMenuIcon className="mr-2 h-4 w-4 md:hidden block" />
+          </SheetTrigger>
+          <SheetContent side={"left"}>
+            <DocsSidebarNav items={docsConfig.sidebarNav} />
+          </SheetContent>
+        </Sheet>
+        <PathParts />
+      </div>
+      {children}
+    </main>
+  </div>
+)
