a747131a5653c3566566c4f8a954fcc90f4fc34a
notcharliee <saunderscharlie@hotmail.com>
2024-01-27 18:05:27 +0000

    modify(site): add 'dev mode' to middleware

--- apps/site/src/middleware.ts
+++ apps/site/src/middleware.ts
@@ -1,17 +1,36 @@
 import { NextResponse, NextRequest } from "next/server"
 import { kv } from "@vercel/kv"
 
+import { env } from "@/lib/env"
 import type { User } from "@/types/auth"
 
 
+const devMode = !!(
+  env.NODE_ENV == "development" &&
+  env.NEXT_MIDDLEWARE_AUTH_ID &&
+  env.NEXT_MIDDLEWARE_USER_ID &&
+  env.NEXT_MIDDLEWARE_USER_TOKEN
+)
+
+
 export async function middleware(request: NextRequest) {
+  const headers = new Headers(request.headers)
+
+  if (devMode) {
+    headers.set("x-auth-id", env.NEXT_MIDDLEWARE_AUTH_ID!)
+    headers.set("x-user-id", env.NEXT_MIDDLEWARE_USER_ID!)
+    headers.set("x-user-token", env.NEXT_MIDDLEWARE_USER_TOKEN!)
+
+    return NextResponse.next({
+      request: { headers },
+    })
+  }
+
   const userCookie = request.cookies.get("user")
   const user = userCookie ? await kv.get(`user:${userCookie.value}`) as User | null : null
 
   if (!user || (user.expires_timestamp < (Date.now() + (1000 * 60 * 15)) / 1000))
     return NextResponse.redirect(new URL("/api/auth", request.url))
-
-  const headers = new Headers(request.headers)
   
   headers.set("x-auth-id", user.id)
   headers.set("x-user-id", user.discord_id)
