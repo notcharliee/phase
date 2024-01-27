39ccf11eeb9bc8568715c981f1d0b7080d12a93e
notcharliee <saunderscharlie@hotmail.com>
2024-01-27 17:09:51 +0000

    fix(api): `/bot/commands` response

--- apps/site/src/app/api/bot/commands/route.ts
+++ apps/site/src/app/api/bot/commands/route.ts
@@ -1,55 +1,54 @@
-import { NextResponse, NextRequest } from "next/server"
+import { NextResponse } from "next/server"
+import { AppConfigDynamic } from "next/dist/build/utils"
+
 import { API } from "@discordjs/core/http-only"
 import { REST } from "@discordjs/rest"
+
+import {
+  ApplicationCommandOptionType,
+  type APIApplicationCommandSubcommandOption,
+} from "discord-api-types/v10"
+
 import { env } from "@/lib/env"
-import discord_api_types_v10 from "discord-api-types/v10"
+import type { ExtractAPIType } from "@/types/api"
 
 
-export const GET = async (request: NextRequest) => {
-  const discordREST = new REST().setToken(env.DISCORD_TOKEN!)
-  const discordAPI = new API(discordREST)
+const discordREST = new REST().setToken(env.DISCORD_TOKEN)
+const discordAPI = new API(discordREST)
 
-  const globalCommands = await discordAPI.applicationCommands.getGlobalCommands(
-    env.DISCORD_ID,
-  )
 
-  const commandArray: {
-    name: string
-    description: string
-    default_member_permissions: string | null
-    dm_permission: boolean | undefined
-    options: discord_api_types_v10.APIApplicationCommandOption[] | undefined
-    nsfw: boolean | undefined
-  }[] = []
+export const dynamic: AppConfigDynamic = "force-dynamic"
 
-  for (const globalCommand of globalCommands) {
 
-    if(globalCommand.name != globalCommand.description) commandArray.push({
-      name: globalCommand.name,
-      description: globalCommand.description,
-      default_member_permissions: globalCommand.default_member_permissions,
-      dm_permission: globalCommand.dm_permission,
-      options: globalCommand.options?.filter((option) => option.type !== discord_api_types_v10.ApplicationCommandOptionType.Subcommand),
-      nsfw: globalCommand.nsfw,
-    })
+export const GET = async () => {
+  const commands = await discordAPI.applicationCommands.getGlobalCommands(env.DISCORD_ID)
 
-    if (globalCommand.options)
-      for (const globalCommandOption of globalCommand.options) {
-        if (
-          globalCommandOption.type ==
-          discord_api_types_v10.ApplicationCommandOptionType.Subcommand
-        )
-          commandArray.push({
-            name: `${globalCommand.name} ${globalCommandOption.name}`,
-            description: globalCommandOption.description,
-            default_member_permissions:
-              globalCommand.default_member_permissions,
-            dm_permission: globalCommand.dm_permission,
-            options: globalCommandOption.options,
-            nsfw: globalCommand.nsfw,
-          })
-      }
-  }
+  const response = commands.flatMap(command => {
+    if (command.name !== command.description) return {
+      name: command.name,
+      description: command.description,
+      default_member_permissions: command.default_member_permissions,
+      dm_permission: command.dm_permission,
+      options: command.options?.filter((option) => option.type !== ApplicationCommandOptionType.Subcommand),
+      nsfw: command.nsfw,
+    }
 
-  return NextResponse.json(commandArray)
+    if (command.options) return command.options
+      .filter(option => option.type === ApplicationCommandOptionType.Subcommand)
+      .map((subcommand) => ({
+        name: `${command.name} ${subcommand.name}`,
+        description: subcommand.description,
+        default_member_permissions: command.default_member_permissions,
+        dm_permission: command.dm_permission,
+        options: (subcommand as APIApplicationCommandSubcommandOption).options,
+        nsfw: command.nsfw,
+      }))
+
+    return []
+  })
+
+  return NextResponse.json(response)
 }
+
+
+export type GetBotCommandsResponse = ExtractAPIType<typeof GET>
