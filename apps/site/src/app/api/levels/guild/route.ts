a09234f2d01562ced7f3ef1481c73d27cea0e683
notcharliee <saunderscharlie@hotmail.com>
2024-01-27 17:42:19 +0000

    modify(api): levels guild route

--- apps/site/src/app/api/levels/guild/route.ts
+++ apps/site/src/app/api/levels/guild/route.ts
@@ -6,7 +6,9 @@ import { REST } from "@discordjs/rest"
 import { LevelSchema } from "@repo/schemas"
 
 import { dbConnect } from "@/lib/db"
+import { absoluteURL } from "@/lib/utils"
 import { env } from "@/lib/env"
+import { type ExtractAPIType } from "@/types/api"
 
 
 const discordREST = new REST().setToken(env.DISCORD_TOKEN)
@@ -18,44 +20,44 @@ export const GET = async (request: NextRequest) => {
   const rankEnd = parseInt(request.nextUrl.searchParams.get("rankEnd") || "1", 10)
   const guildId = request.nextUrl.searchParams.get("guild")
 
-  if (!guildId || rankStart < 0 || rankStart > rankEnd || rankEnd > 15) {
+  if (!guildId || rankStart <= 0 || rankStart > rankEnd || rankEnd > 15) {
     return NextResponse.json({
       error: "Bad Request",
-      documentation: `${env.NEXT_PUBLIC_BASE_URL}/docs/api/levels`,
+      documentation: absoluteURL("/docs/api/levels"),
     }, { status: 400 })
   }
 
-  try {
-    await dbConnect()
+  await dbConnect()
 
-    const usersData = await LevelSchema.find({ guild: guildId })
-      .sort({ level: -1, xp: -1 })
-      .skip(rankStart - 1)
-      .limit(rankEnd - rankStart + 1)
+  const usersData = await LevelSchema.find({ guild: guildId })
+    .sort({ level: -1, xp: -1 })
+    .skip(rankStart - 1)
+    .limit(rankEnd - rankStart + 1)
 
-    if (!usersData || !usersData.length) {
-      return NextResponse.json({
-        error: "Not Found",
-        message: "No users found within the specified rank range.",
-      }, { status: 404 })
-    }
-
-    return NextResponse.json(await Promise.all(usersData.map(async (data, index) => {
-      const user = await discordAPI.users.get(data.user)
-
-      return {
-        id: user.id,
-        username: user.username,
-        global_name: user.global_name,
-        avatar: user.avatar ? discordREST.cdn.avatar(user.id, user.avatar, { size: 128, forceStatic: true, extension: "png" }) : `${env.NEXT_PUBLIC_BASE_URL}/discord.png`,
-        level: data.level,
-        xp: data.xp,
-        rank: rankStart + index,
-        target: 500 * (data.level + 1),
-      }
-    })))
-  } catch (error) {
-    console.log(error)
-    return NextResponse.json(error, { status: 500 })
+  if (!usersData || !usersData.length) {
+    return NextResponse.json({
+      error: "Not Found",
+      message: "No users found within the specified rank range.",
+    }, { status: 404 })
   }
+
+  const response = usersData.map(async (data, index) => {
+    const user = await discordAPI.users.get(data.user)
+
+    return {
+      id: user.id,
+      username: user.username,
+      global_name: user.global_name,
+      avatar: user.avatar ? discordREST.cdn.avatar(user.id, user.avatar, { size: 128, forceStatic: true, extension: "png" }) : `${env.NEXT_PUBLIC_BASE_URL}/discord.png`,
+      level: data.level,
+      xp: data.xp,
+      rank: rankStart + index,
+      target: 500 * (data.level + 1),
+    }
+  })
+
+  return NextResponse.json(await Promise.all(response))
 }
+
+
+export type GetLevelsGuildResponse = ExtractAPIType<typeof GET>
--- apps/site/src/types/api.ts
+++ apps/site/src/types/api.ts
@@ -3,8 +3,11 @@ import { NextResponse } from "next/server"
 
 export type ExtractResponseType<T> = T extends NextResponse<infer R> ? R : never
 
-export type ExtractAPIType<T extends (params: any) => Promise<NextResponse<any>>> = ExtractResponseType<Awaited<ReturnType<T>>>
+export type ExtractAPIType<T extends (request: any) => Promise<NextResponse<any>>> = 
+  T extends (request: any) => Promise<NextResponse<infer R>> ? Exclude<R, { error: any }> : never;
 
 
 export type { GetBotResponse } from "@/app/api/bot/route"
 export type { GetBotCommandsResponse } from "@/app/api/bot/commands/route"
+
+export type { GetLevelsGuildResponse } from "@/app/api/levels/guild/route"
