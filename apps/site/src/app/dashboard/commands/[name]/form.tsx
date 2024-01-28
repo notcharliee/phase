b85670053273644d44c65af9f3b7c1a2b4e9c3ba
notcharliee <saunderscharlie@hotmail.com>
2024-01-28 17:39:40 +0000

    modify(dash): commands config metadata, loading & form

--- apps/site/src/app/dashboard/commands/[name]/form.tsx
+++ apps/site/src/app/dashboard/commands/[name]/form.tsx
@@ -1,11 +1,10 @@
 "use client"
 
-import { type APIRole } from "discord-api-types/v10"
-
+import { useForm } from "react-hook-form"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { z } from "zod"
 
-import { useForm } from "react-hook-form"
+import { type APIRole } from "discord-api-types/v10"
 
 import { Button } from "@/components/ui/button"
 import {
@@ -18,12 +17,11 @@ import {
   FormMessage,
 } from "@/components/ui/form"
 import { Label } from "@/components/ui/label"
+import { toast } from "sonner"
 import {
   RoleSelect,
   SelectFallback,
 } from "@/components/dashboard/modules/select"
-
-import { toast } from "sonner"
 
 import { type Command } from "@/types/commands"
 import { updateCommand } from "@/lib/actions"
@@ -36,27 +34,57 @@ const formSchema = z.object({
 type FormValues = z.infer<typeof formSchema>
 
 
-export const CommandForm = (props: {
-  defaultValues: FormValues,
-  data: {
-    command: Command,
-    roles: APIRole[],
-  },
-}) => {
+type CommandFormProps<T extends boolean> = T extends true
+  ? {
+    fallback: T,
+    defaultValues?: FormValues,
+    data?: {
+      command: Command,
+      roles: APIRole[],
+    }
+  } : {
+    fallback?: T,
+    defaultValues: FormValues,
+    data: {
+      command: Command,
+      roles: APIRole[],
+    }
+  }
+
+export const CommandForm = <T extends boolean> (props: CommandFormProps<T>) => {
+  if (props.fallback) return (
+    <div className="space-y-8">
+      <div className="space-y-2">
+        <Label>Required Role</Label>
+        <SelectFallback />
+        <p className="text-sm text-muted-foreground">
+          Users will require this role to run this command.
+        </p>
+      </div>
+      <Button>Update command</Button>
+    </div>
+  )
+
+
   const form = useForm<FormValues>({
     resolver: zodResolver(formSchema),
     defaultValues: props.defaultValues,
   })
 
+
   const onSubmit = (data: FormValues) => {
-    toast.promise(updateCommand(props.data.command.name, { name: props.data.command.name, permissions: data.role ?? "" }), {
-      loading: "Saving changes...",
-      success: "Changes saved!",
-      error: "An error occured.",
-    })
+    toast.promise(
+      updateCommand(props.data.command.name, {
+        name: props.data.command.name,
+        permissions: data.role ?? "",
+      }), {
+        loading: "Saving changes...",
+        success: "Changes saved!",
+        error: "An error occured.",
+      }
+    )
   }
 
-  const roles = props.data.roles
 
   return (
     <Form {...form}>
@@ -68,9 +96,11 @@ export const CommandForm = (props: {
             <FormItem>
               <FormLabel>Required Role</FormLabel>
               <FormControl>
-                <RoleSelect roles={roles} field={field} />
+                <RoleSelect roles={props.data.roles} field={field} />
               </FormControl>
-              <FormDescription>Users will require this role to run this command.</FormDescription>
+              <FormDescription>
+                Users will require this role to run this command.
+              </FormDescription>
               <FormMessage />
             </FormItem>
           )}
@@ -80,14 +110,3 @@ export const CommandForm = (props: {
     </Form>
   )
 }
-
-
-export const CommandFormFallback = () => (
-  <div className="space-y-8">
-    <div className="space-y-2">
-      <Label>Required Role</Label>
-      <SelectFallback />
-    </div>
-    <Button>Update command</Button>
-  </div>
-)
--- apps/site/src/app/dashboard/commands/[name]/page.tsx
+++ apps/site/src/app/dashboard/commands/[name]/page.tsx
@@ -1,18 +1,24 @@
+import { type Metadata } from "next"
 import { cookies, headers } from "next/headers"
+
+import { Suspense } from "react"
 
 import { REST } from "@discordjs/rest"
 import { API } from "@discordjs/core/http-only"
-
-import axios from "axios"
 
 import { GuildSchema } from "@repo/schemas"
 
 import { Separator } from "@/components/ui/separator"
 import { CommandForm } from "./form"
 
-import type { Command } from "@/types/commands"
+import {
+  GET as getCommands,
+  type GetBotCommandsResponse,
+} from "@/app/api/bot/commands/route"
 
+import { siteConfig } from "@/config/site"
 import { dbConnect } from "@/lib/db"
+import { absoluteURL } from "@/lib/utils"
 import { env } from "@/lib/env"
 
 
@@ -20,11 +26,60 @@ const discordREST = new REST().setToken(env.DISCORD_TOKEN)
 const discordAPI = new API(discordREST)
 
 
-export default async ({ params }: { params: { name: string } }) => {
+export async function generateMetadata({ params }: { params: { name: string } }): Promise<Metadata> {
+  const commandName = params.name
+
+  return {
+    title: `/${commandName}`,
+    description: `Dashboard config for the /${commandName} command.`,
+    openGraph: {
+      title: `/${commandName}`,
+      description: `Dashboard config for the /${params} command.`,
+      type: "website",
+      url: absoluteURL("/dashboard/commands/" + commandName),
+      images: [
+        {
+          url: siteConfig.ogImage,
+          width: 1200,
+          height: 630,
+          alt: siteConfig.name,
+        },
+      ],
+    },
+    twitter: {
+      card: "summary_large_image",
+      title: `/${commandName}`,
+      description: `Dashboard config for the /${commandName} command.`,
+      images: [siteConfig.ogImage],
+      creator: "@" + siteConfig.creator,
+    },
+  }
+}
+
+
+export default ({ params }: { params: { name: string } }) => (
+  <Suspense fallback={<Form command={params.name} fallback />}>
+    <Form command={params.name} />
+  </Suspense>
+)
+
+
+const Form = async (props: { fallback?: boolean, command: string }) => {
+  if (props.fallback) return (
+    <div className="space-y-6">
+      <div>
+        <h3 className="text-lg font-medium">{`/${props.command.replaceAll("-", " ")}`}</h3>
+        <p className="text-sm text-muted-foreground">Loading...</p>
+      </div>
+      <Separator />
+      <CommandForm fallback />
+    </div>
+  )
+
   await dbConnect()
 
-  const commands = (await axios.get<Command[]>(env.NEXT_PUBLIC_BASE_URL + "/api/bot/commands")).data
-  const command = commands.find(command => command.name == params.name.replaceAll("-", " "))
+  const commands: GetBotCommandsResponse = await (await getCommands()).json().then(json => json)
+  const command = commands.find(command => command.name == props.command.replaceAll("-", " "))
 
   const guildId = cookies().get("guild")?.value!
   const userId = headers().get("x-user-id")!
@@ -37,18 +92,13 @@ export default async ({ params }: { params: { name: string } }) => {
   return (
     <div className="space-y-6">
       <div>
-        <h3 className="text-lg font-medium">/{command.name}</h3>
+        <h3 className="text-lg font-medium">{`/${command.name.replaceAll("-", " ")}`}</h3>
         <p className="text-sm text-muted-foreground">{command.description}</p>
       </div>
       <Separator />
       <CommandForm
-        data={{
-          command,
-          roles,
-        }}
-        defaultValues={{
-          role: guild.commands ? guild.commands[command.name]?.permissions ?? "" : "",
-        }}
+        data={{ command, roles }}
+        defaultValues={{ role: guild.commands ? guild.commands[command.name]?.permissions ?? "" : "" }}
       />
     </div>
   )
