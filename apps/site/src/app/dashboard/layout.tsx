c4d22a71b759ef4907f4c50a5f43b11ad2e3a356
notcharliee <saunderscharlie@hotmail.com>
2024-01-28 17:41:04 +0000

    modify(dash): main-nav items & select-server combobox

--- apps/site/src/app/dashboard/layout.tsx
+++ apps/site/src/app/dashboard/layout.tsx
@@ -1,25 +1,20 @@
-import { cookies } from "next/headers"
+import { cookies, headers } from "next/headers"
+
+import { Suspense } from "react"
+
+import { REST } from "@discordjs/rest"
+import { API } from "@discordjs/core/http-only"
+
+import { GuildSchema } from "@repo/schemas"
 
 import { MainNav } from "./components/main-nav"
 import { UserNav } from "./components/user-nav"
 import { SearchDashboard } from "./components/search-dashboard"
-import { SelectGuild } from "./components/select-guild"
+import { SelectServer } from "./components/select-server"
 
-
-const mainNavItems = [
-  {
-    title: "Overview",
-    href: "/dashboard",
-  },
-  {
-    title: "Modules",
-    href: "/dashboard/modules",
-  },
-  {
-    title: "Commands",
-    href: "/dashboard/commands",
-  },
-]
+import { dbConnect } from "@/lib/db"
+import { absoluteURL } from "@/lib/utils"
+import { dashboardConfig } from "@/config/dashboard"
 
 
 export default ({ children }: { children: React.ReactNode }) => {
@@ -29,11 +24,15 @@ export default ({ children }: { children: React.ReactNode }) => {
     <main className="w-full min-h-screen flex flex-col">
       <header className="border-b z-50 sticky top-0 backdrop-blur-sm">
         <div className="flex h-16 items-center px-4">
-          <SelectGuild />
-          <MainNav items={mainNavItems} className="mx-6" />
+          <Suspense fallback={<ServerSelectCombobox fallback />}>
+            <ServerSelectCombobox />
+          </Suspense>
+          <MainNav items={dashboardConfig.mainNav} className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
             <SearchDashboard />
-            <UserNav />
+            <Suspense fallback={<UserNav fallback />}>
+              <UserNav />
+            </Suspense>
           </div>
         </div>
       </header>
@@ -44,4 +43,40 @@ export default ({ children }: { children: React.ReactNode }) => {
       </div>
     </main>
   )
+}
+
+
+const ServerSelectCombobox = async (props: { fallback?: boolean }) => {
+  if (props.fallback) return <SelectServer fallback/>
+
+  await dbConnect()
+
+  const user = {
+    id: headers().get("x-user-id")!,
+    token: headers().get("x-user-token")!,
+  }
+
+  const userREST = new REST({ authPrefix: "Bearer" }).setToken(user.token)
+  const userAPI = new API(userREST)
+
+  const databaseGuilds = await GuildSchema.find({ admins: user.id })
+  const discordGuilds = await userAPI.users.getGuilds()
+
+  const guilds = discordGuilds.toSorted((a, b) => {
+    const aIsInDocuments = databaseGuilds.some(doc => doc.id == a.id)
+    const bIsInDocuments = databaseGuilds.some(doc => doc.id == b.id)
+
+    if (aIsInDocuments && !bIsInDocuments) return -1
+    else if (!aIsInDocuments && bIsInDocuments) return 1
+    else return a.name.toLowerCase().localeCompare(b.name.toLowerCase())
+  }).map((guild, index) => ({
+    id: guild.id,
+    name: guild.name,
+    icon: guild.icon ? userREST.cdn.icon(guild.id, guild.icon) : absoluteURL("/discord.png"),
+    disabled: !!(index >= databaseGuilds.length)
+  }))
+
+  const defaultValue = databaseGuilds.find(guild => guild.id === cookies().get("guild")?.value)
+
+  return <SelectServer guilds={guilds} defaultValue={defaultValue?.id ?? ""} />
 }
