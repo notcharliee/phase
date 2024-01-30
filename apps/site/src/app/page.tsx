8f192d4076e24df836c063b7228212cf497ba8d9
notcharliee <saunderscharlie@hotmail.com>
2024-01-30 22:40:33 +0000

    modify(site): rework homepage

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -1,30 +1,38 @@
+"use client"
+
 import Link from "next/link"
 
-import { Button } from "@/components/ui/button"
-import { Moon } from "@/components/moon"
-import { Stars } from "@/components/stars"
+import { Announcement } from "@/components/announcement"
+import { buttonVariants } from "@/components/ui/button"
+import {
+  PageHeading,
+  PageDescription,
+  PageActions,
+} from "@/components/page-heading"
+
+import { useMediaQuery } from "@/hooks/use-media-query"
+
+import { siteConfig } from "@/config/site"
 
 
-export default () => (
-  <main className="min-h-dvh grid text-light-900">
-    <Stars className="place-self-center fixed overflow-hidden scale-50 rotate-90 sm:scale-[.6] sm:rotate-0 md:scale-75" />
-    <div className="relative z-10 place-self-center w-full flex flex-col items-center">
-      <div className="relative flex justify-center items-center w-full">
-        <h1 className="animate-[scale-up_1s_ease-out_forwards] shadow-foreground/30 text-shadow-glow text-6xl sm:text-7xl md:text-8xl max-w-[500px] sm:max-w-[600px] md:max-w-[700px] font-black tracking-tighter text-center absolute z-20">The all in one Discord Bot</h1>
-        <Moon className="w-72 h-72 sm:w-80 sm:h-80 md:w-96 md:h-96 animate-[moon-rotate_1s_ease-out_forwards]" />
-      </div>
-      <div className="animate-[scale-up_1s_ease-out_forwards] opacity-0 animation-delay-500 flex flex-col sm:flex-row gap-6 shadow-foreground/30 mt-20">
-        <Link href={"/dashboard"}>
-          <Button variant={"default"} className="shadow-glow" size={"xl"}>
+export default () => {
+  const isMobile = useMediaQuery("(max-width: 640px)")
+
+  return (
+    <div className="my-auto min-h-[calc(100vh-4rem-1px)] grid place-items-center p-6">
+      <section className="mx-auto flex max-w-[980px] flex-col items-center gap-2 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
+        <Announcement />
+        <PageHeading>The all-in-one Discord bot</PageHeading>
+        <PageDescription>{siteConfig.description}</PageDescription>
+        <PageActions>
+          <Link href="/dashboard" className={isMobile ? buttonVariants({ size: "lg" }) : buttonVariants({ size: "xl" })}>
             Get Started
-          </Button>
-        </Link>
-        <Link href={"/docs"}>
-          <Button variant={"outline"} size={"xl"}>
+          </Link>
+          <Link href="/docs" className={isMobile ? buttonVariants({ variant: "outline", size: "lg" }) : buttonVariants({ variant: "outline", size: "xl" })}>
             Learn More
-          </Button>
-        </Link>
-      </div>
+          </Link>
+        </PageActions>
+      </section>
     </div>
-  </main>
-)
+  )  
+}
