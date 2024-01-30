d67977f96ca8830523424047491c8e6aa0026725
notcharliee <saunderscharlie@hotmail.com>
2024-01-30 16:04:02 +0000

    modify(dash): add select-guild dialog to layout

--- apps/site/src/app/dashboard/layout.tsx
+++ apps/site/src/app/dashboard/layout.tsx
@@ -2,21 +2,19 @@ import { cookies, headers } from "next/headers"
 
 import { Suspense } from "react"
 
-import { REST } from "@discordjs/rest"
-
 import { MainNav } from "./components/main-nav"
 import { UserNav } from "./components/user-nav"
 import { SearchDashboard } from "./components/search-dashboard"
-import { SelectServer } from "./components/select-server"
+
+import {
+  SelectServerCombobox,
+  SelectServerDialog,
+} from "./components/select-server"
 
 import { getGuilds } from "./cache/guilds"
 import { getUser } from "./cache/user"
 
-import { absoluteURL } from "@/lib/utils"
 import { dashboardConfig } from "@/config/dashboard"
-
-
-const discordREST = new REST()
 
 
 export default ({ children }: { children: React.ReactNode }) => {
@@ -31,8 +29,8 @@ export default ({ children }: { children: React.ReactNode }) => {
     <main className="w-full h-screen flex flex-col">
       <header className="border-b z-50 sticky top-0 backdrop-blur-sm">
         <div className="flex h-16 items-center px-4">
-          <Suspense fallback={<ServerSelectCombobox fallback />}>
-            <ServerSelectCombobox />
+          <Suspense fallback={<SelectServerCombobox fallback />}>
+            <SelectServerCombobox />
           </Suspense>
           <MainNav items={dashboardConfig.mainNav} className="mx-6" />
           <div className="ml-auto flex items-center space-x-4">
@@ -44,38 +42,12 @@ export default ({ children }: { children: React.ReactNode }) => {
         </div>
       </header>
       <div className="flex-1 space-y-4 p-8 pt-6">
-        {guildId ? children : (
-          <div>Select a server first!</div>
-        )}
+        {guildId ? children : <SelectServerDialog combobox={
+          <Suspense fallback={<SelectServerCombobox fallback />}>
+            <SelectServerCombobox />
+          </Suspense>
+        } />}
       </div>
     </main>
   )
-}
-
-
-const ServerSelectCombobox = async (props: { fallback?: boolean }) => {
-  if (props.fallback) return <SelectServer fallback/>
-
-  const userId = headers().get("x-user-id")!
-  const userToken = headers().get("x-user-token")!
-
-  const cachedGuilds = await getGuilds(userId, userToken)
-
-  const guilds = [...cachedGuilds.discord].sort((a, b) => {
-    const aIsInDocuments = cachedGuilds.database.some(doc => doc.id == a.id)
-    const bIsInDocuments = cachedGuilds.database.some(doc => doc.id == b.id)
-
-    if (aIsInDocuments && !bIsInDocuments) return -1
-    else if (!aIsInDocuments && bIsInDocuments) return 1
-    else return a.name.toLowerCase().localeCompare(b.name.toLowerCase())
-  }).map((guild, index) => ({
-    id: guild.id,
-    name: guild.name,
-    icon: guild.icon ? discordREST.cdn.icon(guild.id, guild.icon) : absoluteURL("/discord.png"),
-    disabled: !!(index >= cachedGuilds.database.length)
-  }))
-
-  const defaultValue = cachedGuilds.database.find(guild => guild.id === cookies().get("guild")?.value)
-
-  return <SelectServer guilds={guilds} defaultValue={defaultValue?.id ?? ""} />
 }
