39306e885bb27063bb4a41510d558ccea9676106
notcharliee <saunderscharlie@hotmail.com>
2024-01-30 16:00:41 +0000

    modify(dash): select-server component index

--- apps/site/src/app/dashboard/components/select-server/select.tsx
+++ apps/site/src/app/dashboard/components/select-server/select.tsx
@@ -45,7 +45,7 @@ export const SelectServer = <T extends boolean> (props: SelectServerProps<T>) =>
       variant="outline"
       role="combobox"
       aria-expanded={false}
-      className="w-[200px] justify-between"
+      className="w-full sm:w-[200px] justify-between"
     >
       Loading...
     </Button>
@@ -63,7 +63,7 @@ export const SelectServer = <T extends boolean> (props: SelectServerProps<T>) =>
           variant="outline"
           role="combobox"
           aria-expanded={open}
-          className="w-[200px] justify-between"
+          className="w-full sm:w-[200px] justify-between"
         >
           {
             guild ? (
@@ -79,7 +79,7 @@ export const SelectServer = <T extends boolean> (props: SelectServerProps<T>) =>
           <CaretSortIcon className="ml-2 h-4 w-4 shrink-0 opacity-50" />
         </Button>
       </PopoverTrigger>
-      <PopoverContent className="w-[200px] p-0">
+      <PopoverContent className="w-full sm:w-[200px] p-0">
         <Command>
           <CommandInput placeholder="Search servers..." className="h-9" />
           <CommandEmpty>No server found.</CommandEmpty>
@@ -94,7 +94,7 @@ export const SelectServer = <T extends boolean> (props: SelectServerProps<T>) =>
                     currentValue = props.guilds.find(g => g.name.toLowerCase() === currentValue || g.id === currentValue)!.id
 
                     setValue(currentValue === value ? "" : currentValue)
-                    setGuildCookie(currentValue)
+                    setGuildCookie(currentValue === value ? "" : currentValue)
                     setOpen(false)
                   }}
                 >
