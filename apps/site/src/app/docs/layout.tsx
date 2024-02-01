902a85a9e7e3345f6aa91ba6f889cdbb9d5c491b
notcharliee <saunderscharlie@hotmail.com>
2024-02-01 19:36:12 +0000

    fix(docs): sidebar height

--- apps/site/src/app/docs/layout.tsx
+++ apps/site/src/app/docs/layout.tsx
@@ -13,11 +13,11 @@ import { DocsSidebarNav } from "./components/sidebar-nav"
 
 
 export default ({ children }: { children: React.ReactNode }) => (
-  <div className="w-full max-w-[1400px] min-h-screen mx-auto flex gap-12 px-8">
-    <aside className="w-64 h-screen py-6 lg:py-8 pr-6 sticky top-0 hidden md:block">
+  <div className="w-full max-w-[1400px] min-h-[calc(100vh-4rem)] mx-auto flex gap-12 px-8">
+    <aside className="w-64 h-[calc(100vh-4rem)] py-8 pr-6 sticky top-16 hidden md:block">
       <DocsSidebarNav items={docsConfig.sidebarNav} />
     </aside>
-    <main className="py-8 w-full">
+    <div className="py-8 w-full">
       <div className="mb-4 flex items-center space-x-1 text-muted-foreground text-sm">
         <Sheet>
           <SheetTrigger>
@@ -30,6 +30,6 @@ export default ({ children }: { children: React.ReactNode }) => (
         <PathParts />
       </div>
       {children}
-    </main>
+    </div>
   </div>
 )
