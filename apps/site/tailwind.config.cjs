be9dd322850d7a0d73c8f74c1dff8f2bbdbca9ff
notcharliee <saunderscharlie@hotmail.com>
2024-02-02 17:45:19 +0000

    modify(site): remove unused tw config stuff

--- apps/site/tailwind.config.cjs
+++ apps/site/tailwind.config.cjs
@@ -8,10 +8,6 @@ module.exports = {
         "accordion-down": "accordion-down 0.2s ease-out",
         "accordion-up": "accordion-up 0.2s ease-out",
       },
-      backgroundImage: {
-        "main": "url(/bg.png)",
-        "noise": "url(/noise.png)",
-      },
       boxShadow: {
         "glow-sm": `
           0 0 4px 0 var(--tw-shadow-color),
@@ -59,48 +55,9 @@ module.exports = {
       fontFamily: {
         "geist-sans": "var(--font-geist-sans)",
       },
-      height: {
-        "dvh": "100dvh",
-      },
-      maxHeight: {
-        "dvh": "100dvh",
-      },
-      minHeight: {
-        "dvh": "100dvh",
-      },
-      textShadow: {
-        "sm": "0 1px 2px var(--tw-shadow-color)",
-        "DEFAULT": "0 2px 4px var(--tw-shadow-color)",
-        "lg": "0 8px 16px var(--tw-shadow-color)",
-        "glow-sm": `
-          0 0 4px var(--tw-shadow-color),
-          0 0 11px var(--tw-shadow-color),
-          0 0 19px var(--tw-shadow-color),
-          0 0 40px var(--tw-shadow-color)
-        `,
-        "glow": `
-          0 0 4px var(--tw-shadow-color),
-          0 0 11px var(--tw-shadow-color),
-          0 0 19px var(--tw-shadow-color),
-          0 0 40px var(--tw-shadow-color),
-          0 0 80px var(--tw-shadow-color)
-        `,
-        "glow-lg": `
-          0 0 4px var(--tw-shadow-color),
-          0 0 11px var(--tw-shadow-color),
-          0 0 19px var(--tw-shadow-color),
-          0 0 40px var(--tw-shadow-color),
-          0 0 80px var(--tw-shadow-color),
-          0 0 120px var(--tw-shadow-color)
-        `,
-      },
     },
   },
   plugins: [
     require("tailwindcss-animate"),
-    require("tailwindcss/plugin")(({ matchUtilities, theme }) => matchUtilities(
-      { "text-shadow": (value) => ({ textShadow: value }) },
-      { values: theme("textShadow") }
-    )),
   ],
 }
