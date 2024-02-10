1732bb27559b3ac1987abf1e155296eac0a768a6
notcharliee <saunderscharlie@hotmail.com>
2024-02-10 02:30:48 +0000

    fix(api): guild level responses

--- apps/site/src/app/api/image/levels/guild.png/route.tsx
+++ apps/site/src/app/api/image/levels/guild.png/route.tsx
@@ -1,13 +1,16 @@
-import { Font } from "next/dist/compiled/@vercel/og/satori"
 import { NextRequest, NextResponse } from "next/server"
 import { ImageResponse } from "next/og"
 import { env } from "@/lib/env"
 import { z } from "zod"
 
+import { absoluteURL, getOrdinal } from "@/lib/utils"
+
+
 export const runtime = "edge"
 export const dynamic = "force-dynamic"
 
-export async function GET(request: NextRequest) {
+
+export async function GET (request: NextRequest) {
   const rankStart = parseInt(request.nextUrl.searchParams.get("rankStart") || "1", 10)
   const rankEnd = parseInt(request.nextUrl.searchParams.get("rankEnd") || "1", 10)
   const guildId = request.nextUrl.searchParams.get("guild")
@@ -20,7 +23,7 @@ export async function GET(request: NextRequest) {
   const background = backgroundImage ? `url(${backgroundImage})` : "#"+backgroundColour
 
   try {
-    const dataResponse = await fetch(`${env.NEXT_PUBLIC_BASE_URL}/api/levels/guild?guild=${guildId}&rankStart=${rankStart}&rankEnd=${rankEnd}&t=${Date.now()}`)
+    const dataResponse = await fetch(absoluteURL(`/api/levels/guild?guild=${guildId}&rankStart=${rankStart}&rankEnd=${rankEnd}&t=${Date.now()}`))
     const dataResponseJSON = await dataResponse.json() as unknown
   
     const dataSchema = z.array(
@@ -41,24 +44,6 @@ export async function GET(request: NextRequest) {
 
     const data = dataParsed.data
   
-    const geistSemiboldResponse = await fetch(`${env.NEXT_PUBLIC_BASE_URL}/fonts/Geist-600.otf`)
-    const geistSemiboldArrayBuffer = await geistSemiboldResponse.arrayBuffer()
-  
-    const geistSemibold: Font = {
-      data: geistSemiboldArrayBuffer,
-      name: "Geist",
-      style: "normal",
-      weight: 600,
-    }
-
-    const getOrdinal = (number: number): string => {
-      if (number >= 11 && number <= 13) return number + "th"
-      return (
-        number +
-        (["th", "st", "nd", "rd"][number % 10] || ["th", "st", "nd", "rd"][0]!)
-      )
-    }
-  
     return new ImageResponse((
       <div style={{
         background: background,
@@ -68,59 +53,60 @@ export async function GET(request: NextRequest) {
         gap: "24px",
         width: "450px",
       }}>  
-        {await Promise.all(data.map(async (user, index) => {
-          const avatarResponse = await fetch(user.avatar)
-          const avatarArrayBuffer = await avatarResponse.arrayBuffer()
-
-          return (
-            <div key={index} style={{
+        {await Promise.all(data.map(async (user, index) => (
+          <div key={index} style={{
+            display: "flex",
+            gap: "24px",
+            alignItems: "center",
+            height: "60px",
+          }}>
+            <img src={user.avatar} width={60} height={60} style={{
+              borderRadius: "11.25px",
+            }}/>
+            <div style={{
               display: "flex",
-              gap: "24px",
-              alignItems: "center",
-              height: "60px",
+              flexDirection: "column",
             }}>
-              {/* @ts-ignore */}
-              <img src={avatarArrayBuffer} width={60} height={60} style={{
-                borderRadius: "11.25px",
-              }}/>
-              <div style={{
-                display: "flex",
-                flexDirection: "column",
-              }}>
-                <span style={{
-                  color: "#"+primaryColour,
-                  fontSize: "20px",
-                  fontWeight: "600",
-                }}>{getOrdinal(user.rank)} Place</span>
-                <span style={{
-                  color: "#"+secondaryColour,
-                  fontSize: "20px",
-                  fontWeight: "600",
-                }}>{user.username}</span>
-              </div>
-              <svg xmlns="http://www.w3.org/2000/svg" width={60} height={60} viewBox="-1 -1 34 34" style={{
-                marginRight: "0px",
-                marginLeft: "auto",
-              }}>
-                <g transform="rotate(-90 16 16)">
-                  <circle cx="16" cy="16" r="15.9155" stroke={"#"+primaryColour} strokeWidth="2" fill="none"/>
-                  <circle cx="16" cy="16" r="15.9155" stroke={"#"+secondaryColour} strokeWidth="2" strokeDasharray="100 100" strokeDashoffset={-((user.xp / user.target) * 100)} fill="none"/>
-                </g>
-                <span style={{
-                  color: "#"+primaryColour,
-                  fontSize: "20px",
-                  fontWeight: "600",
-                  margin: "auto",
-                }}>{user.level}</span>
-              </svg>
+              <span style={{
+                color: "#"+primaryColour,
+                fontSize: "20px",
+                fontWeight: "600",
+              }}>{getOrdinal(user.rank)} Place</span>
+              <span style={{
+                color: "#"+secondaryColour,
+                fontSize: "20px",
+                fontWeight: "600",
+              }}>{user.username}</span>
             </div>
-          )
-        }))}
+            <svg xmlns="http://www.w3.org/2000/svg" width={60} height={60} viewBox="-1 -1 34 34" style={{
+              marginRight: "0px",
+              marginLeft: "auto",
+            }}>
+              <g transform="rotate(-90 16 16)">
+                <circle cx="16" cy="16" r="15.9155" stroke={"#"+primaryColour} strokeWidth="2" fill="none"/>
+                <circle cx="16" cy="16" r="15.9155" stroke={"#"+secondaryColour} strokeWidth="2" strokeDasharray="100 100" strokeDashoffset={-((user.xp / user.target) * 100)} fill="none"/>
+              </g>
+              <span style={{
+                color: "#"+primaryColour,
+                fontSize: "20px",
+                fontWeight: "600",
+                margin: "auto",
+              }}>{user.level}</span>
+            </svg>
+          </div>
+        )))}
       </div>
     ), {
       width: 450,
       height: (data.length * 60) + ((data.length - 1) * 24) + 48,
-      fonts: [geistSemibold],
+      fonts: [
+        {
+          data: await (await fetch(absoluteURL("/fonts/Geist-600.otf"))).arrayBuffer(),
+          name: "Geist",
+          style: "normal",
+          weight: 600,
+        },
+      ],
     })
   } catch (error) {
     console.log(error)
--- apps/site/src/app/api/levels/guild/route.ts
+++ apps/site/src/app/api/levels/guild/route.ts
@@ -6,13 +6,19 @@ import { REST } from "@discordjs/rest"
 import { LevelSchema } from "@repo/schemas"
 
 import { dbConnect } from "@/lib/db"
-import { absoluteURL } from "@/lib/utils"
 import { env } from "@/lib/env"
-import { type ExtractAPIType } from "@/types/api"
+
+import type { ExtractAPIType } from "@/types/api"
+
+import { StatusCodes } from "http-status-codes"
 
 
 const discordREST = new REST().setToken(env.DISCORD_TOKEN)
 const discordAPI = new API(discordREST)
+
+
+const badRequest = (message: string) =>
+  NextResponse.json({ error: "Bad Request", message }, { status: StatusCodes.BAD_REQUEST })
 
 
 export const GET = async (request: NextRequest) => {
@@ -20,12 +26,10 @@ export const GET = async (request: NextRequest) => {
   const rankEnd = parseInt(request.nextUrl.searchParams.get("rankEnd") || "1", 10)
   const guildId = request.nextUrl.searchParams.get("guild")
 
-  if (!guildId || rankStart <= 0 || rankStart > rankEnd || rankEnd > 15) {
-    return NextResponse.json({
-      error: "Bad Request",
-      documentation: absoluteURL("/docs/api/levels"),
-    }, { status: 400 })
-  }
+  if (!guildId) return badRequest("Missing 'guild' search param.")
+  if (rankStart <= 0) return badRequest("'rankStart' cannot be less than or equal to 0.")
+  if (rankStart > rankEnd) return badRequest("'rankStart' cannot be greater than 'rankEnd'")
+  if (rankEnd > 15) return badRequest("'rankEnd' cannot be greater than 15.")
 
   await dbConnect()
 
@@ -38,25 +42,33 @@ export const GET = async (request: NextRequest) => {
     return NextResponse.json({
       error: "Not Found",
       message: "No users found within the specified rank range.",
-    }, { status: 404 })
+    }, { status: StatusCodes.NOT_FOUND })
   }
 
-  const response = usersData.map(async (data, index) => {
-    const user = await discordAPI.users.get(data.user)
+  const response = []
 
-    return {
-      id: user.id,
-      username: user.username,
-      global_name: user.global_name,
-      avatar: user.avatar ? discordREST.cdn.avatar(user.id, user.avatar, { size: 128, forceStatic: true, extension: "png" }) : `${env.NEXT_PUBLIC_BASE_URL}/discord.png`,
-      level: data.level,
-      xp: data.xp,
-      rank: rankStart + index,
-      target: 500 * (data.level + 1),
+  for (let index = 0; index < usersData.length; index++) {
+    const data = usersData[index]!
+
+    try {
+      const user = await discordAPI.users.get(data.user)
+
+      response.push({
+        id: user.id,
+        username: user.username,
+        global_name: user.global_name ?? user.username,
+        avatar: user.avatar ? discordREST.cdn.avatar(user.id, user.avatar, { size: 128, forceStatic: true, extension: "png" }) : `${env.NEXT_PUBLIC_BASE_URL}/discord.png`,
+        level: data.level,
+        xp: data.xp,
+        rank: rankStart + index,
+        target: 500 * (data.level + 1),
+      })
+    } catch (error) {
+      continue
     }
-  })
+  }
 
-  return NextResponse.json(await Promise.all(response))
+  return NextResponse.json(response)
 }
 
 
