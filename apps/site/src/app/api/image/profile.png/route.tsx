0fb49726d8136a062f99381ced648551acb10546
notcharliee <saunderscharlie@hotmail.com>
2024-02-19 04:52:53 +0000

    fix: profile.png og route

--- apps/site/src/app/api/image/profile.png/route.tsx
+++ apps/site/src/app/api/image/profile.png/route.tsx
@@ -1,13 +1,11 @@
 import { NextRequest } from "next/server"
 import { imageResponse } from "@/utils/imageResponse"
 
-export {
-  runtime,
-  dynamic,
-} from "@/utils/imageResponse"
+export const runtime = "edge"
+export const dynamic = "force-dynamic"
 
 export const GET = (request: NextRequest) => imageResponse({
-  fonts: [],
+  fonts: [600],
   width: 630,
   height: 1200,
 }, (
