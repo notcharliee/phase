698b94f27f07f78b29f78568a0b113a81cb1a628
notcharliee <saunderscharlie@hotmail.com>
2024-02-21 05:07:41 +0000

    feat(dash): remove disabled modules from sidebar

--- apps/site/src/app/dashboard/components/sidebar-nav.tsx
+++ apps/site/src/app/dashboard/components/sidebar-nav.tsx
@@ -24,38 +24,20 @@ export const SidebarNav = (props: { items: SidebarNavItem[] }) => {
         </div>
         <Separator />
         <div className="p-4 flex flex-col gap-2">
-          {props.items.map((item, index) =>
-            item.href && !item.disabled ? (
-              <Link
-                key={index}
-                href={pathname === item.href ? "/dashboard/modules" : item.href}
-                className={cn(
-                  buttonVariants({ variant: "ghost" }),
-                  pathname === item.href
-                    ? "bg-muted hover:bg-muted"
-                    : "hover:bg-transparent hover:underline",
-                  "justify-start w-full border border-muted"
-                )}
-              >
-                {item.title}
-              </Link>
-            ) : (
-              <span
-                key={index}
-                className={cn(
-                  "flex w-full cursor-not-allowed items-center rounded-md p-2 text-muted-foreground hover:underline",
-                  buttonVariants({ variant: "ghost" }),
-                  item.disabled && "cursor-not-allowed opacity-60",
-                )}
-              >
-                {item.title}
-                {item.label && (
-                  <span className="ml-2 rounded-md bg-muted px-1.5 py-0.5 text-xs leading-none text-muted-foreground no-underline group-hover:no-underline">
-                    {item.label}
-                  </span>
-                )}
-              </span>
-            )
+          {props.items.filter(item => !item.disabled && item.href).map((item, index) =>
+            <Link
+              key={index}
+              href={pathname === item.href ? "/dashboard/modules" : item.href!}
+              className={cn(
+                buttonVariants({ variant: "ghost" }),
+                pathname === item.href
+                  ? "bg-muted hover:bg-muted"
+                  : "hover:bg-transparent hover:underline",
+                "justify-start w-full border border-muted"
+              )}
+            >
+              {item.title}
+            </Link>
           )}
         </div>
       </ScrollArea>
