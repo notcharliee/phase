bcce43fb669cd8cfb38086eda240f8be6ba09f3a
notcharliee <saunderscharlie@hotmail.com>
2024-02-21 05:37:18 +0000

    refactor: modules layout

--- apps/site/src/app/dashboard/modules/layout.tsx
+++ apps/site/src/app/dashboard/modules/layout.tsx
@@ -1,18 +1,20 @@
-import { SidebarNav } from "../components/sidebar-nav"
-
 import { dashboardNavConfig } from "@/config/nav/dashboard"
 
+import { SidebarNav } from "@/app/dashboard/components/sidebar-nav"
 
-const sidebarNavItems = dashboardNavConfig.sidebarNav[0]!.items
-
-
-export default ({ children }: { children: React.ReactNode }) => (
-  <div className="p-8 py-6">
-    <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
-      <aside className="hidden md:block w-[40%] lg:w-[30%] xl:w-[20%]">
-        <SidebarNav items={sidebarNavItems} />
-      </aside>
-      <div className="flex-1 grid lg:max-w-2xl xl:max-w-4xl">{children}</div>
+export default function ModulesLayout({
+  children,
+}: {
+  children: React.ReactNode
+}) {
+  return (
+    <div className="p-8 py-6">
+      <div className="flex flex-col space-y-8 lg:flex-row lg:space-x-12 lg:space-y-0">
+        <aside className="hidden w-[40%] md:block lg:w-[30%] xl:w-[20%]">
+          <SidebarNav items={dashboardNavConfig.sidebarNav[0]!.items} />
+        </aside>
+        <div className="grid flex-1 lg:max-w-2xl xl:max-w-4xl">{children}</div>
+      </div>
     </div>
-  </div>
-)
+  )
+}
