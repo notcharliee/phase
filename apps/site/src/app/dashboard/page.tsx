a911d8a01d687988b20e06f3542bc4ec7bf1152b
notcharliee <saunderscharlie@hotmail.com>
2024-02-21 05:39:44 +0000

    refactor: dashboard page

--- apps/site/src/app/dashboard/page.tsx
+++ apps/site/src/app/dashboard/page.tsx
@@ -1,7 +1,5 @@
 import { cookies } from "next/headers"
 
-import { Suspense } from "react"
-
 import {
   Card,
   CardContent,
@@ -17,40 +15,43 @@ import { BotStatus } from "./components/analytics/bot-status"
 import { Commands } from "./components/commands"
 import { LevelsLeaderboard } from "./components/levels-leaderboard"
 
-
-export default () => {
+export default function DashboardPage() {
   const guildId = cookies().get("guild")!.value
 
   return (
-    <div className="flex flex-col overflow-auto md:h-[calc(100vh-65px)] md:overflow-hidden space-y-4 p-8 py-6 sticky top-[5.5rem]">
+    <div className="sticky top-[5.5rem] flex flex-col space-y-4 overflow-auto p-8 py-6 md:h-[calc(100vh-65px)] md:overflow-hidden">
       <div className="grid gap-4 sm:grid-cols-2 lg:grid-cols-4">
         <TotalMembers />
         <OnlineNow />
         <EnabledModules />
         <BotStatus />
       </div>
-      <div className="h-full flex flex-col md:flex-row gap-4 overflow-hidden">
-        <div className="h-full md:min-w-[50%] lg:min-w-[35%] rounded-xl overflow-hidden">
+      <div className="flex h-full flex-col gap-4 overflow-hidden md:flex-row">
+        <div className="h-full overflow-hidden rounded-xl md:min-w-[50%] lg:min-w-[35%]">
           <Card className="h-full overflow-y-scroll">
             <CardHeader>
               <CardTitle>Top Members</CardTitle>
-              <CardDescription>These are the most active members in your server.</CardDescription>
+              <CardDescription>
+                These are the most active members in your server.
+              </CardDescription>
             </CardHeader>
             <CardContent>
-              <div className="h-full flex flex-col gap-3">
+              <div className="flex h-full flex-col gap-3">
                 <LevelsLeaderboard guild={guildId} />
               </div>
             </CardContent>
           </Card>
         </div>
-        <div className="h-full md:w-full rounded-xl overflow-hidden">
+        <div className="h-full overflow-hidden rounded-xl md:w-full">
           <Card className="h-full overflow-auto">
             <CardHeader>
               <CardTitle>Command Permissions</CardTitle>
-              <CardDescription>Set required roles for each slash command.</CardDescription>
+              <CardDescription>
+                Set required roles for each slash command.
+              </CardDescription>
             </CardHeader>
             <CardContent>
-              <div className="h-full grid gap-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
+              <div className="grid h-full gap-3 sm:grid-cols-2 md:grid-cols-1 lg:grid-cols-2 xl:grid-cols-3">
                 <Commands />
               </div>
             </CardContent>
