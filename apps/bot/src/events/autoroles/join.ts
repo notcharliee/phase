28b494e4bb9c779c0970db5446fafc1a9f55cf21
notcharliee <saunderscharlie@hotmail.com>
2024-02-26 05:19:26 +0000

    refactor: autoroles events

---
+++ apps/bot/src/events/autoroles/join.ts
@@ -0,0 +1,20 @@
+import { botEvent } from "phase.js"
+import { GuildSchema } from "@repo/schemas"
+import { GuildFeature } from "discord.js"
+
+export default botEvent("guildMemberAdd", async (client, member) => {
+  if (
+    member.guild.features.includes(GuildFeature.MemberVerificationGateEnabled)
+  )
+    return
+
+  const guildSchema = await GuildSchema.findOne({ id: member.guild.id })
+  const autoRolesModule = guildSchema?.modules.AutoRoles
+  
+  if (!autoRolesModule?.enabled) return
+
+  for (const role of autoRolesModule.roles) {
+    if (member.guild.roles.cache.get(role) && !member.roles.cache.has(role))
+      member.roles.add(role)
+  }
+})
---
+++ apps/bot/src/events/autoroles/pending.ts
@@ -0,0 +1,30 @@
+import { botEvent } from "phase.js"
+import { GuildSchema } from "@repo/schemas"
+import { GuildFeature } from "discord.js"
+
+export default botEvent(
+  "guildMemberUpdate",
+  async (client, oldMember, newMember) => {
+    if (
+      !newMember.guild.features.includes(
+        GuildFeature.MemberVerificationGateEnabled,
+      ) ||
+      !oldMember.pending ||
+      (oldMember.pending && newMember.pending)
+    )
+      return
+
+    const guildSchema = await GuildSchema.findOne({ id: newMember.guild.id })
+    const autoRolesModule = guildSchema?.modules.AutoRoles
+
+    if (!autoRolesModule?.enabled) return
+
+    for (const role of autoRolesModule.roles) {
+      if (
+        newMember.guild.roles.cache.get(role) &&
+        !newMember.roles.cache.has(role)
+      )
+        newMember.roles.add(role)
+    }
+  },
+)
