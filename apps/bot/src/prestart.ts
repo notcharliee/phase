3388d3884e1e0bc4f4de6da4bcad6e957d22312d
notcharliee <saunderscharlie@hotmail.com>
2024-02-26 00:11:32 +0000

    fix: phase.js prestart

--- apps/bot/src/prestart.ts
+++ apps/bot/src/prestart.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose"
 import { env } from "~/env"
 
-export default async () => {
+export default async function prestart () {
   await mongoose.connect(env.MONGODB_URI)
 }
--- packages/phase.js/src/utils/prestart.ts
+++ packages/phase.js/src/utils/prestart.ts
@@ -3,11 +3,11 @@ import { pathToFileURL } from "node:url"
 import { resolve } from "node:path"
 
 export const getPrestart = async () => {
-  const prestartURL = pathToFileURL(resolve(process.cwd(), "build/prestart.cjs"))
+  const prestartURL = pathToFileURL(resolve(process.cwd(), "build/prestart.js"))
 
   if (!existsSync(prestartURL)) return () => undefined
 
-  const prestartFunction = (await (await import(prestartURL.toString())).default).default as unknown
+  const prestartFunction = await (await import(prestartURL.toString())).default as unknown
 
   if (!prestartFunction) return () => undefined
   if (typeof prestartFunction !== "function") throw new Error("Prestart must export a function.")
