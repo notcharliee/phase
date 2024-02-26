b279f5533a3a93045bd72a3a3993b03d8488a4dc
notcharliee <saunderscharlie@hotmail.com>
2024-02-26 06:42:36 +0000

    refactor: afk command

---
+++ apps/bot/src/commands/module/afk.ts
@@ -0,0 +1,40 @@
+import { BotCommandBuilder, botCommand } from "phase.js"
+import { AFKSchema } from "@repo/schemas"
+import { PhaseColour } from "~/utils"
+import { EmbedBuilder } from "discord.js"
+
+export default botCommand(
+  new BotCommandBuilder()
+    .setName("afk")
+    .setDescription("Set your AFK status.")
+    .setDMPermission(false)
+    .addStringOption((option) =>
+      option
+        .setName("reason")
+        .setDescription("Give a reason for going AFK.")
+        .setRequired(false),
+    ),
+  async (client, interaction) => {
+    const reason =
+      interaction.options.getString("reason", false) ?? "No reason set."
+
+    const afkSchema =
+      (await AFKSchema.findOne({ user: interaction.user.id })) ??
+      new AFKSchema({
+        user: interaction.user.id,
+        reason,
+      })
+
+    afkSchema.reason = reason
+    await afkSchema.save()
+
+    interaction.reply({
+      embeds: [
+        new EmbedBuilder()
+          .setColor(PhaseColour.Primary)
+          .setDescription(reason)
+          .setTitle("AFK Status Changed"),
+      ],
+    })
+  },
+)
