6f368fc4620d53e8735b8e40d0c04aee13d5f7e2
notcharliee <saunderscharlie@hotmail.com>
2024-02-26 05:22:38 +0000

    refactor: reactionroles events

---
+++ apps/bot/src/events/reactionroles/add.ts
@@ -0,0 +1,33 @@
+import { GuildSchema } from "@repo/schemas"
+import { botEvent } from "phase.js"
+
+export default botEvent(
+  "messageReactionAdd",
+  async (client, reaction, user) => {
+    if (user.bot || !reaction.message.inGuild()) return
+
+    const guildSchema = await GuildSchema.findOne({
+      id: reaction.message.guildId,
+    })
+    const reactionRolesModule = guildSchema?.modules.ReactionRoles
+    
+    if (!reactionRolesModule?.enabled) return
+
+    for (const reactionRoleReaction of reactionRolesModule.reactions) {
+      if (
+        reactionRoleReaction.emoji !== reaction.emoji.id &&
+        reactionRoleReaction.emoji !== reaction.emoji.name
+      )
+        continue
+
+      const role = reaction.message.guild.roles.cache.get(
+        reactionRoleReaction.role,
+      )
+      const member = reaction.message.guild.members.cache.get(user.id)
+
+      if (role && role.editable && member) {
+        member.roles.add(role)
+      }
+    }
+  },
+)
