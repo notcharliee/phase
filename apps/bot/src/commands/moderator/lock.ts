d7b36b0512fadadf989a3f55da22e02af1c446d0
notcharliee <saunderscharlie@hotmail.com>
2024-02-27 07:40:40 +0000

    refactor: lock command

---
+++ apps/bot/src/commands/moderator/lock.ts
@@ -0,0 +1,87 @@
+import { PermissionFlagsBits, GuildChannel, EmbedBuilder } from "discord.js"
+import { BotCommandBuilder, botCommand } from "phase.js"
+import { missingPermission, errorMessage, PhaseColour } from "~/utils"
+
+export default botCommand(
+  new BotCommandBuilder()
+    .setName("lock")
+    .setDescription("Locks and unlocks a channel.")
+    .setDMPermission(false)
+    .addBooleanOption((option) =>
+      option
+        .setName("state")
+        .setDescription("The state of the channel lock.")
+        .setRequired(true),
+    )
+    .addRoleOption((option) =>
+      option
+        .setName("role")
+        .setDescription(
+          "Specify a role to lock access for (defaults to @everyone).",
+        )
+        .setRequired(false),
+    ),
+  async (client, interaction) => {
+    if (
+      !interaction.memberPermissions?.has(PermissionFlagsBits.ModerateMembers)
+    )
+      return interaction.reply(
+        missingPermission(PermissionFlagsBits.ModerateMembers),
+      )
+
+    const channel = interaction.channel as GuildChannel
+    const state = interaction.options.getBoolean("state", true)
+    const role = interaction.options.getRole("role", false)
+
+    if (channel.isThread()) {
+      interaction.reply(
+        errorMessage({
+          title: "Invalid Channel",
+          description: "This command cannot be used in threads.",
+        }),
+      )
+    }
+
+    if (state) {
+      if (channel.isTextBased())
+        channel.permissionOverwrites.edit(role?.id ?? interaction.guildId!, {
+          SendMessages: false,
+        })
+      if (channel.isVoiceBased())
+        channel.permissionOverwrites.edit(role?.id ?? interaction.guildId!, {
+          Speak: false,
+        })
+
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(PhaseColour.Primary)
+            .setDescription(
+              `Channel has been locked for ${role ?? "@everyone"}.`,
+            )
+            .setTitle("Channel Locked"),
+        ],
+      })
+    } else {
+      if (channel.isTextBased())
+        channel.permissionOverwrites.edit(role?.id ?? interaction.guildId!, {
+          SendMessages: true,
+        })
+      if (channel.isVoiceBased())
+        channel.permissionOverwrites.edit(role?.id ?? interaction.guildId!, {
+          Speak: true,
+        })
+
+      interaction.reply({
+        embeds: [
+          new EmbedBuilder()
+            .setColor(PhaseColour.Primary)
+            .setDescription(
+              `Channel has been unlocked for ${role ?? "@everyone"}.`,
+            )
+            .setTitle("Channel Unlocked"),
+        ],
+      })
+    }
+  },
+)
