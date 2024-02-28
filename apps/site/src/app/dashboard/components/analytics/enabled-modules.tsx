9a284a48c2974b4aff149e5be5ddaadec64e5dc1
notcharliee <saunderscharlie@hotmail.com>
2024-02-28 08:38:58 +0000

    fix(dash): enabled-modules component

--- apps/site/src/app/dashboard/components/analytics/enabled-modules.tsx
+++ apps/site/src/app/dashboard/components/analytics/enabled-modules.tsx
@@ -1,33 +1,29 @@
 import { cookies, headers } from "next/headers"
 import Link from "next/link"
 
-import {
-  LightningBoltIcon,
-  OpenInNewWindowIcon,
-} from "@radix-ui/react-icons"
+import { LightningBoltIcon, OpenInNewWindowIcon } from "@radix-ui/react-icons"
 
-import {
-  Card,
-  CardContent,
-  CardHeader,
-  CardTitle,
-} from "@/components/ui/card"
+import { Card, CardContent, CardHeader, CardTitle } from "@/components/ui/card"
 
 import { getGuilds } from "../../cache/guilds"
 
+import { modulesConfig } from "@/config/modules"
 
 export const EnabledModules = async (props: { fallback?: boolean }) => {
   const fallback = (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">Enabled Modules</CardTitle>
-        <LightningBoltIcon className="h-4 w-4 text-muted-foreground" />
+        <LightningBoltIcon className="text-muted-foreground h-4 w-4" />
       </CardHeader>
       <CardContent>
         <span className="text-2xl font-bold">Loading...</span>
-        <Link className="flex items-center hover:underline underline-offset-2 hover:animate-pulse" href={"/dashboard/modules"}>
-          <p className="text-xs text-muted-foreground">Module settings</p>
-          <OpenInNewWindowIcon className="h-3 w-3 ml-1 text-muted-foreground" />
+        <Link
+          className="flex items-center underline-offset-2 hover:animate-pulse hover:underline"
+          href={"/dashboard/modules"}
+        >
+          <p className="text-muted-foreground text-xs">Module settings</p>
+          <OpenInNewWindowIcon className="text-muted-foreground ml-1 h-3 w-3" />
         </Link>
       </CardContent>
     </Card>
@@ -41,23 +37,28 @@ export const EnabledModules = async (props: { fallback?: boolean }) => {
   const cachedGuilds = await getGuilds(userId, userToken)
 
   const guildId = cookies().get("guild")!.value
-  const guild = cachedGuilds.database.find(guild => guild.id === guildId)
+  const guild = cachedGuilds.database.find((guild) => guild.id === guildId)
 
   if (!guild) return fallback
 
-  const enabledModules = `${Object.values(guild.modules).filter((module) => module.enabled).length} / ${Object.values(guild.modules).length}`
+  const enabledModules = `${Object.values(guild.modules ?? {}).filter((module) => module.enabled).length} / ${modulesConfig.length}`
 
   return (
     <Card>
       <CardHeader className="flex flex-row items-center justify-between space-y-0 pb-2">
         <CardTitle className="text-sm font-medium">Enabled Modules</CardTitle>
-        <LightningBoltIcon className="h-4 w-4 text-muted-foreground" />
+        <LightningBoltIcon className="text-muted-foreground h-4 w-4" />
       </CardHeader>
       <CardContent>
-        <span className="text-2xl font-bold">{enabledModules ?? "Unknown"}</span>
-        <Link className="flex items-center hover:underline underline-offset-2 hover:animate-pulse" href={"/dashboard/modules"}>
-          <p className="text-xs text-muted-foreground">Module settings</p>
-          <OpenInNewWindowIcon className="h-3 w-3 ml-1 text-muted-foreground" />
+        <span className="text-2xl font-bold">
+          {enabledModules ?? "Unknown"}
+        </span>
+        <Link
+          className="flex items-center underline-offset-2 hover:animate-pulse hover:underline"
+          href={"/dashboard/modules"}
+        >
+          <p className="text-muted-foreground text-xs">Module settings</p>
+          <OpenInNewWindowIcon className="text-muted-foreground ml-1 h-3 w-3" />
         </Link>
       </CardContent>
     </Card>
