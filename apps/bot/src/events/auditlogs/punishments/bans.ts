378ff3b051976030ff9b93937cd7f9b677cc1b30
notcharliee <saunderscharlie@hotmail.com>
2024-03-11 21:01:32 +0000

    feat: add ban/unban logs

---
+++ apps/bot/src/events/auditlogs/punishments/bans.ts
@@ -0,0 +1,47 @@
+import { botEvent } from "phase.js"
+import { GuildSchema } from "@repo/schemas"
+import { PhaseColour } from "~/utils"
+
+import { AuditLogEvent, EmbedBuilder, GuildTextBasedChannel } from "discord.js"
+
+export default botEvent("guildBanAdd", async (client, ban) => {
+  const guildSchema = await GuildSchema.findOne({ id: ban.guild.id })
+  if (!guildSchema) return
+
+  if (
+    !guildSchema.modules?.AuditLogs?.enabled ||
+    !guildSchema.modules.AuditLogs.channels.punishments ||
+    !client.channels.cache.has(
+      guildSchema.modules.AuditLogs.channels.punishments,
+    )
+  ) {
+    return
+  }
+
+  const logsChannel = client.channels.cache.get(
+    guildSchema.modules.AuditLogs.channels.punishments,
+  ) as GuildTextBasedChannel
+
+  const member = ban.user
+
+  const responsibleModerator = await ban.guild
+    .fetchAuditLogs({ type: AuditLogEvent.MemberBanAdd, limit: 1 })
+    .then((auditLogs) => auditLogs.entries.first()?.executor)
+    .catch(() => null)
+
+  return logsChannel.send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Member Banned")
+        .setThumbnail(member.displayAvatarURL())
+        .setColor(PhaseColour.Primary)
+        .setDescription(
+          `**Offender:** ${member}\n**Reason:** ${ban.reason ?? "No reason provided"}\n**Responsible Moderator:** ${responsibleModerator?.username ?? "Unknown"}`,
+        )
+        .setFooter({
+          text: `ID: ${member.id}`,
+        })
+        .setTimestamp(),
+    ],
+  })
+})
---
+++ apps/bot/src/events/auditlogs/punishments/unbans.ts
@@ -0,0 +1,47 @@
+import { botEvent } from "phase.js"
+import { GuildSchema } from "@repo/schemas"
+import { PhaseColour } from "~/utils"
+
+import { AuditLogEvent, EmbedBuilder, GuildTextBasedChannel } from "discord.js"
+
+export default botEvent("guildBanRemove", async (client, unban) => {
+  const guildSchema = await GuildSchema.findOne({ id: unban.guild.id })
+  if (!guildSchema) return
+
+  if (
+    !guildSchema.modules?.AuditLogs?.enabled ||
+    !guildSchema.modules.AuditLogs.channels.punishments ||
+    !client.channels.cache.has(
+      guildSchema.modules.AuditLogs.channels.punishments,
+    )
+  ) {
+    return
+  }
+
+  const logsChannel = client.channels.cache.get(
+    guildSchema.modules.AuditLogs.channels.punishments,
+  ) as GuildTextBasedChannel
+
+  const member = unban.user
+
+  const responsibleModerator = await unban.guild
+    .fetchAuditLogs({ type: AuditLogEvent.MemberBanRemove, limit: 1 })
+    .then((auditLogs) => auditLogs.entries.first()?.executor)
+    .catch(() => null)
+
+  return logsChannel.send({
+    embeds: [
+      new EmbedBuilder()
+        .setTitle("Member Unbanned")
+        .setThumbnail(member.displayAvatarURL())
+        .setColor(PhaseColour.Primary)
+        .setDescription(
+          `**Offender:** ${member}\n**Ban Reason:** ${unban.reason ?? "No reason provided"}\n**Responsible Moderator:** ${responsibleModerator?.username ?? "Unknown"}`,
+        )
+        .setFooter({
+          text: `ID: ${member.id}`,
+        })
+        .setTimestamp(),
+    ],
+  })
+})
