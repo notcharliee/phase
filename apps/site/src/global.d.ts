274a115e934e6bb46843d592029a2aee795b7f42
notcharliee <saunderscharlie@hotmail.com>
2024-03-29 22:19:08 +0000

    feat: add UnionToIntersection to site global.d.ts

--- apps/site/src/global.d.ts
+++ apps/site/src/global.d.ts
@@ -1,1 +1,6 @@
+/* eslint-disable @typescript-eslint/no-explicit-any */
+
 type Nullable<T> = T | null
+
+type UnionToIntersection<U> = 
+  (U extends any ? (x: U)=>void : never) extends ((x: infer I)=>void) ? I : never
