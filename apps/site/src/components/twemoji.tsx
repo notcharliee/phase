148f58280c5200206d0422258a1b450b488c3eae
notcharliee <saunderscharlie@hotmail.com>
2024-03-30 23:41:04 +0000

    fix: twemoji

--- apps/site/src/components/twemoji.tsx
+++ apps/site/src/components/twemoji.tsx
@@ -1,22 +1,27 @@
-"use client"
-
-import { memo } from "react"
+import { memo, useMemo } from "react"
 import twemoji from "twemoji"
 
-export const Twemoji = memo(({ emoji }: { emoji: string }) => (
-  <span
-    dangerouslySetInnerHTML={{
-      __html: twemoji.parse(emoji, {
-        attributes() {
-          return {
-            loading: "lazy",
-          }
-        },
-        folder: "svg",
-        ext: ".svg",
-      }),
-    }}
-  />
-))
+export const Twemoji = memo(({ emoji }: { emoji: string }) => {
+  const parsedEmoji = useMemo(() => {
+    return twemoji.parse(emoji, {
+      attributes() {
+        return {
+          loading: "lazy",
+        }
+      },
+      folder: "svg",
+      ext: ".svg",
+      base: "https://cdn.jsdelivr.net/gh/twitter/twemoji@14.0.2/assets/"
+    })
+  }, [emoji])
+
+  return (
+    <span
+      dangerouslySetInnerHTML={{
+        __html: parsedEmoji,
+      }}
+    />
+  )
+})
 
 Twemoji.displayName = "Twemoji"
