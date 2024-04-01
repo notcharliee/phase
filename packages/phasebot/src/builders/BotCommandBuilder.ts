6b40dd25c56425a676fc4eff735da4293a4cabbf
notcharliee <saunderscharlie@hotmail.com>
2024-04-01 22:31:18 +0100

    fix: phasebot command builder subcommands

--- packages/phasebot/src/builders/BotCommandBuilder.ts
+++ packages/phasebot/src/builders/BotCommandBuilder.ts
@@ -1,12 +1,16 @@
+import { Mixin } from "ts-mixer"
+
 import {
   APIApplicationCommandOption,
+  SlashCommandAssertions,
   SlashCommandBuilder,
+  SlashCommandSubcommandBuilder,
+  SlashCommandSubcommandGroupBuilder,
   ToAPIApplicationCommandOptions,
   type ChatInputCommandInteraction,
   type Client,
   type RESTPostAPIChatInputApplicationCommandsJSONBody,
   type SlashCommandOptionsOnlyBuilder,
-  type SlashCommandSubcommandGroupBuilder,
   type SlashCommandSubcommandsOnlyBuilder,
 } from "discord.js"
 
@@ -35,10 +39,7 @@ export type DeprecatedBotCommandFunction = {
   }
 }
 
-/**
- * A builder that creates API-compatible JSON data for slash commands.
- */
-export class BotCommandBuilder extends SlashCommandBuilder {
+class BotCommandBuilderBase {
   /**
    * The function to execute when the command is called.
    */
@@ -46,6 +47,8 @@ export class BotCommandBuilder extends SlashCommandBuilder {
 
   /**
    * Set the function to execute when the command is called.
+   *
+   * @param fn - The function to execute when the command is called.
    */
   setExecute(fn: BotCommandExecuteFunction) {
     Reflect.set(this, "execute", fn)
@@ -59,14 +62,27 @@ export class BotCommandBuilder extends SlashCommandBuilder {
 
   /**
    * Set the metadata for the command.
+   *
+   * @param metadata - The metadata for the command.
    */
   setMetadata(metadata: object) {
     Reflect.set(this, "metadata", metadata)
     return this
   }
+}
 
+/**
+ * A builder that creates API-compatible JSON data for phasebot slash commands.
+ * @extends SlashCommandBuilder
+ */
+export class BotCommandBuilder extends Mixin(
+  BotCommandBuilderBase,
+  SlashCommandBuilder,
+) {
   /**
    * The options for the command.
+   *
+   * @param options - The options for the command.
    */
   setOptions(options: APIApplicationCommandOption[]) {
     const toAPIApplicationCommandOptions: ToAPIApplicationCommandOptions[] =
@@ -76,5 +92,75 @@ export class BotCommandBuilder extends SlashCommandBuilder {
 
     Reflect.set(this, "options", toAPIApplicationCommandOptions)
     return this
+  }
+
+  /**
+   * Adds a new subcommand group to this command.
+   *
+   * @param input - A function that returns a subcommand group builder or an already built builder
+   */
+  addSubcommandGroup(
+    input:
+      | SlashCommandSubcommandGroupBuilder
+      | ((
+          subcommandGroup: SlashCommandSubcommandGroupBuilder,
+        ) => SlashCommandSubcommandGroupBuilder),
+  ) {
+    const { options } = this
+
+    // First, assert options conditions - we cannot have more than 25 options
+    SlashCommandAssertions.validateMaxOptionsLength(options)
+
+    // Get the final result
+    const result =
+      typeof input === "function"
+        ? input(new SlashCommandSubcommandGroupBuilder())
+        : input
+
+    SlashCommandAssertions.assertReturnOfBuilder(
+      result,
+      SlashCommandSubcommandGroupBuilder,
+    )
+
+    // Push it
+    options.push(result)
+
+    return this as unknown as SlashCommandSubcommandsOnlyBuilder &
+      BotCommandBuilderBase
+  }
+
+  /**
+   * Adds a new subcommand to this command.
+   *
+   * @param input - A function that returns a subcommand builder or an already built builder
+   */
+  addSubcommand(
+    input:
+      | SlashCommandSubcommandBuilder
+      | ((
+          subcommandGroup: SlashCommandSubcommandBuilder,
+        ) => SlashCommandSubcommandBuilder),
+  ) {
+    const { options } = this
+
+    // First, assert options conditions - we cannot have more than 25 options
+    SlashCommandAssertions.validateMaxOptionsLength(options)
+
+    // Get the final result
+    const result =
+      typeof input === "function"
+        ? input(new SlashCommandSubcommandBuilder())
+        : input
+
+    SlashCommandAssertions.assertReturnOfBuilder(
+      result,
+      SlashCommandSubcommandBuilder,
+    )
+
+    // Push it
+    options.push(result)
+
+    return this as unknown as SlashCommandSubcommandsOnlyBuilder &
+      BotCommandBuilderBase
   }
 }
