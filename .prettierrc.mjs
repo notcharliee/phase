f1eb11d163f17662abbc29ef8c0a3d54d8de15b0
notcharliee <saunderscharlie@hotmail.com>
2024-04-06 01:35:45 +0100

    chore: update prettier plugins

--- .prettierrc.mjs
+++ .prettierrc.mjs
@@ -1,5 +1,29 @@
 /** @type {import("prettier").Config} */
 export default {
-  plugins: ["prettier-plugin-tailwindcss", "prettier-plugin-organize-imports"],
+  importOrder: [
+    "<BUILTIN_MODULES>",
+    "",
+    "^(next/(.*)$)|^(next$)|^(react/(.*)$)|^(react$)",
+    "",
+    "<THIRD_PARTY_MODULES>",
+    "",
+    "^@/components/(.*)$",
+    "",
+    "^@/hooks/(.*)$",
+    "",
+    "^@/config/(.*)$",
+    "",
+    "^@/lib/(.*)$",
+    "",
+    "^@/types/(.*)$",
+    "",
+    "^@/styles/(.*)$",
+    "",
+    "^[.]",
+  ],
+  plugins: [
+    "@ianvs/prettier-plugin-sort-imports",
+    "prettier-plugin-tailwindcss",
+  ],
   semi: false,
 }
