9e5bc3914bb79f9edc7727a335182ce45cc72d42
notcharliee <saunderscharlie@hotmail.com>
2024-05-03 17:11:25 +0100

    fix: jtc

--- apps/bot/src/events/jointocreate/voiceUpdate.ts
+++ apps/bot/src/events/jointocreate/voiceUpdate.ts
@@ -1,57 +1,49 @@
-import { GuildSchema } from "@repo/schemas"
 import { ChannelType } from "discord.js"
-import { botEvent } from "phasebot"
+import { BotEventBuilder } from "phasebot/builders"
 
-export default botEvent(
-  "voiceStateUpdate",
-  async (client, oldVoice, newVoice) => {
-    try {
-      const guildSchema = await GuildSchema.findOne({ id: oldVoice.guild.id })
-      const joinToCreateModule = guildSchema?.modules?.JoinToCreates
-      if (!guildSchema || !joinToCreateModule?.enabled) return
+import { GuildModuleJoinToCreates, GuildSchema } from "@repo/schemas"
 
-      if (
-        // If...
-        oldVoice.channel && // User was in a channel before update AND...
-        joinToCreateModule.active && // Active array exists AND...
-        joinToCreateModule.active.includes(oldVoice.channel.id) && // The channel was a jtc channel AND...
-        (!oldVoice.channel.members.size ||
-          oldVoice.channel.members.every((member) => member.user.bot)) // There's no members left OR the only members left are bots...
-      ) {
-        // Delete old jtc channel and update database.
-        oldVoice.channel.delete()
+export default new BotEventBuilder()
+  .setName("voiceStateUpdate")
+  .setExecute(async (_, oldVoice, newVoice) => {
+    const guildSchema = await GuildSchema.findOne({ id: oldVoice.guild.id })
+    const joinToCreateModule = guildSchema?.modules?.JoinToCreates
 
-        joinToCreateModule.active.splice(
-          joinToCreateModule.active.indexOf(oldVoice.channel.id),
-          1,
-        )
+    if (!guildSchema || !joinToCreateModule?.enabled) return
 
-        guildSchema.markModified("modules")
-        guildSchema.save()
-      }
+    if (
+      oldVoice.channel &&
+      "active" in joinToCreateModule &&
+      joinToCreateModule.active.includes(oldVoice.channel.id) &&
+      oldVoice.channel.members.filter((member) => !member.user.bot).size === 0
+    ) {
+      oldVoice.channel.delete()
 
-      if (
-        // If...
-        newVoice.channelId == joinToCreateModule.channel // New channel is the jtc base channel...
-      ) {
-        // Create new jtc channel, move member, and update database.
-        const newVoiceChannel = await newVoice.guild.channels.create({
-          name: newVoice.member?.displayName ?? "voice channel",
-          type: ChannelType.GuildVoice,
-          parent: joinToCreateModule.category,
-        })
+      joinToCreateModule.active.splice(
+        joinToCreateModule.active.indexOf(oldVoice.channel.id),
+        1,
+      )
 
-        newVoice.setChannel(newVoiceChannel)
-
-        if (!joinToCreateModule.active) joinToCreateModule.active = []
-        
-        joinToCreateModule.active.push(newVoiceChannel.id)
-
-        guildSchema.markModified("modules")
-        guildSchema.save()
-      }
-    } catch (error) {
-      throw error
+      guildSchema.markModified("modules")
+      guildSchema.save()
     }
-  },
-)
+
+    if (newVoice.channelId === joinToCreateModule.channel) {
+      const newVoiceChannel = await newVoice.guild.channels.create({
+        name: newVoice.member?.user.username ?? "voice channel",
+        type: ChannelType.GuildVoice,
+        parent: joinToCreateModule.category,
+      })
+
+      newVoice.setChannel(newVoiceChannel)
+
+      if (!("active" in joinToCreateModule)) {
+        ;(joinToCreateModule as GuildModuleJoinToCreates).active = []
+      }
+
+      joinToCreateModule.active.push(newVoiceChannel.id)
+
+      guildSchema.markModified("modules")
+      guildSchema.save()
+    }
+  })
