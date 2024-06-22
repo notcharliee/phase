cc743fe0216b219cf8e7757d6a0a184ea0de8713
notcharliee <saunderscharlie@hotmail.com>
2024-06-22 17:53:10 +0100

    feat: update bot env vars

--- apps/bot/src/env.ts
+++ apps/bot/src/env.ts
@@ -5,9 +5,7 @@ export const env = createEnv({
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     MONGODB_URI: z.string(),
-    BOT_TOKEN: z.string(),
-    BOT_SECRET: z.string(),
-    BOT_ID: z.string(),
+    DISCORD_TOKEN: z.string(),
     TWITCH_CLIENT_ID: z.string(),
     TWITCH_CLIENT_SECRET: z.string(),
     WEBHOOK_ALERT: z.string(),
