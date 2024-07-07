950584f35a9a2798108dd0af1cbc414e2dfce61c
notcharliee <saunderscharlie@hotmail.com>
2024-07-07 15:06:00 +0100

    refactor: home page

--- apps/site/src/app/page.tsx
+++ apps/site/src/app/page.tsx
@@ -7,13 +7,8 @@ import { ArrowRightIcon } from "@radix-ui/react-icons"
 import { toast } from "sonner"
 
 import { Header } from "~/components/header"
-import {
-  PageActions,
-  PageDescription,
-  PageHeading,
-} from "~/components/page-heading"
-import { buttonVariants } from "~/components/ui/button"
 import { OrbitingDots } from "~/components/orbiting-dots"
+import { Button } from "~/components/ui/button"
 
 import { siteConfig } from "~/config/site"
 
@@ -35,33 +30,31 @@ export default function HomePage({
   return (
     <main className="flex min-h-screen w-full flex-col">
       <Header />
-      <div className="mx-auto my-auto grid h-full max-w-7xl place-items-center gap-8 p-6 sm:gap-0 sm:px-8 md:px-12">
-      <OrbitingDots />
+      <div className="container grid h-full place-items-center">
+        <OrbitingDots />
         <section className="flex max-w-[980px] flex-col items-center gap-4 py-8 md:py-12 md:pb-8 lg:py-24 lg:pb-20">
           <Link
             href="/docs/changelog"
             className="bg-muted inline-flex items-center rounded-lg px-3 py-1 text-sm font-medium"
           >
             <span className="mr-4">🎉</span>
-            <span className="inline">Check out the v3 changelog.</span>
+            <span className="inline">Check out the v3.6 changelog.</span>
             <ArrowRightIcon className="ml-2 h-4 w-4" />
           </Link>
-          <PageHeading>The all-in-one Discord bot</PageHeading>
-          <PageDescription>{siteConfig.description}</PageDescription>
-          <PageActions>
-            <Link
-              href="/redirect/invite"
-              className={buttonVariants({ size: "xl" })}
-            >
-              Invite the bot
-            </Link>
-            <Link
-              href="/docs"
-              className={buttonVariants({ variant: "outline", size: "xl" })}
-            >
-              Learn More
-            </Link>
-          </PageActions>
+          <h1 className="text-center text-4xl font-bold leading-none tracking-tighter md:text-6xl lg:leading-[1.1]">
+            The all-in-one Discord bot
+          </h1>
+          <p className="text-muted-foreground max-w-[750px] text-center text-lg sm:text-xl">
+            {siteConfig.description}
+          </p>
+          <div className="flex w-full items-center justify-center space-x-4 py-4 md:pb-10">
+            <Button size={"xl"} asChild>
+              <Link href="/redirect/invite">Invite the bot</Link>
+            </Button>
+            <Button variant={"outline"} size={"xl"} asChild>
+              <Link href="/docs">Learn More</Link>
+            </Button>
+          </div>
         </section>
       </div>
       <footer className="sm:border-t sm:backdrop-blur-sm">
