3086e255bea4bc233fd164fa9df9a84f389c9123
notcharliee <saunderscharlie@hotmail.com>
2024-07-15 19:10:30 +0100

    feat: update tictactoe command

--- apps/bot/src/commands/fun/tictactoe.ts
+++ apps/bot/src/commands/fun/tictactoe.ts
@@ -2,103 +2,194 @@ import {
   ActionRowBuilder,
   ButtonBuilder,
   ButtonStyle,
-  EmbedBuilder,
+  ComponentType,
 } from "discord.js"
 import { BotCommandBuilder } from "phasebot/builders"
 
-import { GameSchema } from "@repo/schemas"
+import dedent from "dedent"
 
-import { memberNotFound, PhaseColour, ZeroWidthJoiner } from "~/utils"
+import { ZeroWidthJoiner } from "~/utils"
+
+import type {
+  ButtonInteraction,
+  ChatInputCommandInteraction,
+  GuildMember,
+  Message,
+} from "discord.js"
 
 export default new BotCommandBuilder()
   .setName("tictactoe")
-  .setDescription("Play tic-tac-toe against another user.")
+  .setDescription("Play tic-tac-toe with another member.")
   .setDMPermission(false)
   .addUserOption((option) =>
     option
-      .setName("member")
-      .setDescription("The member you want to select.")
+      .setName("opponent")
+      .setDescription("The opponent you want to select.")
       .setRequired(true),
   )
   .setExecute(async (interaction) => {
-    const member = interaction.options.getUser("member")
+    const host = interaction.member as GuildMember
+    const opponent = interaction.options.getMember("opponent") as GuildMember
+    const players = { "❌": opponent, "⭕": host }
 
-    if (!member) return interaction.reply(memberNotFound())
+    let moves = Array.from({ length: 9 }).map(() => ZeroWidthJoiner)
+    let turn: "❌" | "⭕" = "❌"
+    let tied: boolean = false
+    let winner: {
+      member: GuildMember
+      marker: "❌" | "⭕"
+      winningMoves: number[]
+    }
 
-    const message = await interaction.reply({
-      components: [
-        new ActionRowBuilder<ButtonBuilder>().addComponents(
-          new ButtonBuilder()
-            .setCustomId("tictactoe.1")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.2")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.3")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-        ),
-        new ActionRowBuilder<ButtonBuilder>().addComponents(
-          new ButtonBuilder()
-            .setCustomId("tictactoe.4")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.5")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.6")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-        ),
-        new ActionRowBuilder<ButtonBuilder>().addComponents(
-          new ButtonBuilder()
-            .setCustomId("tictactoe.7")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.8")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-          new ButtonBuilder()
-            .setCustomId("tictactoe.9")
-            .setLabel(ZeroWidthJoiner)
-            .setStyle(ButtonStyle.Secondary),
-        ),
-      ],
-      embeds: [
-        new EmbedBuilder()
-          .setColor(PhaseColour.Primary)
-          .setDescription(`${member} it's your go. Make a move!`)
-          .setTitle("TicTacToe"),
-      ],
-      fetchReply: true,
-    })
+    const makeMove = (move: number) => {
+      const checkWinner = () => {
+        const winningCombinations = [
+          [0, 1, 2],
+          [3, 4, 5],
+          [6, 7, 8],
+          [0, 3, 6],
+          [1, 4, 7],
+          [2, 5, 8],
+          [0, 4, 8],
+          [2, 4, 6],
+        ]
 
-    new GameSchema({
-      id: message.id,
-      type: "TICTACTOE",
-      game_data: {
-        current_turn: {
-          marker: "❌",
-          player: member.id,
-        },
-        moves: [
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-          ZeroWidthJoiner,
-        ],
-      },
-      players: [interaction.user.id, member.id],
-    }).save()
+        for (const combo of winningCombinations) {
+          const [a, b, c] = combo
+
+          if (moves[a] === turn && moves[b] === turn && moves[c] === turn) {
+            return [a, b, c]
+          }
+        }
+
+        return false
+      }
+
+      const checkTie = () =>
+        !winner && moves.every((move) => move !== ZeroWidthJoiner)
+
+      if (moves[move] === ZeroWidthJoiner) {
+        moves[move] = turn
+
+        const isWinner = checkWinner()
+        const isTie = checkTie()
+
+        if (isWinner) {
+          winner = {
+            member: players[turn],
+            marker: turn,
+            winningMoves: isWinner,
+          }
+        } else if (isTie) {
+          tied = true
+        } else {
+          turn = turn === "❌" ? "⭕" : "❌"
+        }
+      }
+    }
+
+    const createMessage = async (
+      interaction: ButtonInteraction | ChatInputCommandInteraction,
+    ) => {
+      const commandMention = `</tictactoe:${interaction.isChatInputCommand() ? interaction.id : interaction.client.application.commands.cache.find((command) => command.name === "tictactoe")!.id}>`
+
+      const content = dedent`
+        :zap: **${players["❌"].displayName}** vs **${players["⭕"].displayName}** :zap:
+        ${
+          winner
+            ? `${winner.member} has won! Play again! ${commandMention}`
+            : tied
+              ? `It's a tie. Play again! ${commandMention}`
+              : `${players[turn]} it's your turn, make a move!`
+        }
+      `
+
+      const components = Array.from({ length: 3 }).map((_, rowIndex) =>
+        new ActionRowBuilder<ButtonBuilder>().addComponents(
+          ...Array.from({ length: 3 }).map((_, buttonIndex) => {
+            const index = rowIndex * 3 + buttonIndex
+
+            return new ButtonBuilder()
+              .setStyle(ButtonStyle.Secondary)
+              .setLabel(moves[index])
+              .setCustomId(
+                winner
+                  ? `games.tictactoe.${index}`
+                  : `games.tictactoe.${index}`,
+              )
+              .setDisabled(
+                winner ? !winner?.winningMoves.includes(index) : tied,
+              )
+          }),
+        ),
+      )
+
+      const awaitMessageComponent = (message: Message) =>
+        message
+          .awaitMessageComponent({
+            filter: (buttonInteraction) =>
+              buttonInteraction.user.id === host.id ||
+              buttonInteraction.user.id === opponent.id,
+            componentType: ComponentType.Button,
+            time: 1000 * 60 * 5,
+          })
+          .then(async (buttonInteraction) => {
+            if (buttonInteraction.user.id !== players[turn].id) {
+              buttonInteraction.reply({
+                content: "Wait your turn!",
+              })
+            } else {
+              await buttonInteraction.deferUpdate()
+              makeMove(+buttonInteraction.customId.split(".")[2])
+            }
+
+            createMessage(buttonInteraction)
+          })
+          .catch(() => {
+            message.edit({
+              content: dedent`
+                ${message.content.split("\n")[0]}
+                Game timed out due to inactivity.
+              `,
+              components: message.components.map((component) => {
+                const json = component.toJSON()
+
+                return {
+                  ...json,
+                  components: json.components.map((button) => {
+                    button.disabled = true
+                    return button
+                  }),
+                }
+              }),
+            })
+
+            message.reply(dedent`
+              Game timed out due to inactivity.
+              You can start a new game with ${commandMention}
+            `)
+          })
+
+      if (interaction.isChatInputCommand()) {
+        const gameMessage = await interaction.reply({
+          content,
+          components,
+          fetchReply: true,
+        })
+
+        awaitMessageComponent(gameMessage)
+      } else {
+        const gameMessage = await interaction.channel!.send({
+          content,
+          components,
+        })
+
+        // Only await the button interaction if the game is not over.
+        if (!winner && !tied) awaitMessageComponent(gameMessage)
+
+        void interaction.message.delete()
+      }
+    }
+
+    createMessage(interaction)
   })
