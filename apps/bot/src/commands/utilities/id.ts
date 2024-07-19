bf7f0862456cedb1335a9bb85e0986a3de6d9565
notcharliee <saunderscharlie@hotmail.com>
2024-07-19 03:14:19 +0100

    feat: split id command into subcommands

--- apps/bot/src/commands/utilities/id.ts
+++ apps/bot/src/commands/utilities/id.ts
@@ -2,25 +2,58 @@ import { BotCommandBuilder } from "phasebot/builders"
 
 export default new BotCommandBuilder()
   .setName("id")
-  .setDescription("Get the ID of a user or role.")
-  .addMentionableOption((option) =>
-    option
-      .setName("mention")
-      .setDescription("The user or role to get the ID of.")
-      .setRequired(true),
+  .setDescription("id")
+  .setDMPermission(false)
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("user")
+      .setDescription("Gets the ID of a user.")
+      .addUserOption((option) =>
+        option
+          .setName("user")
+          .setDescription("The user to get the ID of.")
+          .setRequired(true),
+      ),
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("role")
+      .setDescription("Gets the ID of a role.")
+      .addRoleOption((option) =>
+        option
+          .setName("role")
+          .setDescription("The role to get the ID of.")
+          .setRequired(true),
+      ),
+  )
+  .addSubcommand((subcommand) =>
+    subcommand
+      .setName("channel")
+      .setDescription("Gets the ID of a channel.")
+      .addChannelOption((option) =>
+        option
+          .setName("channel")
+          .setDescription("The channel to get the ID of.")
+          .setRequired(true),
+      ),
+  )
+  .addSubcommand((subcommand) =>
+    subcommand.setName("server").setDescription("Gets the ID of the server."),
   )
   .setExecute((interaction) => {
-    const mention = interaction.options.getMentionable("mention", true)
+    const subcommand = interaction.options.getSubcommand()
 
-    if (mention && "id" in mention) {
-      interaction.reply({
-        content: mention.id,
-        ephemeral: true,
-      })
-    } else {
-      interaction.reply({
-        content: "Could not find the ID of that user or role.",
-        ephemeral: true,
-      })
-    }
+    const id =
+      subcommand === "server"
+        ? interaction.guildId!
+        : subcommand === "channel"
+          ? interaction.options.getChannel("channel", true).id
+          : subcommand === "role"
+            ? interaction.options.getRole("role", true).id
+            : interaction.options.getUser("user", true).id
+
+    void interaction.reply({
+      content: id,
+      ephemeral: true,
+    })
   })
