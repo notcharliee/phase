19799b881cd004665530e1c850f69af96160d80b
notcharliee <saunderscharlie@hotmail.com>
2024-07-22 02:27:08 +0100

    feat: change db config

--- apps/bot/src/lib/db.ts
+++ apps/bot/src/lib/db.ts
@@ -3,8 +3,8 @@ import { Database, mongoose } from "@repo/database"
 import { env } from "~/lib/env"
 
 const db = await new Database({
-  debug: env.NODE_ENV !== "production",
-  cacheConnection: true,
+  cacheConnection: env.NODE_ENV !== "production",
+  debug: false,
 }).init()
 
 export { db, mongoose }
