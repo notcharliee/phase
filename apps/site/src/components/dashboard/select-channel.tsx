0aa51b37200f3d064cae47e69ace911f86d09fe0
notcharliee <saunderscharlie@hotmail.com>
2024-07-31 21:54:10 +0100

    feat: update channel select

--- apps/site/src/components/dashboard/select-channel.tsx
+++ apps/site/src/components/dashboard/select-channel.tsx
@@ -4,7 +4,6 @@ import { CheckIcon } from "@radix-ui/react-icons"
 import * as DiscordAPITypes from "discord-api-types/v10"
 import { type ControllerRenderProps } from "react-hook-form"
 
-import { Spinner } from "~/components/spinner"
 import {
   Select,
   SelectContent,
@@ -16,33 +15,14 @@ import {
 
 import { cn } from "~/lib/utils"
 
-type SelectChannelType = <
-  TFallback extends boolean,
-  TChannel extends DiscordAPITypes.GuildChannelType,
->(
-  props: TFallback extends true
-    ? { fallback: TFallback }
-    : ControllerRenderProps & {
-        categories?: true
-        channels: DiscordAPITypes.APIGuildChannel<DiscordAPITypes.GuildChannelType>[]
-        channelType: TChannel
-        fallback?: TFallback
-      },
-) => JSX.Element
+interface SelectChannelProps extends ControllerRenderProps {
+  channels: DiscordAPITypes.APIGuildChannel<DiscordAPITypes.GuildChannelType>[]
+  channelType: DiscordAPITypes.GuildChannelType
+  categories?: boolean
+  placeholder?: string
+}
 
-export const SelectChannel: SelectChannelType = (props) => {
-  if (props.fallback)
-    return (
-      <Select>
-        <SelectTrigger>
-          <div className="flex items-center gap-2">
-            <Spinner />
-            Loading...
-          </div>
-        </SelectTrigger>
-      </Select>
-    )
-
+export function SelectChannel(props: SelectChannelProps) {
   const channels = props.channels
     .filter((channel) => channel.type === props.channelType)
     .sort((a, b) => a.position - b.position)
@@ -53,16 +33,21 @@ export const SelectChannel: SelectChannelType = (props) => {
     )
     .sort((a, b) => a.position - b.position)
 
-  const channelsWithCategories =
-    props.categories &&
-    new Map(
-      categories.map((category) => [
-        category.id,
-        channels.filter((channel) => channel.parent_id === category.id),
-      ]),
-    )
+  const categoriesWithChannels: [string, typeof channels][] | undefined =
+    props.categories
+      ? categories.map((category) => [
+          category.name,
+          channels.filter((channel) => channel.parent_id === category.id),
+        ])
+      : undefined
 
-  const placeholder = `Select a ${props.channelType === DiscordAPITypes.ChannelType.GuildCategory ? "category" : "channel"}`
+  const orphanedChannels = categoriesWithChannels
+    ? channels.filter((channel) => !channel.parent_id)
+    : undefined
+
+  const placeholder =
+    props.placeholder ??
+    `Select a ${props.channelType === DiscordAPITypes.ChannelType.GuildCategory ? "category" : "channel"}`
 
   const selectedChannel = channels.find((channel) => channel.id === props.value)
 
@@ -80,52 +65,78 @@ export const SelectChannel: SelectChannelType = (props) => {
       </SelectTrigger>
       <SelectContent className="no-scroll-buttons">
         <div className="max-h-[30vh] overflow-x-hidden overflow-y-scroll p-1">
-          {channelsWithCategories
-            ? Array.from(channelsWithCategories.keys())
-                .filter((key) => channelsWithCategories.get(key)!.length)
-                .map((key) => (
-                  <SelectGroup key={key}>
-                    <SelectLabel>
-                      {categories.find((category) => category.id === key)!.name}
-                    </SelectLabel>
-                    {channelsWithCategories.get(key)!.map((channel) => (
-                      <SelectItem
-                        value={
-                          channel.id === props.value ? "deselect" : channel.id
-                        }
-                        key={channel.id}
-                        className={cn(
-                          props.value !== channel.id && "text-muted-foreground",
-                          "pr-2 [&>*:nth-child(2)]:w-full",
+          {categoriesWithChannels ? (
+            <>
+              {orphanedChannels && (
+                <SelectGroup>
+                  <SelectLabel className="sr-only">
+                    Orphaned Channels
+                  </SelectLabel>
+                  {orphanedChannels.map((channel) => (
+                    <SelectItem
+                      value={
+                        channel.id === props.value ? "deselect" : channel.id
+                      }
+                      key={channel.id}
+                      className={cn(
+                        props.value !== channel.id && "text-muted-foreground",
+                        "pr-2 [&>*:nth-child(2)]:w-full",
+                      )}
+                    >
+                      <div className="flex items-center justify-between">
+                        {channel.name}
+                        {props.value === channel.id && (
+                          <CheckIcon className="ml-auto h-4 w-4" />
                         )}
-                      >
-                        <div className="flex items-center justify-between">
-                          {channel.name}
-                          {props.value === channel.id && (
-                            <CheckIcon className="ml-auto h-4 w-4" />
-                          )}
-                        </div>
-                      </SelectItem>
-                    ))}
-                  </SelectGroup>
-                ))
-            : channels.map((channel) => (
-                <SelectItem
-                  value={channel.id === props.value ? "deselect" : channel.id}
-                  key={channel.id}
-                  className={cn(
-                    props.value !== channel.id && "text-muted-foreground",
-                    "pr-2 [&>*:nth-child(2)]:w-full",
-                  )}
-                >
-                  <div className="flex items-center justify-between">
-                    {channel.name}
-                    {props.value === channel.id && (
-                      <CheckIcon className="ml-auto h-4 w-4" />
-                    )}
-                  </div>
-                </SelectItem>
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
+              )}
+              {categoriesWithChannels.map(([categoryName, channels]) => (
+                <SelectGroup key={categoryName}>
+                  <SelectLabel>{categoryName}</SelectLabel>
+                  {channels.map((channel) => (
+                    <SelectItem
+                      value={
+                        channel.id === props.value ? "deselect" : channel.id
+                      }
+                      key={channel.id}
+                      className={cn(
+                        props.value !== channel.id && "text-muted-foreground",
+                        "pr-2 [&>*:nth-child(2)]:w-full",
+                      )}
+                    >
+                      <div className="flex items-center justify-between">
+                        {channel.name}
+                        {props.value === channel.id && (
+                          <CheckIcon className="ml-auto h-4 w-4" />
+                        )}
+                      </div>
+                    </SelectItem>
+                  ))}
+                </SelectGroup>
               ))}
+            </>
+          ) : (
+            channels.map((channel) => (
+              <SelectItem
+                value={channel.id === props.value ? "deselect" : channel.id}
+                key={channel.id}
+                className={cn(
+                  props.value !== channel.id && "text-muted-foreground",
+                  "pr-2 [&>*:nth-child(2)]:w-full",
+                )}
+              >
+                <div className="flex items-center justify-between">
+                  {channel.name}
+                  {props.value === channel.id && (
+                    <CheckIcon className="ml-auto h-4 w-4" />
+                  )}
+                </div>
+              </SelectItem>
+            ))
+          )}
         </div>
       </SelectContent>
     </Select>
