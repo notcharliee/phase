c1d7845531c5cdeacb457a4d926a020e98d5c259
notcharliee <saunderscharlie@hotmail.com>
2024-08-04 09:54:32 +0100

    fix: specify ffmpeg path

--- apps/bot/src/lib/clients/distube/index.ts
+++ apps/bot/src/lib/clients/distube/index.ts
@@ -2,10 +2,9 @@ import { getClient } from "phasebot"
 
 import { YouTubePlugin } from "@distube/youtube"
 import { DisTube, Events } from "distube"
+import ffmpegPath from "ffmpeg-static"
 
 import { getCookies } from "./cookies"
-
-export { getCookies }
 
 const globalForDistubeClient = globalThis as unknown as {
   distubeClient: DisTube | undefined
@@ -15,6 +14,7 @@ export const distubeClient =
   globalForDistubeClient.distubeClient ??
   new DisTube(getClient(), {
     plugins: [new YouTubePlugin({ cookies: await getCookies() })],
+    ffmpeg: { path: ffmpegPath! },
     emitAddListWhenCreatingQueue: false,
     emitAddSongWhenCreatingQueue: false,
     emitNewSongOnly: false,
@@ -26,3 +26,5 @@ distubeClient.on(Events.FINISH, (queue) => queue.stop())
 distubeClient.on(Events.DISCONNECT, (queue) => queue.stop())
 distubeClient.on(Events.DELETE_QUEUE, (queue) => queue.voice.leave())
 distubeClient.on(Events.ERROR, (error) => console.error(error))
+
+export { getCookies }
