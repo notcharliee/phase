f477968d547fcfae5d7cbea728d8394b5ff8571d
notcharliee <saunderscharlie@hotmail.com>
2024-08-05 15:15:23 +0100

    refactor: merge distube client files

--- apps/bot/src/lib/clients/distube.ts
+++ apps/bot/src/lib/clients/distube.ts
@@ -1,3 +1,8 @@
+import { getClient } from "phasebot"
+
+import { YouTubePlugin } from "@distube/youtube"
+import { DisTube, Events } from "distube"
+import ffmpegPath from "ffmpeg-static"
 import puppeteer from "puppeteer-extra"
 import stealthPlugin from "puppeteer-extra-plugin-stealth"
 
@@ -95,3 +100,24 @@ export async function getCookies(refresh: boolean = false): Promise<Cookie[]> {
   )
   return []
 }
+
+const globalForDistubeClient = globalThis as unknown as {
+  distubeClient: DisTube | undefined
+}
+
+export const distubeClient =
+  globalForDistubeClient.distubeClient ??
+  new DisTube(getClient(), {
+    plugins: [new YouTubePlugin({ cookies: await getCookies() })],
+    ffmpeg: { path: ffmpegPath! },
+    emitAddListWhenCreatingQueue: false,
+    emitAddSongWhenCreatingQueue: false,
+    emitNewSongOnly: false,
+    savePreviousSongs: false,
+    nsfw: false,
+  })
+
+distubeClient.on(Events.FINISH, (queue) => queue.stop())
+distubeClient.on(Events.DISCONNECT, (queue) => queue.stop())
+distubeClient.on(Events.DELETE_QUEUE, (queue) => queue.voice.leave())
+distubeClient.on(Events.ERROR, (error) => console.error(error))
