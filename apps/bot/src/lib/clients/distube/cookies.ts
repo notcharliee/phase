7330f018b9bd332e93e7b167799323bd388cdbc2
notcharliee <saunderscharlie@hotmail.com>
2024-08-05 15:13:03 +0100

    feat: merge distube cookies functions

--- apps/bot/src/lib/clients/distube/cookies.ts
+++ apps/bot/src/lib/clients/distube/cookies.ts
@@ -15,85 +15,83 @@ puppeteer.use(stealth)
 export async function getCookies(refresh: boolean = false): Promise<Cookie[]> {
   let attempt = 0
 
-  // if refresh is true, wait for the cookies to refresh first
-  if (refresh) await refreshCookies()
-
   while (attempt < 3) {
     attempt++
 
     const cookiesFile = Bun.file(".cookies.json")
     const cookiesFileExists = await cookiesFile.exists()
 
-    if (cookiesFileExists) {
+    if (cookiesFileExists && !refresh) {
       try {
         const cookiesJson = await cookiesFile.json()
         return cookiesJson as Cookie[]
       } catch (e) {
         console.error("Failed to parse cookies JSON:", e)
       }
+    }
+
+    // Refresh cookies if the file doesn't exist or if refresh is requested
+    const browser = await puppeteer.launch({
+      headless: true,
+      args: ["--no-sandbox", "--disable-setuid-sandbox"],
+      ignoreDefaultArgs: ["--disable-extensions"],
+    })
+
+    const page = await browser.newPage()
+    await page.goto("https://www.youtube.com", { waitUntil: "networkidle2" })
+
+    try {
+      // Attempt to click "sign in" button
+      await page.click(
+        "#topbar > div.top-buttons.style-scope.ytd-consent-bump-v2-lightbox > div:nth-child(2) > ytd-button-renderer > yt-button-shape > a",
+      )
+    } catch (e) {
+      // Fallback to clicking "sign in" on navbar
+      await page.click("#buttons > ytd-button-renderer > yt-button-shape > a")
+    }
+
+    // Enter email
+    await page.waitForSelector("#identifierId", { visible: true })
+    await page.type("#identifierId", env.GOOGLE_EMAIL)
+    await page.click("#identifierNext")
+
+    // Enter password
+    await page.waitForSelector("#password", { visible: true })
+    await page.type("#password input", env.GOOGLE_PASSWORD)
+
+    await page.evaluate(
+      // @ts-expect-error because page.click() on '#passwordNext' is not working
+      (selector) => document.querySelector(selector).click(),
+      "#passwordNext",
+    )
+
+    // Handle potential security prompt
+    try {
+      const notNowSelector =
+        "#yDmH0d > c-wiz:nth-child(9) > div > div > div > div.L5MEH.Bokche.ypEC4c > div.lq3Znf > div:nth-child(1) > button > span"
+
+      await page.waitForSelector(notNowSelector, { timeout: 10000 })
+      await page.click(notNowSelector)
+    } catch (e) {
+      await page.goto("https://www.youtube.com", { waitUntil: "networkidle2" })
+    }
+
+    const cookies = await page.cookies()
+    await browser.close()
+
+    if (cookies.length >= 10) {
+      await Bun.write(".cookies.json", JSON.stringify(cookies))
+      return cookies
     } else {
-      await refreshCookies()
-      await new Promise((resolve) => setTimeout(resolve, 1000))
+      console.error("Failed to authenticate with Google")
+      if (attempt >= 3) {
+        return []
+      }
     }
   }
 
-  throw new Error(
-    "Failed to retrieve or parse cookies from .cookies.json after 3 attempts",
+  console.log(
+    "Failed to retrieve or parse cookies from .cookies.json after 3 attempts, so falling back to guest cookies.",
   )
-}
-
-export async function refreshCookies() {
-  const browser = await puppeteer.launch({
-    headless: true,
-    args: ["--no-sandbox", "--disable-setuid-sandbox"],
-    ignoreDefaultArgs: ["--disable-extensions"],
-  })
-
-  const page = await browser.newPage()
-  await page.goto("https://www.youtube.com", { waitUntil: "networkidle2" })
-
-  try {
-    // press "sign in" button on youtube in accept cookie usage
-    await page.click(
-      "#topbar > div.top-buttons.style-scope.ytd-consent-bump-v2-lightbox > div:nth-child(2) > ytd-button-renderer > yt-button-shape > a",
-    )
-  } catch (e) {
-    // press "sign in" on navbar
-    await page.click("#buttons > ytd-button-renderer > yt-button-shape > a")
-  }
-
-  // type email in form
-  await page.waitForSelector("#identifierId", { visible: true })
-  await page.type("#identifierId", env.GOOGLE_EMAIL)
-  await page.click("#identifierNext")
-
-  // type password in form
-  await page.waitForSelector("#password", { visible: true })
-  await page.type("#password input", env.GOOGLE_PASSWORD)
-
-  await page.evaluate(
-    // @ts-expect-error because page.click() on '#passwordNext' is not working
-    (selector) => document.querySelector(selector).click(),
-    "#passwordNext",
-  )
-
-  // skip measures of security (if google asks)
-  try {
-    const NotNowSelector =
-      "#yDmH0d > c-wiz:nth-child(9) > div > div > div > div.L5MEH.Bokche.ypEC4c > div.lq3Znf > div:nth-child(1) > button > span"
-    await page.waitForSelector(NotNowSelector, { timeout: 1e4 })
-    await page.click(NotNowSelector)
-  } catch (e) {
-    await page.goto("https://www.youtube.com", { waitUntil: "networkidle2" })
-  }
-
-  const cookies = await page.cookies()
-
-  await browser.close()
-
-  if (cookies.length < 10) {
-    console.error("Failed to authenticate with Google")
-  }
-
-  await Bun.write(".cookies.json", JSON.stringify(cookies))
+  return []
 }
