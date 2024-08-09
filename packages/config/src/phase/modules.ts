8e16a0f0e827accfdbd61f1a90a1e418a47e309f
notcharliee <saunderscharlie@hotmail.com>
2024-08-09 04:21:03 +0100

    refactor: modules config

--- packages/config/src/phase/modules.ts
+++ packages/config/src/phase/modules.ts
@@ -40,80 +40,67 @@ export const moduleTags = [
 ] as const
 
 export const modules = {
-  // "2uA4lWg9gW":
-  AuditLogs: {
+  [ModuleId.AuditLogs]: {
     name: "Audit Logs",
     description: `Logs audit log events in the channel of your choice.`,
     tags: ["Moderation"],
   },
-  // "2Cwqwmk84t":
-  AutoMessages: {
+  [ModuleId.AutoMessages]: {
     name: "Auto Messages",
     description: `Sends a message to a channel at a set interval.`,
     tags: ["Utility"],
   },
-  // "56Fd2OVE6Q":
-  AutoRoles: {
+  [ModuleId.AutoRoles]: {
     name: "Auto Roles",
     description: `Assigns roles to new members of your server as soon as they join.`,
     tags: ["Utility"],
   },
-  // "7ImdmYll1t":
-  BumpReminders: {
+  [ModuleId.BumpReminders]: {
     name: "Bump Reminders",
     description: `Reminds members to re-bump your server after a set period.`,
     tags: ["Engagement"],
   },
-  // "5QvEVG03KK":
-  Counters: {
+  [ModuleId.Counters]: {
     name: "Counters",
     description: `Displays server statistics directly in your channel names.`,
     tags: ["Utility", "New"],
   },
-  // "1uWxuEkesd":
-  Forms: {
+  [ModuleId.Forms]: {
     name: "Forms",
     description: `Adds dynamic application forms within the bot's DMs.`,
     tags: ["Utility"],
   },
-  // "67NXpiGedU":
-  JoinToCreates: {
+  [ModuleId.JoinToCreates]: {
     name: "Join to Creates",
     description: `Creates a temporary voice channel, then deletes it once all members have left.`,
     tags: ["Utility"],
   },
-  // "23OmPKCPG0":
-  Levels: {
+  [ModuleId.Levels]: {
     name: "Levels",
     description: `Rewards activity with XP and level-ups as members reach milestones.`,
     tags: ["Engagement"],
   },
-  // "3MOVeYkTa1":
-  ReactionRoles: {
+  [ModuleId.ReactionRoles]: {
     name: "Reaction Roles",
     description: `Lets members self-assign roles by reacting to a message.`,
     tags: ["Utility"],
   },
-  // "7i5YEGu2Fj":
-  Tickets: {
+  [ModuleId.Tickets]: {
     name: "Tickets",
     description: `Lets members create individualised staff assistance channels.`,
     tags: ["Utility"],
   },
-  // "1q4EDddac2":
-  TwitchNotifications: {
+  [ModuleId.TwitchNotifications]: {
     name: "Twitch Notifications",
     description: `Notifies your server when a Twitch streamer goes live.`,
     tags: ["Notifications"],
   },
-  // "3aqDA1m91r":
-  Warnings: {
+  [ModuleId.Warnings]: {
     name: "Warnings",
     description: `Helps moderators add, remove, and track member warnings.`,
     tags: ["Moderation"],
   },
-  // "44Xaf7KFo1":
-  WelcomeMessages: {
+  [ModuleId.WelcomeMessages]: {
     name: "Welcome Messages",
     description: `Gives new members a warm welcome to your server.`,
     tags: ["Engagement"],
