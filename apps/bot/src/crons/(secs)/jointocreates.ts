3adf8dd22c89b25883e6f0a1daf119cae2d3d27e
notcharliee <saunderscharlie@hotmail.com>
2024-08-22 20:05:18 +0100

    refactor: jtc channel clean-up logic

---
+++ apps/bot/src/crons/(secs)/jointocreates.ts
@@ -0,0 +1,62 @@
+import { BotCronBuilder } from "phasebot/builders"
+
+import { ModuleId } from "@repo/config/phase/modules.ts"
+
+import { cache } from "~/lib/cache"
+import { db } from "~/lib/db"
+
+import type { VoiceChannel } from "discord.js"
+
+export default new BotCronBuilder()
+  .setPattern("*/5 * * * * *") // every 5 seconds
+  .setExecute(async (client) => {
+    const guildDocsWithActiveJTCs = (await cache.guilds.values()).filter(
+      (guildDoc) => guildDoc.modules?.[ModuleId.JoinToCreates]?.active.length,
+    )
+
+    const channelsToDelete: Promise<void>[] = []
+    const guildIdsToUpdate: string[] = []
+
+    for (const guildDoc of guildDocsWithActiveJTCs) {
+      const activeJTCChannelIDs =
+        guildDoc.modules![ModuleId.JoinToCreates]!.active
+
+      const activeJTCChannels = client.channels.cache.filter(
+        (channel): channel is VoiceChannel =>
+          activeJTCChannelIDs.includes(channel.id),
+      )
+
+      for (const [, activeJTCChannel] of activeJTCChannels) {
+        const activeJTCChannelMembers = activeJTCChannel.members.filter(
+          (member) => !member.user.bot,
+        )
+
+        if (activeJTCChannelMembers.size === 0) {
+          channelsToDelete.push(
+            activeJTCChannel
+              .delete()
+              .then(() => {})
+              .catch((err) => {
+                console.error(
+                  `Failed to delete an active JTC channel ${activeJTCChannel.id}:`,
+                  err,
+                )
+              }),
+          )
+        }
+      }
+
+      guildIdsToUpdate.push(guildDoc.id)
+    }
+
+    if (channelsToDelete.length > 0) {
+      await Promise.all(channelsToDelete)
+    }
+
+    if (guildIdsToUpdate.length > 0) {
+      await db.guilds.updateMany(
+        { id: { $in: guildIdsToUpdate } },
+        { $set: { [`modules.${ModuleId.JoinToCreates}.active`]: [] } },
+      )
+    }
+  })
--- apps/bot/src/events/jointocreate/voiceUpdate.ts
+++ apps/bot/src/events/jointocreate/voiceUpdate.ts
@@ -9,45 +9,47 @@ import { db } from "~/lib/db"
 export default new BotEventBuilder()
   .setName("voiceStateUpdate")
   .setExecute(async (_, oldVoice, newVoice) => {
+    if (!newVoice.channelId) return
+
     const guildDoc = await cache.guilds.get(oldVoice.guild.id)
     const joinToCreateModule = guildDoc?.modules?.[ModuleId.JoinToCreates]
 
-    if (!guildDoc || !joinToCreateModule?.enabled) return
-
     if (
-      oldVoice.channel &&
-      "active" in joinToCreateModule &&
-      joinToCreateModule.active.includes(oldVoice.channel.id) &&
-      oldVoice.channel.members.filter((member) => !member.user.bot).size === 0
+      !guildDoc ||
+      !joinToCreateModule?.enabled ||
+      newVoice.channelId !== joinToCreateModule.channel
     ) {
-      void oldVoice.channel.delete()
-
-      const newActive = joinToCreateModule.active.filter(
-        (activeChannelId) => activeChannelId !== oldVoice.channel!.id,
-      )
-
-      void db.guilds.updateOne(
-        { id: oldVoice.guild.id },
-        { $set: { [`modules.${ModuleId.JoinToCreates}.active`]: newActive } },
-      )
+      return
     }
 
-    if (newVoice.channelId === joinToCreateModule.channel) {
-      const newVoiceChannel = await newVoice.guild.channels.create({
+    const newVoiceChannel = await newVoice.guild.channels
+      .create({
         name: newVoice.member?.user.username ?? "voice channel",
         type: ChannelType.GuildVoice,
         parent: joinToCreateModule.category,
       })
+      .catch((err) => {
+        console.error(
+          `Failed to create a new JTC channel in guild ${newVoice.guild.id}:`,
+          err,
+        )
+      })
 
-      void newVoice.setChannel(newVoiceChannel)
-
-      const newActive = !("active" in joinToCreateModule)
-        ? []
-        : joinToCreateModule.active.concat(newVoiceChannel.id)
-
-      void db.guilds.updateOne(
+    if (newVoiceChannel) {
+      void (await db.guilds.updateOne(
         { id: newVoice.guild.id },
-        { $set: { [`modules.${ModuleId.JoinToCreates}.active`]: newActive } },
-      )
+        {
+          $push: {
+            [`modules.${ModuleId.JoinToCreates}.active`]: newVoiceChannel.id,
+          },
+        },
+      ))
+
+      await newVoice.setChannel(newVoiceChannel).catch((err) => {
+        console.error(
+          `Failed to move a member into a new JTC channel in guild ${newVoice.guild.id}:`,
+          err,
+        )
+      })
     }
   })
