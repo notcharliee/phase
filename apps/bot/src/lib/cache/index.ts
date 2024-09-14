488a40fe1888c4d00bc36632edb224afb187aeb3
notcharliee <saunderscharlie@hotmail.com>
2024-09-14 17:24:37 +0100

    refactor: remove old caching implementation and its related files

--- apps/bot/src/lib/cache/index.ts
+++
@@ -1,9 +0,0 @@
-import { configsCache } from "./config"
-import { guildsCache } from "./guilds"
-import { twitchStreamStatusesCache } from "./twitch"
-
-export const cache = {
-  configs: configsCache,
-  guilds: guildsCache,
-  twitchStreamStatuses: twitchStreamStatusesCache,
-}
--- apps/bot/src/lib/cache/twitch.ts
+++
@@ -1,6 +0,0 @@
-import { PersistentCache } from "@repo/cache"
-
-// stores the streamer id and current stream status
-export const twitchStreamStatusesCache = await new PersistentCache({
-  filePath: "./.cache/twitch-stream-statuses.bson",
-}).init<Record<string, boolean>>()
