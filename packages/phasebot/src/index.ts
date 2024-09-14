51f7be27f1abcc4eb65b14556bf873ef90116c06
notcharliee <saunderscharlie@hotmail.com>
2024-09-14 10:53:01 +0100

    refactor: remove deprecated botEvent function in favor of BotEventBuilder

--- packages/phasebot/src/index.ts
+++ packages/phasebot/src/index.ts
@@ -1,12 +1,1 @@
-import type { Client, ClientEvents } from "discord.js"
-
 export * from "~/client"
-
-/**
- * @deprecated Use `BotEventBuilder` instead.
- * @since v0.4.0
- */
-export const botEvent = <T extends keyof ClientEvents>(
-  name: T,
-  execute: (client: Client<true>, ...args: ClientEvents[T]) => unknown,
-) => ({ name, execute })
