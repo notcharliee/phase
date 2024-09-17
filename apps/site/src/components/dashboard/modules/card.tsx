eafb9e7608014ec5e381f6b295d3fc870c068733
notcharliee <saunderscharlie@hotmail.com>
2024-09-17 12:19:26 +0100

    fix: remove unnecessary form field in ModuleCard component

--- apps/site/src/components/dashboard/modules/card.tsx
+++ apps/site/src/components/dashboard/modules/card.tsx
@@ -36,7 +36,7 @@ import { defaultValues } from "~/app/dashboard/modules/_utils/client"
 
 import type { ModuleId } from "@repo/config/phase/modules.ts"
 import type { ModulesFormValues } from "~/types/dashboard"
-import type { ControllerRenderProps, UseFormReturn } from "react-hook-form"
+import type { UseFormReturn } from "react-hook-form"
 
 export enum ModuleCardState {
   Undefined = 0,
@@ -72,15 +72,14 @@ export function ModuleCard({
   const isDisabled = isEditMode && isSubmitting
 
   const handleCardClick = useCallback(
-    (
-      event: React.MouseEvent<HTMLDivElement, MouseEvent>,
-      field: ControllerRenderProps<ModulesFormValues, ModuleId>,
-    ) => {
+    (event: React.MouseEvent<HTMLDivElement, MouseEvent>) => {
       if (!isManageMode) return
 
       event.preventDefault()
 
-      if (!field.value) {
+      const field = form.getValues()[id]
+
+      if (!field) {
         form.setValue(id, defaultValues[id], { shouldDirty: true })
       } else {
         form.setValue(id, undefined, { shouldDirty: true })
@@ -92,85 +91,73 @@ export function ModuleCard({
   if (isEditMode && isUndefined) return null
 
   return (
-    <FormField
-      control={form.control}
-      name={id}
-      render={({ field }) => (
-        <Credenza>
-          <CredenzaTrigger asChild>
-            <Card
-              role="button"
-              tabIndex={0}
-              aria-disabled={isDisabled}
-              onClick={(event) => handleCardClick(event, field)}
-              className={cn(
-                "focus-visible:ring-ring group relative h-full cursor-pointer transition-all focus-visible:outline-none focus-visible:ring-1 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
-                isDirty && "border-primary bg-primary/5",
-                isInvalid && "border-destructive bg-destructive/5",
-                isManageMode &&
-                  isUndefined &&
-                  "border-muted-foreground border-dashed opacity-50",
-              )}
-            >
-              <CardHeader className="flex flex-col space-y-1.5">
-                <CardTitle>{name}</CardTitle>
-                <div className="inline-flex gap-0.5">
-                  {tags.map((tag) => (
-                    <Badge variant="secondary" key={tag}>
-                      {tag}
-                    </Badge>
-                  ))}
-                </div>
-                {isEditMode && (
-                  <FormField
-                    control={form.control}
-                    name={`${id}.enabled`}
-                    render={({ field: { value, onChange, ...field } }) => (
-                      <FormItem className="absolute right-6 top-6 !m-0">
-                        <FormHeader className="sr-only">
-                          <FormLabel>Enabled</FormLabel>
-                          <FormDescription>
-                            Whether or not the module is enabled
-                          </FormDescription>
-                        </FormHeader>
-                        <FormControl>
-                          <Switch
-                            {...field}
-                            disabled={isDisabled}
-                            checked={value}
-                            defaultChecked={value}
-                            className="absolute right-0 top-0 !m-0"
-                            onCheckedChange={onChange}
-                            onClick={(event) => event.stopPropagation()}
-                          />
-                        </FormControl>
-                      </FormItem>
-                    )}
-                  />
+    <Credenza>
+      <CredenzaTrigger asChild>
+        <Card
+          role="button"
+          tabIndex={0}
+          aria-disabled={isDisabled}
+          onClick={handleCardClick}
+          className={cn(
+            "focus-visible:ring-ring group relative h-full cursor-pointer transition-all focus-visible:outline-none focus-visible:ring-1 aria-disabled:cursor-not-allowed aria-disabled:opacity-50",
+            isDirty && "border-primary bg-primary/5",
+            isInvalid && "border-destructive bg-destructive/5",
+            isManageMode &&
+              isUndefined &&
+              "border-muted-foreground border-dashed opacity-50",
+          )}
+        >
+          <CardHeader className="flex flex-col space-y-1.5">
+            <CardTitle>{name}</CardTitle>
+            <div className="inline-flex gap-0.5">
+              {tags.map((tag) => (
+                <Badge variant="secondary" key={tag}>
+                  {tag}
+                </Badge>
+              ))}
+            </div>
+            {isEditMode && (
+              <FormField
+                control={form.control}
+                name={`${id}.enabled`}
+                render={({ field: { value, onChange, ...field } }) => (
+                  <FormItem className="absolute right-6 top-6 !m-0">
+                    <FormHeader className="sr-only">
+                      <FormLabel>Enabled</FormLabel>
+                      <FormDescription>
+                        Whether or not the module is enabled
+                      </FormDescription>
+                    </FormHeader>
+                    <FormControl>
+                      <Switch
+                        {...field}
+                        disabled={isDisabled}
+                        checked={value}
+                        defaultChecked={value}
+                        className="absolute right-0 top-0 !m-0"
+                        onCheckedChange={onChange}
+                        onClick={(event) => event.stopPropagation()}
+                      />
+                    </FormControl>
+                  </FormItem>
                 )}
-              </CardHeader>
-              <CardContent>
-                <CardDescription>{description}</CardDescription>
-              </CardContent>
-            </Card>
-          </CredenzaTrigger>
-          <CredenzaContent className="max-h-[90%] overflow-auto lg:max-h-[80%]">
-            <FormItem className="md:space-y-8">
-              <CredenzaHeader>
-                <FormLabel asChild>
-                  <CredenzaTitle>{name}</CredenzaTitle>
-                </FormLabel>
-                <FormDescription asChild>
-                  <CredenzaDescription>{description}</CredenzaDescription>
-                </FormDescription>
-              </CredenzaHeader>
-              <FormControl>
-                <CredenzaBody>{children}</CredenzaBody>
-              </FormControl>
-            </FormItem>
-          </CredenzaContent>
-        </Credenza>
-      )}
-    />
+              />
+            )}
+          </CardHeader>
+          <CardContent>
+            <CardDescription>{description}</CardDescription>
+          </CardContent>
+        </Card>
+      </CredenzaTrigger>
+      <CredenzaContent className="max-h-[90%] overflow-auto lg:max-h-[80%]">
+        <FormItem className="md:space-y-8">
+          <CredenzaHeader>
+            <CredenzaTitle>{name}</CredenzaTitle>
+            <CredenzaDescription>{description}</CredenzaDescription>
+          </CredenzaHeader>
+          <CredenzaBody>{children}</CredenzaBody>
+        </FormItem>
+      </CredenzaContent>
+    </Credenza>
   )
 }
