7b05aa5e586da04518553e229967938bb91d2e4c
notcharliee <saunderscharlie@hotmail.com>
2024-09-20 23:24:03 +0100

    feat: extend bot env with railway preset

--- apps/bot/src/lib/env.ts
+++ apps/bot/src/lib/env.ts
@@ -1,7 +1,9 @@
 import { createEnv } from "@t3-oss/env-core"
+import { railway } from "@t3-oss/env-core/presets"
 import { z } from "zod"
 
 export const env = createEnv({
+  extends: [railway()],
   server: {
     NODE_ENV: z.enum(["development", "test", "production"]),
     MONGODB_URI: z.string(),
