7b71e3db2ee5b4201ba5864f516860a7a131f42c
notcharliee <saunderscharlie@hotmail.com>
2024-10-01 07:45:03 +0100

    feat: remove youtube cookies cron job

--- apps/bot/src/app/crons/(days)/music.ts
+++
@@ -1,7 +0,0 @@
-import { BotCronBuilder } from "phasebot/builders"
-
-export default new BotCronBuilder()
-  .setPattern("0 0 * * *")
-  .setExecute(async (client) => {
-    client.music.youtube.cookies = await client.music.getCookies(true)
-  })
