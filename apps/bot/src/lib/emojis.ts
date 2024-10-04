754afe5ab580236ce0ab1edfb13d7c38365d0c15
notcharliee <saunderscharlie@hotmail.com>
2024-10-04 08:14:39 +0100

    feat: add ticket-related emojis and icons for locked, unlocked, and delete actions

--- apps/bot/src/lib/emojis.ts
+++ apps/bot/src/lib/emojis.ts
@@ -6,12 +6,18 @@ export const ApplicationEmojis = {
   Connect4_Empty: formatEmoji("1287414239098835025"),
   Connect4_Player1: formatEmoji("1287414378412642304"),
   Connect4_Player2: formatEmoji("1287539813058084914"),
+  Ticket_Locked: formatEmoji("1291406900977729598"),
+  Ticket_Unlocked: formatEmoji("1291406915682832515"),
+  Ticket_Delete: formatEmoji("1291416942485438555"),
 }
 
 export const FallbackEmojis = {
   Connect4_Empty: "⬛",
   Connect4_Player1: "⚪",
   Connect4_Player2: "🟣",
+  Ticket_Locked: "🔒",
+  Ticket_Unlocked: "🔓",
+  Ticket_Delete: "🗑️",
 } satisfies Record<keyof typeof ApplicationEmojis, string>
 
 export const Emojis = {
