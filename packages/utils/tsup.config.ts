94473bdb6723404871d13b6d5bf5199bb574a5d8
notcharliee <saunderscharlie@hotmail.com>
2024-10-06 19:44:08 +0100

    refactor: update utils tsup config to enable sourcemaps and correct format settings

--- packages/utils/tsup.config.ts
+++ packages/utils/tsup.config.ts
@@ -2,10 +2,10 @@ import { defineConfig } from "tsup"
 
 export default defineConfig({
   entry: ["./src/variables/index.ts", "./src/modules/index.ts"],
+  format: ["esm"],
   clean: true,
   dts: true,
   minify: true,
   splitting: false,
-  format: ["esm"],
-  sourcemap: "inline",
+  sourcemap: true,
 })
