6f26fb7a0b32b07b6bdf1fe853975f635cfa11b8
notcharliee <saunderscharlie@hotmail.com>
2024-10-07 10:25:49 +0100

    feat: update cat command to use `attachment://` images and add api attribution

--- apps/bot/src/app/commands/(fun)/cat.ts
+++ apps/bot/src/app/commands/(fun)/cat.ts
@@ -1,18 +1,38 @@
-import { EmbedBuilder } from "discord.js"
+import { AttachmentBuilder } from "discord.js"
 import { BotCommandBuilder } from "phasebot/builders"
 
-import { PhaseColour } from "~/lib/enums"
+import { BotErrorMessage } from "~/structures/BotError"
+import { CustomMessageBuilder } from "~/structures/CustomMessageBuilder"
+
+const API_URL = new URL("https://cataas.com/cat")
 
 export default new BotCommandBuilder()
   .setName("cat")
   .setDescription("Gives you a random picture of a cat.")
   .setExecute(async (interaction) => {
-    void interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("Random Cat")
-          .setColor(PhaseColour.Primary)
-          .setImage("https://cataas.com/cat?t=" + Date.now()),
-      ],
-    })
+    await interaction.deferReply()
+
+    try {
+      const response = await fetch(API_URL.toString())
+      const arrayBuffer = await response.arrayBuffer()
+      const buffer = Buffer.from(arrayBuffer)
+
+      const attachment = new AttachmentBuilder(buffer)
+        .setName("random-cat.png")
+        .setDescription("Random cat image")
+
+      return void interaction.editReply(
+        new CustomMessageBuilder().setFiles(attachment).setEmbeds((embed) => {
+          return embed
+            .setColor("Primary")
+            .setTitle("Random Cat")
+            .setImage(attachment)
+            .setFooter({ text: `Made with ${API_URL.hostname} 🤍` })
+        }),
+      )
+    } catch {
+      return void interaction.editReply(
+        new BotErrorMessage("Failed to fetch a cat image."),
+      )
+    }
   })
