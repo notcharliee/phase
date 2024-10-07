d8b2f3305426f1286e8df6dcb0d9abb8e8cd2e47
notcharliee <saunderscharlie@hotmail.com>
2024-10-07 11:09:21 +0100

    feat: add rare responses for coinflip command

--- apps/bot/src/app/commands/(fun)/coinflip.ts
+++ apps/bot/src/app/commands/(fun)/coinflip.ts
@@ -4,7 +4,23 @@ export default new BotCommandBuilder()
   .setName("coinflip")
   .setDescription("Flips a coin.")
   .setExecute(async (interaction) => {
-    void interaction.reply(
-      ["It's **Tails**!", "It's **Heads**!"][Math.floor(Math.random() * 2)]!,
-    )
+    await interaction.deferReply()
+
+    const commonResponses = ["It's **Heads**!", "It's **Tails**!"]
+
+    const rareResponses = [
+      "The coin landed on its side :/",
+      "The outcome was decided long before you flipped the coin...\n-# It's **Heads** though.",
+      "Coin flips are just chaos wrapped in the illusion of choice...\n-# It’s **Tails** though.",
+      "Nothing is ever truly random, it’s all just angles, force, and gravity...\n-# It’s **Heads** though.",
+    ]
+
+    const random = Math.floor(Math.random() * 250)
+
+    const response =
+      random < 10
+        ? rareResponses[Math.floor(Math.random() * rareResponses.length)]!
+        : commonResponses[Math.floor(Math.random() * commonResponses.length)]!
+
+    return void interaction.editReply(response)
   })
