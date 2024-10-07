2f6903f2871c3d1fe40b648a3c2d8172c99938ce
notcharliee <saunderscharlie@hotmail.com>
2024-10-07 10:47:27 +0100

    feat: update dog command to use `attachment://` images and add api attribution

--- apps/bot/src/app/commands/(fun)/dog.ts
+++ apps/bot/src/app/commands/(fun)/dog.ts
@@ -1,22 +1,45 @@
-import { EmbedBuilder } from "discord.js"
+import { AttachmentBuilder } from "discord.js"
 import { BotCommandBuilder } from "phasebot/builders"
 
-import { PhaseColour } from "~/lib/enums"
-import dogs from "./_data/dogs.json"
+import { BotErrorMessage } from "~/structures/BotError"
+import { CustomMessageBuilder } from "~/structures/CustomMessageBuilder"
+
+const API_URL = new URL("https://random.dog")
 
 export default new BotCommandBuilder()
   .setName("dog")
   .setDescription("Gives you a random picture of a dog.")
   .setExecute(async (interaction) => {
-    void interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("Random Dog")
-          .setColor(PhaseColour.Primary)
-          .setImage(
-            "https://random.dog/" +
-              dogs[Math.floor(Math.random() * dogs.length)],
-          ),
-      ],
-    })
+    await interaction.deferReply()
+
+    try {
+      API_URL.pathname = "/woof"
+
+      const imageURLResponse = await fetch(API_URL.toString())
+      const imageURL = await imageURLResponse.text()
+
+      API_URL.pathname = "/" + imageURL
+
+      const imageResponse = await fetch(API_URL.toString())
+      const imageArrayBuffer = await imageResponse.arrayBuffer()
+      const imageBuffer = Buffer.from(imageArrayBuffer)
+
+      const attachment = new AttachmentBuilder(imageBuffer)
+        .setName("random-dog.png")
+        .setDescription("Random dog image")
+
+      return void interaction.editReply(
+        new CustomMessageBuilder().setFiles(attachment).setEmbeds((embed) => {
+          return embed
+            .setTitle("Random Dog")
+            .setColor("Primary")
+            .setImage(attachment)
+            .setFooter({ text: `Made with ${API_URL.hostname} 🤍` })
+        }),
+      )
+    } catch {
+      return void interaction.editReply(
+        new BotErrorMessage("Failed to fetch a dog image."),
+      )
+    }
   })
