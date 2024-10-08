8925de7583ef556f9ffac0b2572e230065ec2487
notcharliee <saunderscharlie@hotmail.com>
2024-10-08 12:43:56 +0100

    refactor: update help command to use CustomMessageBuilder

--- apps/bot/src/app/commands/(bot)/bot/help.ts
+++ apps/bot/src/app/commands/(bot)/bot/help.ts
@@ -1,37 +1,38 @@
-import {
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  EmbedBuilder,
-} from "discord.js"
+import { ButtonStyle } from "discord.js"
 import { BotSubcommandBuilder } from "phasebot/builders"
 
 import { PhaseColour } from "~/lib/enums"
+
+import { CustomMessageBuilder } from "~/structures/CustomMessageBuilder"
 
 export default new BotSubcommandBuilder()
   .setName("help")
   .setDescription("Links to the bot's docs and support discord.")
   .setExecute(async (interaction) => {
-    void interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor(PhaseColour.Primary)
-          .setTitle("Help")
-          .setDescription(
-            "You can read our docs or ask the team directly using the buttons below!",
-          ),
-      ],
-      components: [
-        new ActionRowBuilder<ButtonBuilder>().setComponents(
-          new ButtonBuilder()
-            .setLabel("Documentation")
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://phasebot.xyz/docs"),
-          new ButtonBuilder()
-            .setLabel("Support Discord")
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://discord.gg/338tyqeg82"),
-        ),
-      ],
-    })
+    return void interaction.reply(
+      new CustomMessageBuilder()
+        .setEmbeds((embed) => {
+          return embed
+            .setColor(PhaseColour.Primary)
+            .setTitle("Help")
+            .setDescription(
+              "You can read our docs or ask the team directly using the buttons below!",
+            )
+        })
+        .setComponents((actionrow) => {
+          return actionrow
+            .addButton((button) => {
+              return button
+                .setLabel("Documentation")
+                .setURL("https://phasebot.xyz/docs")
+                .setStyle(ButtonStyle.Link)
+            })
+            .addButton((button) => {
+              return button
+                .setLabel("Support Discord")
+                .setURL("https://discord.gg/338tyqeg82")
+                .setStyle(ButtonStyle.Link)
+            })
+        }),
+    )
   })
