94a24f2648695aa06eb3e4ad0aface8723761edf
notcharliee <saunderscharlie@hotmail.com>
2024-10-08 13:01:15 +0100

    feat: add new ticket emojis and image assets for locked, unlocked, and deleted states

--- apps/bot/src/lib/emojis.ts
+++ apps/bot/src/lib/emojis.ts
@@ -6,22 +6,22 @@ export const ApplicationEmojis = {
   Connect4_Empty: formatEmoji("1287414239098835025"),
   Connect4_Player1: formatEmoji("1287414378412642304"),
   Connect4_Player2: formatEmoji("1287539813058084914"),
-  // Ticket_Locked: formatEmoji("1291406900977729598"),
-  // Ticket_Unlocked: formatEmoji("1291406915682832515"),
-  // Ticket_Delete: formatEmoji("1291416942485438555"),
+  Ticket_Locked: `<:lock_closed:1293180513816809496>`,
+  Ticket_Unlocked: `<:lock_open:1293180537317494815>`,
+  Ticket_Delete: `<:delete:1293180561598447656>`,
 }
 
 export const FallbackEmojis = {
   Connect4_Empty: "⬛",
   Connect4_Player1: "⚪",
   Connect4_Player2: "🟣",
+  Ticket_Locked: "🔒",
+  Ticket_Unlocked: "🔓",
+  Ticket_Delete: "🗑️",
 } satisfies Record<keyof typeof ApplicationEmojis, string>
 
 export const Emojis = {
   ZeroWidthJoiner: "‍",
   Giveaway_Reaction: "🎉",
-  Ticket_Locked: "🔒",
-  Ticket_Unlocked: "🔓",
-  Ticket_Delete: "🗑️",
   ...(env.NODE_ENV === "production" ? ApplicationEmojis : FallbackEmojis),
 }
