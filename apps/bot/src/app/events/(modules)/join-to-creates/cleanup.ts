623e751fd965520e4b89fe2533b3e6b0998e78c2
notcharliee <saunderscharlie@hotmail.com>
2024-10-09 15:09:29 +0100

    feat: add new jtc cleanup logic

---
+++ apps/bot/src/app/events/(modules)/join-to-creates/cleanup.ts
@@ -0,0 +1,37 @@
+import { ChannelType } from "discord.js"
+import { BotEventBuilder } from "phasebot/builders"
+
+import { ModuleId } from "@repo/utils/modules"
+
+import { db } from "~/lib/db"
+
+export default new BotEventBuilder()
+  .setName("channelDelete")
+  .setExecute(async (client, channel) => {
+    if (channel.isDMBased()) return
+
+    const guildDoc = client.store.guilds.get(channel.guildId)
+    const moduleConfig = guildDoc?.modules?.[ModuleId.JoinToCreates]
+
+    if (!moduleConfig?.enabled) return
+
+    const isCategory = channel.type === ChannelType.GuildCategory
+    const isVoice = channel.type === ChannelType.GuildVoice
+
+    if (
+      (!isCategory && !isVoice) ||
+      (isCategory && channel.id !== moduleConfig.category) ||
+      (isVoice && channel.parentId !== moduleConfig.category)
+    ) {
+      return
+    }
+
+    try {
+      await db.joinToCreates.deleteOne({
+        guild: channel.guildId,
+        channel: channel.id,
+      })
+    } catch {
+      // do nothing
+    }
+  })
