dcf9bafb6c9e16fd5562c3e1a001bd4ea1e5837c
notcharliee <saunderscharlie@hotmail.com>
2024-10-12 17:27:14 +0100

    refactor: remove `x-guild-id` header from middleware

--- apps/site/src/middleware.ts
+++ apps/site/src/middleware.ts
@@ -1,21 +1,7 @@
-import { NextResponse } from "next/server"
-
-import { auth } from "~/auth"
-
 import type { MiddlewareConfig } from "next/server"
 
 export const config: MiddlewareConfig = {
   matcher: ["/dashboard/:path*", "/auth/:path*"],
 }
 
-export default auth((req) => {
-  const pathname = req.nextUrl.pathname
-
-  const guildId = pathname.startsWith("/dashboard/guilds/")
-    ? pathname.split("/")[3]
-    : undefined
-
-  return NextResponse.next({
-    headers: guildId ? { "x-guild-id": guildId } : undefined,
-  })
-})
+export { auth as middleware } from "~/auth"
