bb9d04ff3a92d15dd78db0096c39b89f17d4be08
notcharliee <saunderscharlie@hotmail.com>
2024-10-12 17:24:20 +0100

    refactor: enhance LayoutProps type to support dynamic parameter definitions based on string variations

--- apps/site/src/types/props.ts
+++ apps/site/src/types/props.ts
@@ -1,3 +1,13 @@
-export type LayoutProps = Readonly<{
-  children: React.ReactNode
-}>
+export type LayoutProps<
+  T extends string | string[] | string[][] | undefined = undefined,
+> = Readonly<
+  {
+    children: React.ReactNode
+  } & (T extends string
+    ? { params: { [K in T]: string } }
+    : T extends string[]
+      ? { params: { [K in T[number]]: string } }
+      : T extends string[][]
+        ? { params: { [K in T[number][number]]: string } }
+        : never)
+>
