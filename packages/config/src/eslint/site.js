ac072b738db37d674ead74220bf96a3c059ac1db
notcharliee <saunderscharlie@hotmail.com>
2024-10-13 21:19:17 +0100

    chore: simplify ESLint config by importing base and consolidating rules for better maintainability

--- packages/config/src/eslint/site.js
+++ packages/config/src/eslint/site.js
@@ -1,57 +1,33 @@
-// @ts-nocheck
-
-import eslint from "@eslint/js"
+// @ts-expect-error no types
 import nextPlugin from "@next/eslint-plugin-next"
+// @ts-expect-error no types
 import reactPlugin from "eslint-plugin-react"
+// @ts-expect-error no types
 import hooksPlugin from "eslint-plugin-react-hooks"
 import globals from "globals"
 import tseslint from "typescript-eslint"
 
-export default tseslint.config(
-  eslint.configs.recommended,
-  ...tseslint.configs.recommendedTypeChecked,
-  ...tseslint.configs.stylisticTypeChecked,
-  {
-    languageOptions: {
-      parser: tseslint.parser,
-      parserOptions: {
-        project: true,
-      },
-      globals: {
-        ...globals.node,
-        ...globals.browser,
-      },
-    },
-    plugins: {
-      react: reactPlugin,
-      "react-hooks": hooksPlugin,
-      "@next/next": nextPlugin,
-    },
-    rules: {
-      ...hooksPlugin.configs.recommended.rules,
-      ...nextPlugin.configs.recommended.rules,
-      ...nextPlugin.configs["core-web-vitals"].rules,
-      ...reactPlugin.configs["jsx-runtime"].rules,
-      "@next/next/no-img-element": "off",
-      "@next/next/no-duplicate-head": "off",
-      "@typescript-eslint/array-type": "off",
-      "@typescript-eslint/no-empty-interface": "off",
-      "@typescript-eslint/no-empty-object-type": "off",
-      "@typescript-eslint/consistent-type-definitions": "off",
-      "@typescript-eslint/require-await": "off",
-      "@typescript-eslint/consistent-type-imports": [
-        "warn",
-        { prefer: "type-imports", fixStyle: "separate-type-imports" },
-      ],
-      "@typescript-eslint/no-unused-vars": [
-        "warn",
-        { argsIgnorePattern: "^_" },
-      ],
-      "@typescript-eslint/no-misused-promises": [
-        "error",
-        { checksVoidReturn: { attributes: false } },
-      ],
-      "react/no-unescaped-entities": "off",
+import base from "./base.js"
+
+export default tseslint.config(...base, {
+  languageOptions: {
+    globals: {
+      ...globals.node,
+      ...globals.browser,
     },
   },
-)
+  plugins: {
+    react: reactPlugin,
+    "react-hooks": hooksPlugin,
+    "@next/next": nextPlugin,
+  },
+  rules: {
+    ...hooksPlugin.configs.recommended.rules,
+    ...nextPlugin.configs.recommended.rules,
+    ...nextPlugin.configs["core-web-vitals"].rules,
+    ...reactPlugin.configs["jsx-runtime"].rules,
+    "@next/next/no-img-element": "off",
+    "@next/next/no-duplicate-head": "off",
+    "react/no-unescaped-entities": "off",
+  },
+})
