a803709fe8c787f0d5524d51a8fd6bc918bd9e48
notcharliee <saunderscharlie@hotmail.com>
2024-10-23 16:00:11 +0100

    feat: add cron job for periodic invite store checks

---
+++ apps/bot/src/app/crons/(modules)/invite-logs.ts
@@ -0,0 +1,40 @@
+import { BotCronBuilder } from "phasebot/builders"
+
+import { ModuleId } from "@repo/utils/modules"
+
+import {
+  hasRequiredGuildPermissions,
+  mapInvite,
+} from "~/app/events/(modules)/invite-logs/_utils"
+
+export default new BotCronBuilder()
+  .setPattern("* * * * *")
+  .setExecute(async (client) => {
+    for (const guild of client.guilds.cache.values()) {
+      const hasInvitesInStore = client.stores.invites.has(guild.id)
+
+      if (!hasRequiredGuildPermissions(guild)) {
+        if (hasInvitesInStore) client.stores.invites.delete(guild.id)
+        continue
+      }
+
+      const guildDoc = client.stores.guilds.get(guild.id)
+
+      if (!guildDoc) {
+        if (hasInvitesInStore) client.stores.invites.delete(guild.id)
+        continue
+      }
+
+      const moduleConfig = guildDoc.modules?.[ModuleId.AuditLogs]
+
+      if (!moduleConfig?.enabled || !moduleConfig.channels.invites) {
+        if (hasInvitesInStore) client.stores.invites.delete(guild.id)
+        continue
+      }
+
+      if (hasInvitesInStore) continue
+
+      const invites = await guild.invites.fetch()
+      client.stores.invites.set(guild.id, invites.mapValues(mapInvite))
+    }
+  })
