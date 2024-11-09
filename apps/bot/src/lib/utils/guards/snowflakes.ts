1736a51701d7605b7b37dccca0ab3c6f526282d9
notcharliee <saunderscharlie@hotmail.com>
2024-11-09 10:21:18 +0000

    fix: update isSnowflake check to handle unknown types

--- apps/bot/src/lib/utils/guards/snowflakes.ts
+++ apps/bot/src/lib/utils/guards/snowflakes.ts
@@ -1,7 +1,10 @@
 import { SnowflakeUtil } from "discord.js" // from @sapphire/snowflake
 
-export function isSnowflake(id: string) {
+import type { Snowflake } from "discord.js"
+
+export function isSnowflake(id: unknown): id is Snowflake {
   try {
+    if (typeof id !== "string" || typeof id !== "bigint") return false
     return SnowflakeUtil.deconstruct(id).timestamp > SnowflakeUtil.epoch
   } catch {
     return false
