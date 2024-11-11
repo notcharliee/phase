1913ea498df8c84d1396b89be64498695ee9e863
notcharliee <saunderscharlie@hotmail.com>
2024-11-11 16:24:15 +0000

    fix: edit no-unused-vars rule to include destructured and vars ignore patterns

--- packages/config/src/eslint/base.js
+++ packages/config/src/eslint/base.js
@@ -28,7 +28,11 @@ export default tseslint.config(
       ],
       "@typescript-eslint/no-unused-vars": [
         "warn",
-        { argsIgnorePattern: "^_" },
+        {
+          argsIgnorePattern: "^_",
+          destructuredArrayIgnorePattern: "^_",
+          varsIgnorePattern: "^_",
+        },
       ],
       "@typescript-eslint/no-misused-promises": [
         "error",
