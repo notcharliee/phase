4b310b7ec0803af5054a2b79046c1dcb4eaff952
notcharliee <saunderscharlie@hotmail.com>
2024-11-13 13:51:49 +0000

    feat: add FormFieldEmojiPicker component

---
+++ apps/site/src/components/ui/form/field/emoji-picker.tsx
@@ -0,0 +1,46 @@
+import { EmojiPicker } from "~/components/emoji-picker"
+import {
+  FormControl,
+  FormDescription,
+  FormField,
+  FormItem,
+  FormLabel,
+  FormMessage,
+} from "~/components/ui/form"
+
+import type { Control, FieldPath, FieldValues } from "react-hook-form"
+
+export interface FormFieldEmojiPickerProps<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+> {
+  control: Control<TFieldValues>
+  name: TName
+  label: string
+  description: string
+  disabled?: boolean
+  size?: "default" | "fill"
+}
+
+export function FormFieldEmojiPicker<
+  TFieldValues extends FieldValues = FieldValues,
+  TName extends FieldPath<TFieldValues> = FieldPath<TFieldValues>,
+>(props: FormFieldEmojiPickerProps<TFieldValues, TName>) {
+  return (
+    <FormField
+      control={props.control}
+      name={props.name}
+      disabled={props.disabled}
+      render={({ field }) => (
+        <FormItem>
+          <FormLabel>{props.label}</FormLabel>
+          <FormControl>
+            <EmojiPicker size={props.size} {...field} />
+          </FormControl>
+          <FormDescription>{props.description}</FormDescription>
+          <FormMessage />
+        </FormItem>
+      )}
+    />
+  )
+}
