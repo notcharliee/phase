55fff4b183e7af1741a307a95c56db6f9f87e260
notcharliee <saunderscharlie@hotmail.com>
2024-11-13 13:52:52 +0000

    refactor: update reaction roles form to use new form field components

--- apps/site/src/app/dashboard/guilds/[id]/modules/_forms/reaction-roles.tsx
+++ apps/site/src/app/dashboard/guilds/[id]/modules/_forms/reaction-roles.tsx
@@ -1,147 +1,72 @@
 "use client"
 
-import { TrashIcon } from "@radix-ui/react-icons"
 import { ModuleId } from "@repo/utils/modules"
-import { useFieldArray, useFormContext } from "react-hook-form"
+import { useFormContext } from "react-hook-form"
 
-import { SelectRole } from "~/components/dashboard/select/role"
-import { EmojiPicker } from "~/components/emoji-picker"
-import { Button } from "~/components/ui/button"
-import {
-  FormControl,
-  FormDescription,
-  FormField,
-  FormHeader,
-  FormItem,
-  FormLabel,
-  FormMessage,
-} from "~/components/ui/form"
-import { Input } from "~/components/ui/input"
-import { Label } from "~/components/ui/label"
+import { FormFieldArray } from "~/components/ui/form/field/array"
+import { FormFieldArrayButton } from "~/components/ui/form/field/array-button"
+import { FormFieldArrayCard } from "~/components/ui/form/field/array-card"
+import { FormFieldEmojiPicker } from "~/components/ui/form/field/emoji-picker"
+import { FormFieldInput } from "~/components/ui/form/field/input"
+import { FormFieldSelectRole } from "~/components/ui/form/field/select-role"
+import { FormFieldWrapper } from "~/components/ui/form/field/wrapper"
 
-import type { modulesSchema } from "~/validators/modules"
-import type { z } from "zod"
+import { getOrdinal } from "~/lib/utils"
+
+import type { ModulesFormValuesInput } from "~/types/dashboard"
 
 export const ReactionRoles = () => {
-  const form = useFormContext<z.infer<typeof modulesSchema>>()
-  const formFieldArray = useFieldArray({
-    control: form.control,
-    name: `${ModuleId.ReactionRoles}.reactions`,
-  })
+  const form = useFormContext<ModulesFormValuesInput>()
 
   return (
-    <FormItem className="space-y-8">
-      <FormField
+    <FormFieldWrapper>
+      <FormFieldInput
+        label="Message URL"
+        description="The URL of your reaction role message"
+        placeholder="https://discord.com/channels/{guild}/{channel}/{message}"
         control={form.control}
         name={`${ModuleId.ReactionRoles}.messageUrl`}
-        render={({ field }) => (
-          <FormItem>
-            <FormLabel>Message URL</FormLabel>
-            <FormControl>
-              <Input
-                {...field}
-                placeholder="https://discord.com/channels/{guild}/{channel}/{message}"
-              />
-            </FormControl>
-            <FormDescription>
-              The URL of your reaction role message
-            </FormDescription>
-            <FormMessage />
-          </FormItem>
-        )}
       />
-      <FormField
+      <FormFieldArray
+        label="Reactions"
+        description="The reactions you want members to use"
+        maxLength={20}
         control={form.control}
         name={`${ModuleId.ReactionRoles}.reactions`}
-        render={() => (
-          <FormItem className="space-y-4">
-            <FormHeader>
-              <FormLabel>Reactions</FormLabel>
-              <FormDescription>
-                The reactions you want members to be able to react with (max 20)
-              </FormDescription>
-            </FormHeader>
-            <FormControl>
-              <div className="space-y-4">
-                {formFieldArray.fields.map(({ id }, index) => {
-                  const baseName =
-                    `${ModuleId.ReactionRoles}.reactions.${index}` as const
-
-                  return (
-                    <FormField
-                      key={id}
-                      control={form.control}
-                      name={baseName}
-                      render={() => (
-                        <FormItem className="space-y-0">
-                          <FormLabel className="sr-only">
-                            Reaction {index + 1}
-                          </FormLabel>
-                          <FormControl>
-                            <div className="flex gap-2">
-                              <FormField
-                                control={form.control}
-                                name={`${baseName}.emoji`}
-                                render={({ field }) => (
-                                  <FormItem className="space-y-0">
-                                    <FormLabel className="sr-only">
-                                      Reaction Emoji
-                                    </FormLabel>
-                                    <FormControl>
-                                      <EmojiPicker {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                  </FormItem>
-                                )}
-                              />
-                              <FormField
-                                control={form.control}
-                                name={`${baseName}.role`}
-                                render={({ field }) => (
-                                  <FormItem className="w-full space-y-0">
-                                    <FormLabel className="sr-only">
-                                      Reaction Role
-                                    </FormLabel>
-                                    <FormControl>
-                                      <SelectRole {...field} />
-                                    </FormControl>
-                                    <FormMessage />
-                                  </FormItem>
-                                )}
-                              />
-                              <Button
-                                variant="outline"
-                                size="icon"
-                                onClick={() => formFieldArray.remove(index)}
-                              >
-                                <Label className="sr-only">
-                                  Delete Reaction
-                                </Label>
-                                <TrashIcon className="h-4 w-4" />
-                              </Button>
-                            </div>
-                          </FormControl>
-                          <FormMessage />
-                        </FormItem>
-                      )}
-                    />
-                  )
-                })}
-                <Button
-                  type="button"
-                  variant="outline"
-                  disabled={formFieldArray.fields.length >= 20}
-                  onClick={() =>
-                    formFieldArray.append({ emoji: "🌒", role: "" })
-                  }
-                >
-                  Add Reaction
-                </Button>
-              </div>
-            </FormControl>
-          </FormItem>
+        render={({ fields }) => (
+          <FormFieldWrapper type={"array"}>
+            {fields.map((field, index) => (
+              <FormFieldArrayCard
+                key={field.id}
+                index={index}
+                label={`Reaction ${index + 1}`}
+                description={`The ${getOrdinal(index + 1)} reaction in the message`}
+                control={form.control}
+                name={`${ModuleId.ReactionRoles}.reactions.${index}`}
+              >
+                <FormFieldEmojiPicker
+                  label="Emoji"
+                  description="The emoji to react with"
+                  size="fill"
+                  control={form.control}
+                  name={`${ModuleId.ReactionRoles}.reactions.${index}.emoji`}
+                />
+                <FormFieldSelectRole
+                  label="Role"
+                  description="The role to assign when the emoji is used"
+                  control={form.control}
+                  name={`${ModuleId.ReactionRoles}.reactions.${index}.role`}
+                />
+              </FormFieldArrayCard>
+            ))}
+            <FormFieldArrayButton
+              label="Add Reaction"
+              description="Adds a reaction to the message"
+              appendValue={{ emoji: "🌒", role: "" }}
+            />
+          </FormFieldWrapper>
         )}
       />
-    </FormItem>
+    </FormFieldWrapper>
   )
 }
