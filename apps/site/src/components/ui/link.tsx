d05189e71b6d23550e0166c6287dd85f7085137a
notcharliee <saunderscharlie@hotmail.com>
2024-11-19 01:21:08 +0000

    feat: update Link component with title, aria-label, and external link handling

--- apps/site/src/components/ui/link.tsx
+++ apps/site/src/components/ui/link.tsx
@@ -7,37 +7,43 @@ import { cn } from "~/lib/utils"
 import type { VariantProps } from "class-variance-authority"
 import type { LinkProps as NextLinkProps } from "next/link"
 
-export const linkVariants = cva("font-medium underline-offset-2 transition-colors", {
-  variants: {
-    variant: {
-      default: "text-foreground underline",
-      hover:
-        "text-muted-foreground hover:text-primary aria-selected:text-primary hover:underline",
-      "no-underline": "no-underline",
+export const linkVariants = cva(
+  "font-medium underline-offset-2 transition-colors",
+  {
+    variants: {
+      variant: {
+        default: "text-foreground underline",
+        hover:
+          "text-muted-foreground hover:text-primary aria-selected:text-primary hover:underline",
+        "no-underline": "no-underline",
+      },
+      size: {
+        auto: "",
+        base: "text-base",
+        sm: "text-sm",
+      },
     },
-    size: {
-      base: "text-base",
-      sm: "text-sm",
+    defaultVariants: {
+      variant: "default",
+      size: "sm",
     },
   },
-  defaultVariants: {
-    variant: "default",
-    size: "sm",
-  },
-})
+)
 
 export interface LinkProps
   extends NextLinkProps,
     React.AnchorHTMLAttributes<HTMLAnchorElement>,
     VariantProps<typeof linkVariants> {
   href: string
+  label?: string
   external?: boolean
 }
 
 export function Link({
+  className,
   variant,
   size,
-  className,
+  label,
   external,
   ...props
 }: LinkProps) {
@@ -45,6 +51,10 @@ export function Link({
 
   return (
     <Comp
+      title={label}
+      aria-label={label}
+      target={external ? "_blank" : undefined}
+      rel={external ? "noreferrer" : undefined}
       className={cn(linkVariants({ variant, size, className }))}
       {...props}
     />
