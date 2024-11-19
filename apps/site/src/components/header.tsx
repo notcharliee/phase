b5785ea6e61a349646656e2f78067503ba301c5f
notcharliee <saunderscharlie@hotmail.com>
2024-11-19 01:23:35 +0000

    refactor: rewrite Header component

--- apps/site/src/components/header.tsx
+++ apps/site/src/components/header.tsx
@@ -1,12 +1,13 @@
 "use client"
 
-import { usePathname, useRouter } from "next/navigation"
-import { useCallback, useEffect, useState } from "react"
+import { useRouter } from "next/navigation"
+import * as React from "react"
 
 import { FileIcon, GlobeIcon, RocketIcon } from "@radix-ui/react-icons"
+import { useIntersectionObserver } from "@uidotdev/usehooks"
 
 import { Moon } from "~/components/moon"
-import { Button, buttonVariants } from "~/components/ui/button"
+import { Button } from "~/components/ui/button"
 import {
   CommandDialog,
   CommandEmpty,
@@ -20,43 +21,22 @@ import { Link } from "~/components/ui/link"
 
 import { docsPages, mainPages, splitPagesByCategory } from "~/config/nav"
 
+import { cn } from "~/lib/utils"
+
 import type { NavItem } from "~/config/nav"
 import type { WithRequired } from "~/types/utils"
 
-export const Header = () => {
-  const pathname = usePathname()
-  const router = useRouter()
+export interface HeaderProps extends React.ComponentPropsWithRef<"header"> {}
 
-  const [open, setOpen] = useState(false)
-
-  useEffect(() => {
-    const down = (e: KeyboardEvent) => {
-      if ((e.key === "k" && (e.metaKey || e.ctrlKey)) || e.key === "/") {
-        if (
-          (e.target instanceof HTMLElement && e.target.isContentEditable) ||
-          e.target instanceof HTMLInputElement ||
-          e.target instanceof HTMLTextAreaElement ||
-          e.target instanceof HTMLSelectElement
-        ) {
-          return
-        }
-
-        e.preventDefault()
-        setOpen((open) => !open)
-      }
-    }
-
-    document.addEventListener("keydown", down)
-    return () => document.removeEventListener("keydown", down)
-  }, [])
-
-  const runCommand = useCallback((command: () => unknown) => {
-    setOpen(false)
-    command()
-  }, [])
-
+export function Header({ className, ...props }: HeaderProps) {
   return (
-    <header className="sticky top-0 z-50 h-16 w-full border-b backdrop-blur-sm">
+    <header
+      className={cn(
+        "sticky top-0 z-50 h-16 w-full border-b backdrop-blur-sm",
+        className,
+      )}
+      {...props}
+    >
       <div className="container flex h-full items-center">
         <nav className="mr-8 hidden items-center space-x-4 md:flex lg:space-x-6">
           <Link
@@ -76,7 +56,6 @@ export const Header = () => {
                 href={item.href}
                 title={item.label}
                 aria-label={item.label}
-                aria-selected={item.href === pathname}
                 external={item.external}
                 variant={"hover"}
                 size={"sm"}
@@ -86,91 +65,132 @@ export const Header = () => {
             ))}
         </nav>
         <div className="flex flex-1 items-center justify-between space-x-3 md:justify-end">
-          <div className="w-full flex-1 md:w-auto md:flex-none">
-            <Button
-              variant="outline"
-              className="bg-background text-muted-foreground relative h-9 w-full justify-start text-sm sm:pr-[52px] md:w-64"
-              onClick={() => setOpen(true)}
-            >
-              <span className="flex items-center gap-2">
-                <RocketIcon className="h-3.5 w-3.5" />
-                Wanna explore?
-              </span>
-              <kbd className="bg-muted pointer-events-none absolute right-[4.333px] top-[4.333] hidden h-[26px] select-none items-center gap-1 rounded-sm px-2.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
-                <span className="text-xs">⌘</span>K
-              </kbd>
-            </Button>
-            <CommandDialog open={open} onOpenChange={setOpen}>
-              <CommandInput placeholder="Where do you want to go?" />
-              <CommandList>
-                <CommandEmpty>No results found.</CommandEmpty>
-                <CommandGroup heading="Popular Links">
-                  {mainPages.map((page) => (
-                    <CommandItem
-                      key={page.href}
-                      value={page.label}
-                      onSelect={() => {
-                        runCommand(() => router.push(page.href))
-                      }}
-                    >
-                      {page.external ? (
-                        <GlobeIcon className="mr-2 h-4 w-4" />
-                      ) : (
-                        <FileIcon className="mr-2 h-4 w-4" />
-                      )}
-                      {page.label}
-                    </CommandItem>
-                  ))}
-                </CommandGroup>
-                <CommandSeparator className="mb-2 mt-1" />
-                {Object.entries(splitPagesByCategory(docsPages)).map(
-                  ([category, pages]) => (
-                    <CommandGroup key={category} heading={category}>
-                      {pages.map((page) => (
-                        <CommandItem
-                          key={page.href}
-                          value={page.label}
-                          onSelect={() => {
-                            runCommand(() => router.push(page.href))
-                          }}
-                        >
-                          {page.external ? (
-                            <GlobeIcon className="mr-2 h-4 w-4" />
-                          ) : (
-                            <FileIcon className="mr-2 h-4 w-4" />
-                          )}
-                          {page.label}
-                        </CommandItem>
-                      ))}
-                    </CommandGroup>
-                  ),
-                )}
-              </CommandList>
-            </CommandDialog>
-          </div>
+          <NavigationCombobox />
           <nav className="flex items-center gap-1.5">
             {mainPages
               .filter((i): i is WithRequired<NavItem, "icon"> => !!i.icon)
               .map((item) => (
-                <Link
+                <Button
                   key={item.href}
-                  href={item.href}
                   title={item.label}
                   aria-label={item.label}
-                  external={item.external}
-                  target="_blank"
-                  rel="noreferrer"
-                  className={buttonVariants({
-                    variant: "outline",
-                    size: "icon",
-                  })}
+                  variant={"outline"}
+                  size={"icon"}
+                  asChild
                 >
-                  <item.icon className="h-5 w-5" />
-                </Link>
+                  <Link href={item.href} external={item.external}>
+                    <item.icon />
+                  </Link>
+                </Button>
               ))}
           </nav>
         </div>
       </div>
     </header>
+  )
+}
+
+function NavigationCombobox() {
+  const [open, setOpen] = React.useState(false)
+
+  const router = useRouter()
+
+  React.useEffect(() => {
+    const onKeyDown = (e: KeyboardEvent) => {
+      if (
+        !((e.key === "k" && (e.metaKey || e.ctrlKey)) || e.key === "/") ||
+        (e.target instanceof HTMLElement && e.target.isContentEditable) ||
+        e.target instanceof HTMLInputElement ||
+        e.target instanceof HTMLTextAreaElement ||
+        e.target instanceof HTMLSelectElement
+      ) {
+        return
+      }
+
+      e.preventDefault()
+      setOpen((open) => !open)
+    }
+
+    document.addEventListener("keydown", onKeyDown)
+    return () => document.removeEventListener("keydown", onKeyDown)
+  }, [])
+
+  const navigateTo = React.useCallback(
+    (href: string) => {
+      setOpen(false)
+      router.push(href)
+    },
+    [router],
+  )
+
+  const Item = ({ item }: { item: NavItem }) => {
+    const [ref, entry] = useIntersectionObserver()
+
+    React.useEffect(() => {
+      if (item.external || !entry?.isIntersecting) return
+      router.prefetch(item.href)
+    }, [entry, item.external, item.href])
+
+    return (
+      <CommandItem
+        ref={ref}
+        key={item.href}
+        value={item.label}
+        onSelect={() => navigateTo(item.href)}
+      >
+        {item.external ? (
+          <GlobeIcon className="mr-2 size-4 shrink-0" />
+        ) : (
+          <FileIcon className="mr-2 size-4 shrink-0" />
+        )}
+        {item.label}
+      </CommandItem>
+    )
+  }
+
+  return (
+    <div className="w-full flex-1 md:w-auto md:flex-none">
+      <Button
+        aria-label="Navigation"
+        aria-description="Navigate to a page"
+        variant="outline"
+        className="bg-background text-muted-foreground relative h-9 w-full justify-start text-sm sm:pr-[52px] md:w-64"
+        onClick={() => setOpen(!open)}
+      >
+        <span className="flex items-center gap-2">
+          <RocketIcon className="size-3.5 shrink-0" />
+          Wanna explore?
+        </span>
+        <kbd className="bg-muted pointer-events-none absolute right-[4.333px] top-[4.333] hidden h-[26px] select-none items-center gap-1 rounded-sm px-2.5 font-mono text-[10px] font-medium opacity-100 sm:flex">
+          <span className="text-xs">⌘</span>K
+        </kbd>
+      </Button>
+      <CommandDialog
+        open={open}
+        onOpenChange={setOpen}
+        dialogLabel="Navigation"
+        dialogDescription="Navigate to a page"
+      >
+        <CommandInput placeholder="Where do you want to go?" />
+        <CommandList>
+          <CommandEmpty>{"No results found :("}</CommandEmpty>
+          <CommandGroup heading="Main Links">
+            {mainPages.map((item) => (
+              <Item key={item.href} item={item} />
+            ))}
+          </CommandGroup>
+          <CommandSeparator className="mb-2 mt-1" />
+          {Object.entries(splitPagesByCategory(docsPages)).map(
+            ([category, pages]) => (
+              <CommandGroup key={category} heading={category}>
+                {pages.map((item) => (
+                  <Item key={item.href} item={item} />
+                ))}
+              </CommandGroup>
+            ),
+          )}
+        </CommandList>
+      </CommandDialog>
+    </div>
   )
 }
