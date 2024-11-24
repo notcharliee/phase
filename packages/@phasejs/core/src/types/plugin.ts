e43c61d639096dfb6e55b9ef23f4998a68d48764
notcharliee <saunderscharlie@hotmail.com>
2024-11-24 05:39:02 +0000

    feat: add @phasejs/plugin package

--- packages/@phasejs/core/src/types/plugin.ts
+++ packages/@phasejs/core/src/types/plugin.ts
@@ -1,8 +1,1 @@
-import type { DjsClient } from "~/types/client"
-import type { Awaitable } from "~/types/utils"
-
-export interface BotPlugin {
-  name: string
-  version: `${number}.${number}.${number}`
-  onLoad: (client: DjsClient<false>) => Awaitable<void>
-}
+export type { BotPlugin } from "@phasejs/plugin"
---
+++ packages/@phasejs/plugin/src/index.ts
@@ -0,0 +1,42 @@
+import type { Client } from "discord.js"
+
+export type Awaitable<T> = T | Promise<T>
+
+export interface BotPlugin {
+  name: string
+  version: `${number}.${number}.${number}`
+  onLoad: (client: Client<false>) => Awaitable<void>
+}
+
+export class BotPluginBuilder {
+  public readonly name!: BotPlugin["name"]
+  public readonly version!: BotPlugin["version"]
+  public readonly onLoad!: BotPlugin["onLoad"]
+
+  setName(name: BotPlugin["name"]) {
+    Reflect.set(this, "name", name)
+    return this
+  }
+
+  setVersion(version: BotPlugin["version"]) {
+    Reflect.set(this, "version", version)
+    return this
+  }
+
+  setOnLoad(onLoad: BotPlugin["onLoad"]) {
+    Reflect.set(this, "onLoad", onLoad)
+    return this
+  }
+
+  toJSON(): BotPlugin {
+    if (!this.name || !this.version || !this.onLoad) {
+      throw new Error("Plugin is missing required properties")
+    }
+
+    return {
+      name: this.name,
+      version: this.version,
+      onLoad: this.onLoad,
+    }
+  }
+}
