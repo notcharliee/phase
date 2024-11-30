1d1005047a5a0c0490802230e5baa210e240e817
notcharliee <saunderscharlie@hotmail.com>
2024-11-30 22:47:52 +0000

    fix: update channel slate node icon size

--- apps/site/src/components/ui/richtext/shared/nodes/elements/channel.tsx
+++ apps/site/src/components/ui/richtext/shared/nodes/elements/channel.tsx
@@ -17,7 +17,10 @@ export function ChannelElementNode({
     <span {...attributes} className="h-4 cursor-default">
       <span className="text-foreground bg-foreground/25 whitespace-nowrap rounded-[4px] px-1">
         <span className="mr-0.5 inline-block h-4 align-middle">
-          <ChannelIcon channelType={element.data.type} />
+          <ChannelIcon
+            channelType={element.data.type}
+            className="my-auto size-3.5"
+          />
         </span>
         <span className="leading-none">{element.data.name}</span>
       </span>
