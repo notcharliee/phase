c1aef289756df88001eca0b8f7a664ebed00ab13
notcharliee <saunderscharlie@hotmail.com>
2024-12-10 09:32:40 +0000

    refactor: update font imports and add type declarations for .otf files

--- apps/bot/src/assets/fonts/index.ts
+++ apps/bot/src/assets/fonts/index.ts
@@ -1,8 +1,9 @@
-import { join } from "node:path"
+import geistBold from "./geist-bold.otf" with { type: "file" }
+import geistSemibold from "./geist-semibold.otf" with { type: "file" }
+import geistMedium from "./geist-medium.otf" with { type: "file" }
+import geistRegular from "./geist-regular.otf" with { type: "file" }
 
-export const geistBoldFile = Bun.file(join(__dirname, "./geist-bold.otf"))
-export const geistSemiboldFile = Bun.file(
-  join(__dirname, "./geist-semibold.otf"),
-)
-export const geistMediumFile = Bun.file(join(__dirname, "./geist-medium.otf"))
-export const geistRegularFile = Bun.file(join(__dirname, "./geist-regular.otf"))
+export const geistBoldFile = Bun.file(geistBold)
+export const geistSemiboldFile = Bun.file(geistSemibold)
+export const geistMediumFile = Bun.file(geistMedium)
+export const geistRegularFile = Bun.file(geistRegular)
---
+++ apps/bot/src/types/declarations/fonts.d.ts
@@ -0,0 +1,4 @@
+declare module "*.otf" {
+  const content: string
+  export default content
+}
