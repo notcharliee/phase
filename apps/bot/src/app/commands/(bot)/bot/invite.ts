f577ca4d1927f8e15ac93ba3690b4b9c8f996871
notcharliee <saunderscharlie@hotmail.com>
2024-12-13 14:58:41 +0000

    refactor: invite command

--- apps/bot/src/app/commands/(bot)/bot/invite.ts
+++ apps/bot/src/app/commands/(bot)/bot/invite.ts
@@ -1,27 +1,35 @@
-import { EmbedBuilder } from "discord.js"
 import { BotSubcommandBuilder } from "@phasejs/core/builders"
+import { hyperlink } from "discord.js"
 
-import { PhaseColour } from "~/lib/enums"
+import { MessageBuilder } from "~/structures/builders"
 
 export default new BotSubcommandBuilder()
   .setName("invite")
   .setDescription("Generates an invite link for the bot.")
   .setExecute(async (interaction) => {
-    const inviteUrl = (
-      await fetch("https://phasebot.xyz/redirect/invite", {
-        redirect: "manual",
-      })
-    ).headers.get("Location")!
+    const inviteUrl = await fetchBotInviteLink()
+    const inviteLink = hyperlink("Click here to invite the bot!", inviteUrl)
 
-    void interaction.reply({
-      embeds: [
-        new EmbedBuilder()
-          .setColor(PhaseColour.Primary)
+    return await interaction.reply(
+      new MessageBuilder().setEmbeds((embed) => {
+        return embed
+          .setColor("Primary")
           .setTitle("Invite Link")
-          .setDescription(
-            `[Click here](${inviteUrl}) to invite the bot to your server.`,
-          )
-          .setFooter({ text: "Thanks for using Phase! 🤍" }),
-      ],
-    })
+          .setDescription(inviteLink)
+          .setFooter({ text: "Thanks for using Phase! 🤍" })
+      }),
+    )
   })
+
+/**
+ * Fetches the latest invite link from the bot's redirect page.
+ *
+ * @remarks By sending the actual invite link, discord will open an in-app modal
+ * instead of opening a new browser window.
+ */
+async function fetchBotInviteLink() {
+  const url = "https://phasebot.xyz/redirect/invite"
+  const response = await fetch(url, { redirect: "manual" })
+  const location = response.headers.get("Location")
+  return location!
+}
