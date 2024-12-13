4b9b8f739b9e3a54a5f96295d504e84a2509cedd
notcharliee <saunderscharlie@hotmail.com>
2024-12-13 14:58:30 +0000

    refactor: login command

--- apps/bot/src/app/commands/(bot)/bot/login.ts
+++ apps/bot/src/app/commands/(bot)/bot/login.ts
@@ -1,67 +1,60 @@
-import crypto from "node:crypto"
+import { createHmac } from "node:crypto"
 
-import {
-  ActionRowBuilder,
-  ButtonBuilder,
-  ButtonStyle,
-  EmbedBuilder,
-} from "discord.js"
 import { BotSubcommandBuilder } from "@phasejs/core/builders"
+import { bold, ButtonStyle } from "discord.js"
 
 import { db } from "~/lib/db"
-import { PhaseColour } from "~/lib/enums"
 import { env } from "~/lib/env"
+import { dateToTimestamp } from "~/lib/utils/formatting"
 
 import { BotErrorMessage } from "~/structures/BotError"
+import { MessageBuilder } from "~/structures/builders"
 
 export default new BotSubcommandBuilder()
   .setName("login")
   .setDescription("Generates a dashboard login code.")
+  .setMetadata({ dmPermission: false })
   .setExecute(async (interaction) => {
-    await interaction.deferReply({
-      ephemeral: true,
-    })
-
-    if (!interaction.guild) {
-      void interaction.editReply(BotErrorMessage.serverOnlyCommand().toJSON())
-      return
-    }
+    await interaction.deferReply({ ephemeral: true })
 
     const guildDoc = interaction.client.stores.guilds.get(interaction.guildId!)
 
     if (!guildDoc?.admins.includes(interaction.user.id)) {
-      return void interaction.editReply(BotErrorMessage.userNotAdmin().toJSON())
+      const errorMessage = BotErrorMessage.userNotAdmin()
+      return await interaction.editReply(errorMessage)
     }
 
-    const { value, signature } = await generateOTP()
+    const { value, signature, expiresAt } = await generateLoginCode()
 
-    void interaction.editReply({
-      embeds: [
-        new EmbedBuilder()
-          .setTitle("Dashboard Login Code")
-          .setDescription(
-            `Your login code is **${value}**\nThis will expire in <t:${Math.floor(Date.now() / 1000) + 60}:R>`,
-          )
-          .setColor(PhaseColour.Primary),
-      ],
-      components: [
-        new ActionRowBuilder<ButtonBuilder>().setComponents(
-          new ButtonBuilder()
-            .setLabel("Login page")
-            .setStyle(ButtonStyle.Link)
-            .setURL("https://phasebot.xyz/auth/signin"),
-        ),
-      ],
-    })
-
-    void db.otps.create({
+    await db.otps.create({
       userId: interaction.user.id,
       guildId: interaction.guildId,
       otp: signature,
     })
+
+    return await interaction.editReply(
+      new MessageBuilder()
+        .setEmbeds((embed) => {
+          embed.setTitle("Dashboard Login Code")
+          embed.setColor("Primary")
+          embed.setDescription(`
+            Your login code is ${bold(value)}
+            This will expire in ${dateToTimestamp(expiresAt)}
+          `)
+          return embed
+        })
+        .setComponents((actionrow) => {
+          return actionrow.addButton((button) => {
+            return button
+              .setURL("https://phasebot.xyz/auth/signin")
+              .setLabel("Login page")
+              .setStyle(ButtonStyle.Link)
+          })
+        }),
+    )
   })
 
-async function generateOTP() {
+async function generateLoginCode() {
   const chars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789"
 
   let value = ""
@@ -69,17 +62,19 @@ async function generateOTP() {
     value += chars.charAt(Math.floor(Math.random() * chars.length))
   }
 
-  const signature = crypto
-    .createHmac("sha256", env.AUTH_OTP_SECRET)
+  const signature = createHmac("sha256", env.AUTH_OTP_SECRET)
     .update(value)
     .digest("hex")
 
-  if (await db.otps.findOne({ otp: signature })) {
-    return await generateOTP()
+  if (await db.otps.exists({ otp: signature })) {
+    return await generateLoginCode()
   }
+
+  const expiresAt = new Date(Date.now() + 60_000)
 
   return {
     value,
     signature,
+    expiresAt,
   }
 }
