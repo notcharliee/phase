f3c7275d0aa047eb03c59667329a2fd69a8baff0
notcharliee <saunderscharlie@hotmail.com>
2024-12-20 00:03:13 +0000

    feat: add VariantProps export in @repo/ui

--- packages/@repo/ui/src/lib/utils.ts
+++ packages/@repo/ui/src/lib/utils.ts
@@ -3,7 +3,7 @@ import { twMerge } from "tailwind-merge"
 
 import type { ClassValue } from "clsx"
 
-export { cva } from "class-variance-authority"
+export { cva, type VariantProps } from "class-variance-authority"
 
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
